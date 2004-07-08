9b09bc315ec3a6944a64c42599e337f35f29699f
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-07-08 09:11:24 +0000

    A program to importing splat results into database(graph.splat_result).
    git-svn-id: file:///usr/local/svn/annot@48 4ebff559-900f-0410-a220-daa186eb8a63

---
+++ trunk/bin/splat_to_db.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+import sys,os,cStringIO,psycopg
+
+class splat_result_iterator:
+	'''looping over a splat result file, generate a single pattern result'''
+	def __init__(self, inf):
+		self.inf = inf
+		self.pattern = ''
+	def __iter__(self):
+		return self		
+	def next(self):
+		self.read()
+		return cStringIO.StringIO(self.pattern)
+	def read(self):
+		self.pattern = ''
+		line = self.inf.readline()
+		while line != '\n':
+			if line == '':
+				raise StopIteration
+				break
+			self.pattern += line
+			line = self.inf.readline()
+		self.pattern += line
+	
+class splat_to_db:
+	'''
+	'''
+	def __init__(self, infname, dbname, needcommit=0):
+		self.organism = ''
+		self.no_of_edges = ''
+		self.recurrence_pattern = ''
+		self.edge_set = []
+		self.inf = open(infname, 'r')
+		self.needcommit = needcommit
+		self.conn = psycopg.connect('dbname=%s'%dbname)
+		self.curs = self.conn.cursor()
+		self.curs.execute("set search_path to graph")
+		self.org_short2long = {'at':'Arabidopsis thaliana',
+					'ce':'Caenorhabditis elegans',
+					'dm':'Drosophila melanogaster',
+					'hs':'Homo sapiens',
+					'mm':'Mus musculus',
+					'sc':'Saccharomyces cerevisiae',
+					'Arabidopsis thaliana':'Arabidopsis thaliana',
+					'Caenorhabditis elegans':'Caenorhabditis elegans',
+					'Drosophila melanogaster':'Drosophila melanogaster',
+					'Homo sapiens':'Homo sapiens',
+					'Mus musculus':'Mus musculus',
+					'Gorilla gorilla Pan paniscus Homo sapiens':'Homo sapiens',
+					'Saccharomyces cerevisiae':'Saccharomyces cerevisiae'}
+					
+	def parse(self, pattern):
+		self.organism = self.org_short2long['sc']
+		line = pattern.readline()
+		no_in_string = line[:-1]
+		self.no_of_edges = no_in_string
+		line = pattern.readline()
+		self.recurrence_pattern = line[:-1]
+		line = pattern.readline()
+		self.edge_set = []	#initialize the edge_set structure
+		while line != '\n':
+			list = line.split(')')
+			list.pop()	#throw away the last '\n'
+			for item in list:
+				l_list = item[1:].split()
+				edge = [int(l_list[0]), int(l_list[1])]
+				self.edge_set.append(edge)
+			line = pattern.readline()
+			
+	def run(self):
+		iter = splat_result_iterator(self.inf)
+		for pattern in iter:
+			self.parse(pattern)
+			sys.stderr.write(self.organism + '\n' + self.no_of_edges + '\n' + self.recurrence_pattern +'\n')
+			try:
+				self.curs.execute("insert into splat_result(organism,no_of_edges, \
+							recurrence_pattern,edge_set)values (%s,%s,B%s,ARRAY"+repr(self.edge_set) + ")",  \
+							(self.organism, self.no_of_edges, self.recurrence_pattern ))
+			except:
+				sys.stderr.write('Error occured when inserting pattern. Aborted.\n')
+				self.conn.rollback()
+				sys.exit(1)
+		if self.needcommit:
+			self.conn.commit()
+
+
+if __name__ == '__main__':
+	instance = splat_to_db(sys.argv[1], sys.argv[2], 1)
+	#argv[1] is the splat result file.
+	#argv[2] is the database name
+	instance.run()
