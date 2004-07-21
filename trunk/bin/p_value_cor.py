756c627e9a879192aca20d9fb12fd8afe6e75676
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-07-21 06:17:29 +0000

    Program to generate the corresponding correlation values under different p_values and different degrees of freedom.
    git-svn-id: file:///usr/local/svn/annot@94 4ebff559-900f-0410-a220-daa186eb8a63

---
+++ trunk/bin/p_value_cor.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+'''
+We assume that correlation value between two gene expression vectors is
+distributed as t-distribution. The degree_of_freedom is the number of measurements.
+
+Program is to generate correlation values under different p_values and different
+degree_of_freedom. 
+
+Usage: p_value_cor.py
+
+Output will dumped to stdout. File, 'p_value_cor.png' is generated in the current
+directory, which is a plot, correlation v.s df.
+
+'''
+
+import sys
+from rpy import r
+from numarray import *
+
+class p_value_cor:
+	def __init__(self):
+		self.p_value_list = [0.25, 0.1, 0.05, 0.025, 0.01, 0.005, 0.001, 0.0005, 0.0001]
+		self.df_lower = 6
+		self.df_upper = 100
+		self.result_array = []
+		
+	def run(self):
+		for df in range(self.df_lower, self.df_upper+1):
+			cor_list = []
+			for p_value in self.p_value_list:
+				cor_list.append( r.qt(p_value, df, lower_tail=r.FALSE)/r.sqrt(df) )
+			self.result_array.append(cor_list)
+	
+	def output(self, of=sys.stdout):
+		self.matrix = array(self.result_array)
+		p_value_list = map(str, self.p_value_list)
+		of.write('p_value\t%s\n'%'\t'.join(p_value_list))
+		df = self.df_lower
+		for cor_list in self.result_array:
+			cor_list = map(str, cor_list)
+			of.write('%d\t%s\n'%(df, '\t'.join(cor_list)))
+			df += 1
+		r.png('p_value_cor.png')
+		cor_list = self.matrix[:,2]
+		p_value_label = self.p_value_list[2]
+		df_list = range(self.df_lower, self.df_upper+1)
+		r.plot(df_list, cor_list, type='o', pch='*', xlab='df', ylab='correlation', main='p_value: %s'%p_value_label)
+		r.dev_off()
+		
+if __name__ == '__main__':
+	instance = p_value_cor()
+	instance.run()
+	instance.output()
