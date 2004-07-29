a7549fba8c52fb2dcd02434cd272c06b2745e44b
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-07-29 08:28:50 +0000

    add parameters to specify the x,y limits
    git-svn-id: file:///usr/local/svn/annot@131 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/stat_plot.py
+++ trunk/bin/stat_plot.py
@@ -7,6 +7,8 @@ Options:
   -c ..., --connectivity=...	connectivity_cut_off
   -l ..., --limit=...	max no. of clusters related to one gene
   -t ..., --tag=...	just tag
+  -x ..., --xlim=...	the range for the x axis, 0.0-1.0(default)
+  -y ..., --ylim=...	the range for the y axis, 0.0-1.0(default)
   -o ..., --output=...	output file name
   -h, --help		show this help
 
@@ -14,16 +16,21 @@ Examples:
   stat_plot.py -c 0.8 -l 10 -t sc_known
   stat_plot.py -c 0.8-0.9 -l 10 -t sc_known
   stat_plot.py -c 0.8-0.9 -l 5-10 -t sc_known
+  stat_plot.py -x 0.2-0.8 -y 0.3-0.6 -l 0 -t sc_shu_known
 """
 
 import sys, os, cStringIO, psycopg, getopt
 from rpy import r
 
 class stat_plot:
-	def __init__(self, dbname, c_range, l_range, tag, ofname):
+	def __init__(self, dbname, c_range, l_range, tag, xlim, ylim, ofname):
 		self.conn = psycopg.connect('dbname=%s'%dbname)
 		self.curs = self.conn.cursor()
 		self.curs.execute("set search_path to graph")
+		self.x_range = xlim.split('-')
+		self.x_range = map(float, self.x_range)
+		self.y_range = ylim.split('-')
+		self.y_range = map(float, self.y_range)
 		self.c_range = c_range.split('-')
 		if len(self.c_range) == 1:
 			self.c_range.append(self.c_range[0])
@@ -61,7 +68,7 @@ class stat_plot:
 			positive_predictive_value_list.append(float(entry[0])/(entry[0]+entry[2]))
 
 		if self.no_of_curves==1:
-			r.plot(sensitivity_list, positive_predictive_value_list, type='o',pch='*',xlab='sensitivity', \
+			r.plot(sensitivity_list, positive_predictive_value_list, type='o',pch='*',xlab='sensitivity',xlim=self.x_range,ylim=self.y_range, \
 			ylab='positive_predictive_value', main='Connectivity: %s  Limit: %s'%(repr(self.c_range),repr(self.l_range)), col=self.no_of_curves)
 		else:
 			r.lines(sensitivity_list, positive_predictive_value_list, type='o',pch='*',col=self.no_of_curves)
@@ -72,9 +79,9 @@ if __name__ == '__main__':
 		print __doc__
 		sys.exit(2)
 	
-	long_option_list = ["help", "dbname=", "connectivity=", "limit=", "tag=", "output="]
+	long_option_list = ["help", "dbname=", "connectivity=", "limit=", "tag=", "xlim=", "ylim=", "output="]
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hd:c:l:t:o:", long_option_list)
+		opts, args = getopt.getopt(sys.argv[1:], "hd:c:l:t:x:y:o:", long_option_list)
 	except:
 		print __doc__
 		sys.exit(2)
@@ -83,6 +90,8 @@ if __name__ == '__main__':
 	connectivity = None
 	limit = None
 	tag = None
+	xlim = '0.0-1.0'
+	ylim = '0.0-1.0'
 	output =''
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
@@ -96,13 +105,17 @@ if __name__ == '__main__':
 			limit = arg
 		elif opt in ("-t", "--tag"):
 			tag = arg
+		elif opt in ("-x", "--xlim"):
+			xlim = arg
+		elif opt in ("-y", "--ylim"):
+			ylim = arg
 		elif opt in ("-o", "--output"):
 			output = arg
 	
 	if connectivity and limit and tag:
 		if output=='':
 			output = '%s_%s_%s'%(tag,connectivity,limit)
-		instance = stat_plot(dbname, connectivity, limit, tag, output)
+		instance = stat_plot(dbname, connectivity, limit, tag, xlim, ylim, output)
 		instance.run()
 	else:
 		print __doc__
