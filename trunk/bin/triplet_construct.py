9e2196cca782f9e95aec4a56eae8a20d61447a5f
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-08-07 04:56:00 +0000

    add the feature to construct quadruplets
    git-svn-id: file:///usr/local/svn/annot@157 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/triplet_construct.py
+++ trunk/bin/triplet_construct.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python
-import pickle,sys,os,gzip
+"""
+Usage: triplet_construct.py [OPTION] DATA_DIR FILE
+
+Option:
+	DATA_DIR specifies which directory contains results from
+		graph construction. Those files should be gspan format.
+	FILE is the file to store the resultant triplets or quadruplets.
+		The file will be gzipped.
+	-q, --quadruplets	search for the quadruplets, default is triplets
+	-h, --help              show this help
+	
+Examples:
+	triplet_construct.py gph_result/sc_yh60_type0 pickle/triplets.gz
+	triplet_construct.py -q gph_result/sc_yh60_type0 pickle/quadruplets.gz
+	
+Description:
+.
+"""
+
+import pickle,sys,os,gzip,getopt
 
 class triplet_construct:
-	def __init__(self, of):
+	def __init__(self, of, quadruplets=0):
 		self.local_vertex_dict = {}
 		self.local_duplet_dict = {}
 		self.outf = of
+		self.quadruplets = int(quadruplets)
 
 	def init(self):
 		self.local_vertex_dict = {}
+		#sort of a linked list(see log)
 		self.local_duplet_dict = {}
+		#a graph dictionary
 		
 	def parse(self, inf):
 		#loop below initilizes local_duplet_dict & local_vertex_dict.
@@ -41,7 +63,7 @@ class triplet_construct:
 			#secure that vertices in triplet and duplet are in ascending order.
 			self.local_vertex_dict[vertex].sort()
 			neighbour_list = self.local_vertex_dict[vertex]
-			sys.stderr.write('\t%d\n'%vertex)
+			#sys.stderr.write('\t%d\n'%vertex)
 			no_of_neighbours = len(neighbour_list)
 			for i in range(no_of_neighbours):
 				for j in range(i+1, no_of_neighbours):
@@ -53,17 +75,38 @@ class triplet_construct:
 						self.outf.write('%d,%d,%d\n'%(triplet[0],triplet[1],triplet[2],))
 						triplets_count += 1
 		sys.stderr.write('\tTotal triplets: %d\n'%triplets_count)
-	
+
+	def local_quadruplet_construct_to_file(self):
+		quadruplets_count = 0
+		for vertex in self.local_vertex_dict:
+			#secure that vertices are in ascending order.
+			self.local_vertex_dict[vertex].sort()
+			neighbour_list = self.local_vertex_dict[vertex]
+			#sys.stderr.write('\t%d\n'%vertex)
+			no_of_neighbours = len(neighbour_list)
+			for i in range(no_of_neighbours):
+				for j in range(i+1, no_of_neighbours):
+					if self.local_duplet_dict.has_key((neighbour_list[i], neighbour_list[j])):
+						#sys.stderr.write('duplet: %s\n'%repr(duplet))
+						for k in range(j+1, no_of_neighbours):
+							if (neighbour_list[i], neighbour_list[k]) in self.local_duplet_dict and (neighbour_list[j], neighbour_list[k]) in self.local_duplet_dict:
+								self.outf.write('%d,%d,%d,%d\n'%(vertex, neighbour_list[i], neighbour_list[j], neighbour_list[k]))
+								quadruplets_count += 1
+		sys.stderr.write('\tTotal quadruplets: %d\n'%quadruplets_count)
+
 	def run(self, inf):
 		self.init()
 		self.parse(inf)
-		self.local_triplet_construct_to_file()
+		if self.quadruplets==1:
+			self.local_quadruplet_construct_to_file()
+		else:
+			self.local_triplet_construct_to_file()
 
-def triplet_batch(dir, ofname):
+def triplet_batch(dir, ofname, quadruplets):
 	files = os.listdir(dir)
 	sys.stderr.write("\tTotally, %d files to be processed.\n"%len(files))
 	of = gzip.open(ofname, 'w')
-	instance = triplet_construct(of)
+	instance = triplet_construct(of, quadruplets)
 
 	for f in files:
 		pathname = os.path.join(dir, f)
@@ -75,15 +118,27 @@ def triplet_batch(dir, ofname):
 
 
 if __name__ == '__main__':
-	def helper():
-		sys.stderr.write("\
-	argv[1] specifies which directory contains results from \n\
-		graph construction. Those files should be gspan format.\n\
-	argv[2] the file to store the resultant triplets.\n\
-		The file will be gzipped.\n")
-	
-	if len(sys.argv) == 3:
-		triplet_batch(sys.argv[1], sys.argv[2])
+	if len(sys.argv) == 1:
+		print __doc__
+		sys.exit(2)
+		
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hq", ["help", "quadruplets"])
+	except:
+		print __doc__
+		sys.exit(2)
+
+	quadruplets = 0
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			print __doc__
+			sys.exit(2)
+		elif opt in ("-q", "--quadruplets"):
+			quadruplets = 1
+
+
+	if len(args) == 2:
+		triplet_batch(args[0], args[1], quadruplets)
 	else:
-		helper()
-		sys.exit(1)
+		print __doc__
+		sys.exit(2)
