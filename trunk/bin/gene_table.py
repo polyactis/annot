28a8215f61a4a69d367c3bc1455e460007ad29b6
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-08-15 04:36:27 +0000

    program changed to play only one role: collect genes from a subset of that organism's total datasets into schema.gene, in a union or intersection form. It depends on graph.gene_id_to_no.
    git-svn-id: file:///usr/local/svn/annot@183 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/gene_table.py
+++ trunk/bin/gene_table.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python
 """
-Usage: gene_table.py -d DATABASENAME -k SCHEMA -g ORGANISM [OPTION]
+Usage: gene_table.py -k SCHEMA -g ORGANISM [OPTION] DATADIR
 
 Option:
-	-d ..., --dbname=...	the database name
+	DATADIR is the directory containing all the datasets
+	-d ..., --dbname=...	the database name, graphdb(default)
 	-k ..., --schema=...	which schema in the database
-	-c ..., --commit=...	1 or 0(default) specifies commit or not
 	-g ..., --organism=...	two letter organism abbreviation
+	-u, --union	takes the union of all genes in the datasets, default is intersection
+	-c, --commit	commits the database transaction
 	-h, --help              show this help
 	
 Examples:
-	gene_table.py -d mdb -k shu -g sc
+	gene_table.py -k hs_yh60 -g hs datasets/hs_wanted
 
 Description:
-	Depends on two tables,
-	graph.gene_id_to_no and schema.go.
-	Sets up table schema.gene.
+	This program sets up schema.gene from the datasets, which
+	are probably a subset from that organism's total datasets.
+	It depends on table graph.gene_id_to_no.
 """
 
-import pickle, sys, os, psycopg, getopt
+import pickle, sys, os, psycopg, csv, getopt
+from sets import Set
 
-
-class gene_table_setup:
+class gene_table:
 	'''
-	Initialize the gene_table of yeast from two pickled data structures,
-		known_genes_dict
-		global_struc['vertex_list']
+	Initialize the local gene_id:gene_no mapping in table schema.gene
 	'''
-	def __init__(self, dbname, schema, orgn, needcommit=0):
+	def __init__(self, dir, dbname, schema, orgn, union=0, needcommit=0):
+		self.dir = dir
 		self.conn = psycopg.connect('dbname=%s'%dbname)
 		self.curs = self.conn.cursor()
 		self.curs.execute("set search_path to %s"%schema)
+		self.union = int(union)
 		self.needcommit = int(needcommit)
 		self.org_short2long = {'at':'Arabidopsis thaliana',
 			'ce':'Caenorhabditis elegans',
@@ -46,43 +48,61 @@ class gene_table_setup:
 			'Gorilla gorilla Pan paniscus Homo sapiens':'Homo sapiens',
 			'Saccharomyces cerevisiae':'Saccharomyces cerevisiae'}
 		self.organism = self.org_short2long[orgn]
-
-	def dstruc_loadin(self):
-		self.geneno_go_dict = {}
-		self.curs.execute("select go_no, gene_array from go")
-		rows = self.curs.fetchall()
-		for row in rows:
-			go_no = row[0]
-			if row[1] == '{}':
-				continue
-			gene_no_list = row[1][1:-1].split(',')
-			gene_no_list = map(int, gene_no_list)
-			for gene_no in gene_no_list:
-				if gene_no in self.geneno_go_dict:
-					self.geneno_go_dict[gene_no].append(go_no)
-				else:
-					self.geneno_go_dict[gene_no] = [go_no]
+		#mapping between gene_id and gene_no
+		self.gene_id2gene_no = {}
+		#mapping between gene_id and its occurence
+		self.gene_id2freq = {}
+		#unique gene collection, for database submission
+		self.gene_set = Set()
 		
-		self.geneno_to_geneid = {}
+	def dstruc_loadin(self):
+		#setup self.gene_id2gene_no
 		self.curs.execute("select gene_id, gene_no from graph.gene_id_to_no where organism='%s'"%self.organism)
 		rows = self.curs.fetchall()
 		for row in rows:
-			self.geneno_to_geneid[row[1]] = row[0]
+			self.gene_id2gene_no[row[0]] = row[1]
 	
 	def run(self):
-		for gene_no in self.geneno_go_dict:
-			gene_id = self.geneno_to_geneid[gene_no]
-			known = '1'
-			if len(self.geneno_go_dict[gene_no])==1 and self.geneno_go_dict[gene_no][0]==0 :
-				known = '0'
-			string_geneno_go_dict = repr(self.geneno_go_dict[gene_no])
-			string_geneno_go_dict = '{' + string_geneno_go_dict[1:-1] + '}'
-			self.curs.execute("insert into gene(gene_id, gene_no, known, \
-				go_functions) values ('%s', %d, '%s', '%s')"%\
-				(gene_id, gene_no, known, string_geneno_go_dict ))
+		#load in the data structures first.
+		self.dstruc_loadin()
+		#iterate over all the datasets, find all the genes
+		files = os.listdir(self.dir)
+		sys.stderr.write("\tTotally, %d files to be processed.\n"%len(files))
+		new_yeast_gene_list = []
+		for f in files:
+			sys.stderr.write("%d/%d:\t%s\n"%(files.index(f)+1,len(files),f))
+			f_path = os.path.join(self.dir, f)
+			reader = csv.reader(file(f_path), delimiter='\t')
+			for row in reader:
+				if row[0] in self.gene_id2freq:
+				#not first encountering
+					self.gene_id2freq[row[0]] += 1
+				else:
+				#first encountering
+					self.gene_id2freq[row[0]] = 1
+			del reader
+		
+		if self.union:
+		#take the union set
+			self.gene_set = Set(self.gene_id2freq.keys())
+		else:
+		#take the intersection set
+			for (gene_id, freq) in self.gene_id2freq.iteritems():
+				if freq == len(files):
+				#occur in all datasets
+					self.gene_set.add(gene_id)
+		sys.stderr.write("%d genes to be submitted\n"%len(self.gene_set))
+		#database submission
+		self.submit()
+		
+	def submit(self):
+		sys.stderr.write("Database transacting...")
+		for gene_id in self.gene_set:
+			self.curs.execute("insert into gene(gene_id, gene_no) values ('%s', %d)"%\
+				(gene_id, self.gene_id2gene_no[gene_id] ))
 		if self.needcommit:
 			self.conn.commit()
-
+		sys.stderr.write("done.\n")
 
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
@@ -90,15 +110,16 @@ if __name__ == '__main__':
 		sys.exit(2)
 		
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hd:k:c:g:", ["help", "dbname=", "schema=", "commit=", "organism="])
+		opts, args = getopt.getopt(sys.argv[1:], "hd:k:g:uc", ["help", "dbname=", "schema=", "organism=", "union", "commit"])
 	except:
 		print __doc__
 		sys.exit(2)
 	
-	dbname = ''
+	dbname = 'graphdb'
 	schema = ''
-	commit = 0
 	organism = ''
+	union = 0
+	commit = 0
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
 			print __doc__
@@ -107,14 +128,15 @@ if __name__ == '__main__':
 			dbname = arg
 		elif opt in ("-k", "--schema"):
 			schema = arg
-		elif opt in ("-c", "--commit"):
-			commit = int(arg)
-		elif opt in ("-g", "-organism"):
+		elif opt in ("-g", "--organism"):
 			organism = arg
+		elif opt in ("-u", "--union"):
+			union = 1
+		elif opt in ("-c", "--commit"):
+			commit = 1
 			
-	if dbname and schema and organism:
-		instance = gene_table_setup(dbname, schema, organism, commit)
-		instance.dstruc_loadin()
+	if schema and organism and len(args) == 1:
+		instance = gene_table(args[0], dbname, schema, organism, union, commit)
 		instance.run()
 	else:
 		print __doc__
