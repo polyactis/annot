08bf24b991376ce77c5db27a597b2cbe3cf7a801
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-08-16 09:40:52 +0000

    inherit the class in gene_stat_on_mcl_result.py New stuff, works on leave_one_out data, one cluster one function, L0/L1 approach.
    git-svn-id: file:///usr/local/svn/annot@189 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/gene_stat.py
+++ trunk/bin/gene_stat.py
@@ -7,7 +7,7 @@ Option:
 	-k ..., --schema=...	which schema in the database
 	-p ..., --p_value_cut_off=...	p_value_cut_off
 	-u ..., --unknown_cut_off=...	unknown_cut_off, 0.85(default), for wu's
-	-l ..., --limit=...,	maximum number of clusters related to one gene, 10(default)
+	-l ..., --limit=...,	OBSOLETE, accepted for backwards compatibility
 	-n ..., --connectivity_cut_off=...	0.8(default), minimum connectivity of a mcl cluster
 	-w, --wu	Wu's strategy(Default is Jasmine's strategy)
 	-r, --report	report the progress(a number)
@@ -15,40 +15,34 @@ Option:
 	-h, --help              show this help
 
 Examples:
-	gene_stat.py -k shu -p 0.001
-	gene_stat.py -k shu -l 0 -p 0.001 -n 0.7
-	gene_stat.py -k shu -l 0 -p 0.001 -n 0.7 -u 0.80 -w
+	gene_stat.py -k shu -p 0.001 -w
+	gene_stat.py -k shu -p 0.001 -n 0.7 -w -r
+	gene_stat.py -k shu -p 0.001 -n 0.7 -u 0.80 -w
 
 Description:
 	This program is mainly for validation purpose. Run after cluster_stat.py.
 	Implements both Jasmine and Wu's strategy.
+	The unknown_cut_off is different from the one in gene_stat_on_mcl_result.
+	The former is unknown class p_value cut_off.
+	The latter is unknown genes ratio.
 """
 
 import sys, os, psycopg, getopt
 from rpy import *
+from graphlib import Graph
+from sets import Set
+from gene_stat_on_mcl_result import gene_stat_on_mcl_result, gene_prediction
 
 
-class gene_stat:
-	def __init__(self, dbname, schema, p_value_cut_off, limit=10, connectivity_cut_off=0.8, wu=0, report=0, needcommit=0):
-		self.conn = psycopg.connect('dbname=%s'%dbname)
-		self.curs = self.conn.cursor()
-		self.curs.execute("set search_path to %s"%schema)
-		self.p_value_cut_off = float(p_value_cut_off)
-		self.limit = int(limit)
-		self.connectivity_cut_off = float(connectivity_cut_off)
-		self.wu = int(wu)
-		self.report = int(report)
-		self.needcommit = int(needcommit)
-		self.tp = 0.0
-		self.tn = 0.0
-		self.fp = 0.0
-		self.fn = 0.0
-		self.known_genes_dict = {}
-		self.no_of_records = 0
-		self.gene_to_be_updated = []
-		self.log_file = open('/tmp/gene_stat.log','w')
-		
+class gene_stat(gene_stat_on_mcl_result):
+	def __init__(self, dbname, schema, p_value_cut_off, unknown_cut_off, limit=0, \
+		connectivity_cut_off=0.8, wu=0, report=0, needcommit=0):
+		gene_stat_on_mcl_result.__init__(self, dbname, schema, p_value_cut_off, \
+			unknown_cut_off, limit, connectivity_cut_off, wu, report, needcommit)
+
 	def dstruc_loadin(self):
+		sys.stderr.write("Loading Data STructure...")
+		#setup self.known_genes_dict
 		self.curs.execute("select gene_no,go_functions from gene where known=TRUE")
 		rows = self.curs.fetchall()
 		for row in rows:
@@ -56,128 +50,38 @@ class gene_stat:
 			self.known_genes_dict[row[0]] = []
 			for go_no in go_functions_list:
 				self.known_genes_dict[row[0]].append(int(go_no))
-		if self.wu:
-			self.curs.execute("select count(go_no) from go")
-		else:
-			self.curs.execute("select count(go_no) from go where go_no!=0")
+		
+		#get the non-obsolete biological_process GO DAG		
+		self.curs.execute("select count(go_no) from go")
 		rows = self.curs.fetchall()
 		self.no_of_functions = rows[0][0]
-
-	def run(self):
-		for gene_no in self.known_genes_dict:
-			mcl_id_list = []
-			self.log_file.write('%d '%gene_no)
-			if self.limit == 0:
-				self.curs.execute("select mcl_id, p_value_vector \
-					from cluster_stat where leave_one_out=%d \
-					and connectivity>=%f"%(gene_no, self.connectivity_cut_off))
-			else:
-				self.curs.execute("select mcl_id, p_value_vector \
-					from cluster_stat where leave_one_out=%d \
-					and connectivity>=%f limit %d"%(gene_no, self.connectivity_cut_off, self.limit))
-			rows = self.curs.fetchall()
-			p_functions_dict = {}
-			for row in rows:
-				mcl_id_list.append(row[0])
-				p_value_vector = row[1][1:-1].split(',')
-				for i in range(self.no_of_functions):
-					if float(p_value_vector[i]) <= self.p_value_cut_off:
-						if self.wu:
-							p_functions_dict[i] = 1
-						else:
-							p_functions_dict[i+1] = 1
-			if rows:
-				self._gene_stat(gene_no, mcl_id_list, p_functions_dict)
-				self.no_of_records += 1
-				if self.report:
-					sys.stderr.write('%s%s'%('\x08'*20, self.no_of_records))
-		if self.needcommit:
-			for entry in self.gene_to_be_updated:
-				if entry[6]:
-					self.curs.execute("update gene set cluster_array=ARRAY%s,\
-					tp=%d,tn=%d,fp=%d,fn=%d,p_functions=ARRAY%s where gene_no=%d"%\
-					(repr(entry[1]),entry[2],entry[3],entry[4],entry[5],repr(entry[6]),entry[0]))
-				else:
-					self.curs.execute("update gene set cluster_array=ARRAY%s,\
-					tp=%d,tn=%d,fp=%d,fn=%d where gene_no=%d"%\
-					(repr(entry[1]),entry[2],entry[3],entry[4],entry[5],entry[0]))
-			self.conn.commit()
-		sys.stderr.write('\n\tTotal genes: %d\n'%self.no_of_records)
-		sys.stderr.write('\tSensitvity: %f\n'%(self.tp/(self.tp+self.fn)))
-		sys.stderr.write('\tSpecificity: %f\n'%(self.tn/(self.fp+self.tn)))
-		sys.stderr.write('\tFalse Positive Ratio: %f\n'%(self.fp/(self.tp+self.fp)))
-			
-	def _gene_stat(self, gene_no, mcl_id_list, p_functions_dict):
-		tp = 0
-		tn = 0
-		fp = 0
-		fn = 0
-		p_functions = p_functions_dict.keys()
-		for go_no in self.known_genes_dict[gene_no]:
-			if go_no in p_functions_dict:
-				tp += 1
-				del p_functions_dict[go_no]
-			else:
-				fn += 1
-		fp = len(p_functions_dict)
-		tn = self.no_of_functions - (tp+fp+fn)
-		self.tp += tp
-		self.tn += tn
-		self.fp += fp
-		self.fn += fn
-		self.log_file.write('%d %d %d %d %s %s\n'%(tp,tn,fp,fn,repr(p_functions),repr(mcl_id_list)))
-		self.gene_to_be_updated.append([gene_no,mcl_id_list,tp,tn,fp,fn,p_functions])
-
-class gene_prediction:
-	def __init__(self):
-		self.tp = 0
-		self.tn = 0
-		self.fp = 0
-		self.fn = 0
-		self.p_functions_dict = {}
-		self.mcl_id_list = []
-
-
-class gene_stat_new:
-	def __init__(self, dbname, schema, p_value_cut_off, unknown_cut_off, limit=0, connectivity_cut_off=0.8, wu=0, report=0, needcommit=0):
-		self.conn = psycopg.connect('dbname=%s'%dbname)
-		self.curs = self.conn.cursor()
-		self.curs.execute("set search_path to %s"%schema)
-		self.p_value_cut_off = float(p_value_cut_off)
-		self.unknown_cut_off = float(unknown_cut_off)
-		self.limit = int(limit)
-		self.connectivity_cut_off = float(connectivity_cut_off)
-		self.wu = int(wu)
-		self.report = int(report)
-		self.needcommit = int(needcommit)
-		self.tp = 0.0
-		self.tn = 0.0
-		self.fp = 0.0
-		self.fn = 0.0
-		self.known_genes_dict = {}
-		self.no_of_records = 0
-		self.log_file = open('/tmp/gene_stat.log','w')
-		self.gene_prediction_dict = {}
-		self.no_of_p_known = 0
-		
-	def dstruc_loadin(self):
-		self.curs.execute("select gene_no,go_functions from gene where known=TRUE")
+		self.curs.execute("select t2t.term1_id, t2t.term2_id, t1.acc, t2.acc from \
+			go.term2term t2t, go.term t1, go.term t2 where t2t.term1_id=t1.id and \
+			t2t.term2_id=t2.id and t1.is_obsolete=0 and t2.is_obsolete=0 and \
+			t1.term_type='biological_process' and t2.term_type='biological_process' ")
 		rows = self.curs.fetchall()
 		for row in rows:
-			go_functions_list = row[1][1:-1].split(',')
-			self.known_genes_dict[row[0]] = []
-			for go_no in go_functions_list:
-				self.known_genes_dict[row[0]].append(int(go_no))
+		#setup the go_graph structure
+			self.go_graph.add_edge(row[2], row[3])
+		
+		#setup self.go_no2go_id
+		self.curs.execute("select go_no, go_id from go")
+		rows = self.curs.fetchall()
+		for row in rows:
+			self.go_no2go_id[row[0]] = row[1]
+		
+		#setup self.no_of_functions
 		if self.wu:
 			self.curs.execute("select count(go_no) from go")
 		else:
 			self.curs.execute("select count(go_no) from go where go_no!=0")
 		rows = self.curs.fetchall()
 		self.no_of_functions = rows[0][0]
+		sys.stderr.write("Done\n")
 
 	def run(self):
 		if self.limit != 0:
-			sys.stderr.write("gene_stat_new now doesn't support limit\n")
+			sys.stderr.write("gene_stat now doesn't support limit\n")
 			sys.exit(2)
 
 		self.curs.execute("DECLARE crs CURSOR FOR select mcl_id, leave_one_out, p_value_vector \
@@ -189,7 +93,6 @@ class gene_stat_new:
 				#if self.known_genes_dict.has_key(row[1]):
 				# for clusters containing unknown genes
 				self._gene_stat(row)
-				self.no_of_records += 1
 			if self.report:
 				sys.stderr.write('%s%s'%('\x08'*20, self.no_of_records))
 			
@@ -198,77 +101,49 @@ class gene_stat_new:
 		
 		self.final()
 		if self.needcommit:
-			for gene_no in self.gene_prediction_dict:
-				entry = self.gene_prediction_dict[gene_no]
-				p_functions = entry.p_functions_dict.keys()
-				if gene_no in self.known_genes_dict:
-					if p_functions:
-						self.curs.execute("update gene set cluster_array=ARRAY%s,\
-						tp=%d,tn=%d,fp=%d,fn=%d,p_functions=ARRAY%s where gene_no=%d"%\
-						(repr(entry.mcl_id_list),entry.tp,entry.tn,entry.fp,entry.fn,repr(p_functions),gene_no))
-					else:
-						self.curs.execute("update gene set cluster_array=ARRAY%s,\
-						tp=%d,tn=%d,fp=%d,fn=%d where gene_no=%d"%\
-						(repr(entry.mcl_id_list),entry.tp,entry.tn,entry.fp,entry.fn,gene_no))
-				else:
-					if p_functions:
-						self.curs.execute("update gene set cluster_array=ARRAY%s,\
-						p_functions=ARRAY%s where gene_no=%d"%\
-						(repr(entry.mcl_id_list),repr(p_functions),gene_no))
-					else:
-						self.curs.execute("update gene set cluster_array=ARRAY%s \
-						where gene_no=%d"%\
-						(repr(entry.mcl_id_list),gene_no))
-				
-			self.curs.execute("end")
+		#Database updating is too slow. Do it only if needcommit.
+			self.submit()
 		sys.stderr.write('\n\tp_value_cut_off:%f unknown_cut_off:%f connectivity_cut_off:%f\n'%(self.p_value_cut_off, self.unknown_cut_off, self.connectivity_cut_off))
 		sys.stderr.write('\tTotal genes: %d\n'%len(self.gene_prediction_dict))
 		sys.stderr.write('\tTotal known genes: %d\n'%self.no_of_p_known)
-		sys.stderr.write('\tSensitvity: %f\n'%(self.tp/(self.tp+self.fn)))
+		sys.stderr.write('\tWhole Sensitvity: %f\n'%((self.tp+self.tp1)/(self.tp+self.tp1+self.fn)))
+		sys.stderr.write('\tTP0: %d  TP1: %d  TN: %d  FP: %d  FN: %d\n'%(self.tp, self.tp1, self.tn, self.fp, self.fn))
+		sys.stderr.write('\tTP0_M: %d  TP1_M: %d  FP_M: %d\n'%(self.tp_m, self.tp1_m, self.fp_m))
 		sys.stderr.write('\tSpecificity: %f\n'%(self.tn/(self.fp+self.tn)))
-		sys.stderr.write('\tFalse Positive Ratio: %f\n'%(self.fp/(self.tp+self.fp)))
+		sys.stderr.write('\tFalse Positive Ratio: %f\n'%(self.fp_m/(self.tp_m+self.tp1_m+self.fp_m)))
 			
 	def _gene_stat(self, row):
+		p_value_vector = row[2][1:-1].split(',')
+		#transform into float type
+		p_value_vector = map(float, p_value_vector)
+		min_p_value =min(p_value_vector)
+		if self.wu:
+			if float(p_value_vector[0]) <= self.unknown_cut_off:
+			#too many unknown genes
+				return
+		if min_p_value > self.p_value_cut_off:
+		#none of the predicted functions in this cluster is significant
+			return
+		self.no_of_records += 1
+		mcl_id = row[0]
 		gene_no = row[1]
 		if gene_no not in self.gene_prediction_dict:
 			item = gene_prediction()
 			self.gene_prediction_dict[gene_no] = item
-		self.gene_prediction_dict[gene_no].mcl_id_list.append(row[0])
-		p_value_vector = row[2][1:-1].split(',')
-		if self.wu:
-			if float(p_value_vector[0]) <= self.unknown_cut_off:
-				return
+		self.gene_prediction_dict[gene_no].mcl_id_list.append(mcl_id)				
 		for i in range(self.no_of_functions):
-			if float(p_value_vector[i]) <= self.p_value_cut_off:
+			if p_value_vector[i] == min_p_value:
 				if self.wu:
-					self.gene_prediction_dict[gene_no].p_functions_dict[i] = 1
+				#index 0 corresponds to go_no 0.
+					go_no = i
 				else:
-					self.gene_prediction_dict[gene_no].p_functions_dict[i+1] = 1
-		
-	def final(self):
-		for gene_no in self.gene_prediction_dict:
-			entry = self.gene_prediction_dict[gene_no]
-			p_functions_dict = entry.p_functions_dict.copy()
-			p_functions = p_functions_dict.keys()
-			if gene_no not in self.known_genes_dict:
-				self.log_file.write('%d %s %s\n'%(gene_no, repr(p_functions),repr(entry.mcl_id_list)))
-				continue
-			self.no_of_p_known += 1
-			for go_no in self.known_genes_dict[gene_no]:
-				if go_no in p_functions_dict:
-					entry.tp += 1
-					del p_functions_dict[go_no]
+				#index 0 corresponds to go_no 1
+					go_no = i+1
+				if go_no not in self.gene_prediction_dict[gene_no].p_functions_dict:
+				#value in p_functions_dict stores the number of associated clusters.
+					self.gene_prediction_dict[gene_no].p_functions_dict[go_no] = 1
 				else:
-					entry.fn += 1
-			entry.fp = len(p_functions_dict)
-			entry.tn = self.no_of_functions - (entry.tp+entry.fp+entry.fn)
-
-			self.tp += entry.tp
-			self.tn += entry.tn
-			self.fp += entry.fp
-			self.fn += entry.fn
-			self.log_file.write('%d %d %d %d %d %s %s\n'%(gene_no, entry.tp,entry.tn,entry.fp,entry.fn,repr(p_functions),repr(entry.mcl_id_list)))
-
+					self.gene_prediction_dict[gene_no].p_functions_dict[go_no] += 1
 
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
@@ -285,7 +160,7 @@ if __name__ == '__main__':
 	dbname = 'graphdb'
 	schema = ''
 	p_value_cut_off = None
-	limit = 10
+	limit = 0
 	connectivity_cut_off = 0.8
 	wu = 0
 	report = 0
@@ -315,7 +190,7 @@ if __name__ == '__main__':
 			commit = 1
 	
 	if schema and p_value_cut_off:
-		instance = gene_stat_new(dbname, schema, p_value_cut_off, unknown_cut_off, limit, connectivity_cut_off, wu, report, commit)
+		instance = gene_stat(dbname, schema, p_value_cut_off, unknown_cut_off, limit, connectivity_cut_off, wu, report, commit)
 		instance.dstruc_loadin()
 		instance.run()
 	else:
