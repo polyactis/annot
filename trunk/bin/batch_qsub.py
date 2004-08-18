11ed3896a506221320e5830eda7562c21163594f
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-08-18 07:42:27 +0000

    add a parameter to specify qsub or sh.
    git-svn-id: file:///usr/local/svn/annot@193 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/batch_qsub.py
+++ trunk/bin/batch_qsub.py
@@ -3,13 +3,14 @@
 Usage: batch_qsub.py -c PROGRAM_TO_BE_INVOKED -i INIT_NUM -n NO_OF_JOBS -o OUTPUT_PREFIX [OPTIONS] DATADIR
 
 Option:
-	DATADIR is the directory containing all the files to be qsubbed.
+	DATADIR is the directory containing all the files to be 'qsub'bed or 'sh'ed.
 	-c ..., --call=... Call the program. FULL PATH please.
 	-j ..., --job_prefix=...	the prefix of all the jobs, default is the name of the program called.
 	-i ..., --initnum=...	The initial number for all the jobs.
 	-n ..., --no_of_jobs=...	specify how many jobs you want to submit
 	-p ..., --parameter=...	parameters to be passed to the program invoked, DOUBLE QUOTE IT
 	-o ..., --oprefix=...	output prefix, FULL PATH please.
+	-s, --shell	Execute the job file via call 'sh', instead of 'qsub'
 	-f, --file_form		the input for PROGRAM_TO_BE_INVOKED is a file, not a directory(default)
 	-h, --help              show this help
 	
@@ -17,13 +18,16 @@ Examples:
 	
 	batch_qsub.py -c ~/script/annot/bin/triplet_construct.py -i 0 -n 10 -p "-q -z" -o sc_quadruplets gph_result/sc_yh60_type0
 		:submit 10 quadruplet construction jobs
-	batch_qsub.py -c ~/script/annot/bin/graph.bin -j graph_construct -i 0 -n 10 -o gph_sc_dataset -f datasets/yeast_data
-		:submit 10 graph construction jobs, in file form. This is a bad example. Don't use it.
+	batch_qsub.py -c ~/script/annot/bin/batch_graphcc.py -j graph_construct -i 0 -n 10 -o gph_sc_dataset  datasets/yeast_data
+		:submit 10 graph construction jobs,
+	batch_qsub.py -c ~/script/annot/bin/gspan2splat_output.py -j gspan2splat -i 0 -n 38
+		-p "-n 38" -f -s -o gph_result/sc_yh60_fp_splat/splat gph_result/sc_yh60_fp_type1
+		:batch run gspan2splat_output over 38 files.
 	
 Description:
-	This program partitions a big job into several subjobs and submits them via qsub.
-	It can either move all the files into several sub directories and call the program
-	to run over each directory, or run the program over the files.
+	This program partitions a big job into several subjobs and submits them via qsub
+	or runs them via 'sh'. It can either move all the files into several sub directories
+	and call the program to run over each directory, or run the program over the files.
 
 Warning:
 	If you want submit the same job again. Please remove the tmp directories first.
@@ -37,7 +41,7 @@ class batch_qsub:
 	'''
 	Partition a big job into several small jobs
 	'''
-	def __init__(self, dir, call, job_prefix, initnum, no_of_jobs, parameter, oprefix, file_form):
+	def __init__(self, dir, call, job_prefix, initnum, no_of_jobs, parameter, oprefix, shell, file_form):
 		self.dir = os.path.abspath(dir)
 		self.program = os.path.abspath(call)
 		if job_prefix == '':
@@ -47,6 +51,9 @@ class batch_qsub:
 		self.no_of_jobs = int(no_of_jobs)
 		self.parameter = parameter
 		self.oprefix = oprefix
+		self.submit_command = 'qsub'
+		if shell == 1:
+			self.submit_command = 'sh'
 		self.file_form = int(file_form)
 		
 		if not os.path.isdir(self.dir):
@@ -98,8 +105,9 @@ class batch_qsub:
 			job_f = open(job_fname, 'w')
 			job_f.write(out_block)
 			job_f.close()
-			wl = ['qsub', job_fname]
-			os.spawnvp(os.P_WAIT, 'qsub', wl)
+			wl = [self.submit_command, job_fname]
+			sys.stderr.write('\tJob %d: %s\n'%(i, job_fname))
+			os.spawnvp(os.P_WAIT, self.submit_command, wl)
 			if index == no_of_files:
 			#there're no files left to allocate, so break.
 			#otherwise, you will submit 'empty' jobs
@@ -112,7 +120,8 @@ if __name__ == '__main__':
 		sys.exit(2)
 		
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hc:j:i:n:p:o:f", ["help", "call=", "job_prefix=", "initnum=", "no_of_jobs=", "parameter=", "oprefix=", "file_form"])
+		opts, args = getopt.getopt(sys.argv[1:], "hc:j:i:n:p:o:sf", \
+			["help", "call=", "job_prefix=", "initnum=", "no_of_jobs=", "parameter=", "oprefix=", "shell", "file_form"])
 	except:
 		print __doc__
 		sys.exit(2)
@@ -123,6 +132,7 @@ if __name__ == '__main__':
 	no_of_jobs = None
 	parameter = ''
 	oprefix = ''
+	shell = 0
 	file_form = 0
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
@@ -141,12 +151,14 @@ if __name__ == '__main__':
 			parameter = arg
 		elif opt in ("-o", "--oprefix"):
 			oprefix = arg
+		elif opt in ("-s", "--shell"):
+			shell = 1 
 		elif opt in ("-f", "--file_form"):
 			file_form = 1
 
 
 	if call and initnum and no_of_jobs and oprefix and len(args) == 1:
-		instance = batch_qsub(args[0], call, job_prefix, initnum, no_of_jobs, parameter, oprefix, file_form)
+		instance = batch_qsub(args[0], call, job_prefix, initnum, no_of_jobs, parameter, oprefix, shell, file_form)
 		instance.submit()
 	else:
 		print __doc__
