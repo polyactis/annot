678b10e52bcd873b40fbbaba5197fd663e4e9340
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-08-20 08:48:16 +0000

    add a connectivity_cut_off
    git-svn-id: file:///usr/local/svn/annot@206 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/mcl_result2fp_input.py
+++ trunk/bin/mcl_result2fp_input.py
@@ -6,6 +6,7 @@ Option:
 	-d ..., --dbname=...	the database name, graphdb(default)
 	-k ..., --schema=...	which schema in the database
 	-s ..., --size=...	the number of mcl records in one fetch, 5000(default)
+	-n ..., --connectivity_cut_off=...	0.5(default), minimum connectivity of a mcl cluster
 	-h, --help              show this help
 	
 Examples:
@@ -21,11 +22,12 @@ Description:
 import sys, os, psycopg, getopt
 
 class mcl_result2fp_input:
-	def __init__(self, dbname, schema):
+	def __init__(self, dbname, schema, connectivity_cut_off):
 		self.vertex_dict = {}
 		self.conn = psycopg.connect('dbname=%s'%dbname)
 		self.curs = self.conn.cursor()
 		self.curs.execute("set search_path to %s"%schema)
+		self.connectivity_cut_off = float(connectivity_cut_off)
 		self.no_of_vertices =0	#redundant
 		self.max_cluster_size = 0
 		self.no_of_clusters = 0
@@ -34,7 +36,8 @@ class mcl_result2fp_input:
 
 	def get_from_db(self, package_size):
 		self.curs.execute("begin")
-		self.curs.execute("DECLARE crs CURSOR FOR select mcl_id, splat_id, vertex_set from mcl_result")
+		self.curs.execute("DECLARE crs CURSOR FOR select mcl_id, splat_id, vertex_set from mcl_result\
+			where connectivity>=%f"%self.connectivity_cut_off)
 		self.curs.execute("fetch %d from crs"%(package_size))
 		rows = self.curs.fetchall()
 		no_of_packages = 0
@@ -63,7 +66,8 @@ class mcl_result2fp_input:
 		if len_of_vertex_list > self.max_cluster_size:
 			self.max_cluster_size = len_of_vertex_list
 		#mcl's clusters are already sorted.
-		self.datasetf.write('%d\t%s\n'%(len_of_vertex_list, '\t'.join(vertex_list)))
+		self.datasetf.write('%s\n'%('\t'.join(vertex_list)))
+		#self.datasetf.write('%d\t%s\n'%(len_of_vertex_list, '\t'.join(vertex_list)))
 	
 	def spec_write(self):
 		self.specf.write('dataset\n')
@@ -77,7 +81,7 @@ if __name__ == '__main__':
 		sys.exit(2)
 		
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hd:k:s:", ["help", "dbname=", "schema=", "size="])
+		opts, args = getopt.getopt(sys.argv[1:], "hd:k:s:n:", ["help", "dbname=", "schema=", "size=", "connectivity_cut_off"])
 	except:
 		print __doc__
 		sys.exit(2)
@@ -85,6 +89,7 @@ if __name__ == '__main__':
 	dbname = 'graphdb'
 	schema = ''
 	size = 5000
+	connectivity_cut_off = 0.5
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
 			print __doc__
@@ -95,9 +100,11 @@ if __name__ == '__main__':
 			schema = arg
 		elif opt in ("-s", "--size"):
 			size = int(arg)
+		elif opt in ("-n", "--connectivity_cut_off"):
+			connectivity_cut_off = float(arg)
 
 	if schema:
-		instance = mcl_result2fp_input(dbname, schema)
+		instance = mcl_result2fp_input(dbname, schema, connectivity_cut_off)
 		instance.get_from_db(size)
 
 	else:
