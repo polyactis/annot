58d93a316d1a0586ee7beec5fd4b93f5fee3b69e
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-08-21 00:05:31 +0000

    add a parameter to choose set_connectivity or not.
    git-svn-id: file:///usr/local/svn/annot@208 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/fim_to_db.py
+++ trunk/bin/fim_to_db.py
@@ -6,6 +6,7 @@ Option:
 	FIM_OUTPUT_FILE is a file containing results of fim.
 	-d ..., --dbname=...	the database name, graphdb(default)
 	-k ..., --schema=...	which schema in the database
+	-n, --set_connectivity	this will set all fim_result's connectivity to 1.
 	-c, --commit	commit the database transaction
 	-r, --report	report the progress(a number)
 	-h, --help              show this help
@@ -16,14 +17,15 @@ Examples:
 Description:
 	Parse the fim ouput files.
 	import results into schema.fim_result.
-
+	set_connectivity is to make downstream stats convenient.
+	
 """
 
 import sys, os, psycopg, csv, getopt
 
 
 class fim_to_db:
-	def __init__(self, infname, dbname, schema, report, needcommit=0):
+	def __init__(self, infname, dbname, schema, set_connectivity, report, needcommit=0):
 		self.reader = csv.reader(file(infname), delimiter=' ')
 		self.conn = psycopg.connect('dbname=%s'%dbname)
 		self.curs = self.conn.cursor()
@@ -31,6 +33,7 @@ class fim_to_db:
 		self.curs.execute("truncate fim_result")
 		self.curs.execute("alter sequence fim_result_mcl_id_seq restart with 1")
 		self.report = int(report)
+		self.set_connectivity = int(set_connectivity)
 		self.needcommit = int(needcommit)
 		
 	def submit(self):
@@ -41,8 +44,12 @@ class fim_to_db:
 			if len(row) >= 3:
 				row = map(int, row)
 				string_row = '{' + repr(row)[1:-1] + '}'
-				self.curs.execute("insert into fim_result(vertex_set, support)\
-					values('%s', %d)"%(string_row, support))
+				if self.set_connectivity:
+					self.curs.execute("insert into fim_result(vertex_set, connectivity, support)\
+						values('%s', 1, %d)"%(string_row, support))
+				else:
+					self.curs.execute("insert into fim_result(vertex_set, support)\
+						values('%s', %d)"%(string_row, support))
 				no+=1
 				if self.report:
 					sys.stderr.write('%s%s'%('\x08'*20, no))
@@ -58,15 +65,16 @@ if __name__ == '__main__':
 		sys.exit(2)
 		
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hrcd:k:", ["help", "report", "dbname=", "schema=", "commit"])
+		opts, args = getopt.getopt(sys.argv[1:], "hrd:k:nc", ["help", "report", "dbname=", "schema=", "set_connectivity", "commit"])
 	except:
 		print __doc__
 		sys.exit(2)
 	
 	dbname = 'graphdb'
 	schema = ''
-	commit = 0
+	set_connectivity = 0
 	report = 0
+	commit = 0
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
 			print __doc__
@@ -75,13 +83,16 @@ if __name__ == '__main__':
 			dbname = arg
 		elif opt in ("-k", "--schema"):
 			schema = arg
-		elif opt in ("-c", "--commit"):
-			commit = 1
+		elif opt in ("-n", "--set_connectivity"):
+			set_connectivity = 1
 		elif opt in ("-r", "--report"):
 			report = 1
+		elif opt in ("-c", "--commit"):
+			commit = 1
+
 
 	if schema and len(args)==1:
-		instance = fim_to_db(args[0], dbname, schema, report, commit)
+		instance = fim_to_db(args[0], dbname, schema, set_connectivity, report, commit)
 		instance.submit()
 	else:
 		print __doc__
