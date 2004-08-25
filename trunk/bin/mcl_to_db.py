579ab0f2e239aeb6babcbb9953c5f398c2b7f80f
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-08-25 07:59:55 +0000

    add one iterator and one parser for min's mcode output
    git-svn-id: file:///usr/local/svn/annot@216 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/mcl_to_db.py
+++ trunk/bin/mcl_to_db.py
@@ -4,24 +4,27 @@ Usage: mcl_to_db.py -k SCHEMA -s SIZE [OPTION] MCLINPUTDIR
 
 Option:
 	MCLINPUTDIR is directory containing the mcl output files.
+	-z ..., --hostname=...	the hostname, zhoudb(default)
 	-d ..., --dbname=...	the database name, graphdb(default)
 	-k ..., --schema=...	which schema in the database
+	-t ..., --type=...	result type, 'mcl'(default) or 'mcode'
 	-c, --commit	commit the database transaction
 	-r, --report	report the progress(a number)
 	-h, --help              show this help
 	
 Examples:
 	mcl_to_db.py -k shu gph_result/mcl_input
-	
+	mcl_to_db.py -k shu -t mcode gph_result/pattern-splat.out
+
 Description:
-	Parse the mcl output results, which are prefixed by 'out' and 
+	Parse the mcl or mcode output results, which are prefixed by 'out' and 
 	import results into schema.mcl_result.
 
 """
 
-import sys,os,psycopg,cStringIO,getopt
+import sys, os, psycopg, csv, cStringIO, getopt
 
-class mcl_result_iterator:
+class mcl_iterator:
 	'''looping over a mcl result file, generate a block of clusters related to one splat pattern'''
 	def __init__(self, inf):
 		self.inf = inf
@@ -53,10 +56,31 @@ class mcl_result_iterator:
 			line = self.inf.readline()
 		self.mcl_cluster_block += line
 
+class mcode_iterator:
+	'''looping over a mcode result file, generate a single pattern result'''
+	def __init__(self, inf):
+		self.inf = inf
+		self.pattern = ''
+	def __iter__(self):
+		return self		
+	def next(self):
+		self.read()
+		return cStringIO.StringIO(self.pattern)
+	def read(self):
+		self.pattern = ''
+		line = self.inf.readline()
+		while line != '\n':
+			if line == '':
+				raise StopIteration
+				break
+			self.pattern += line
+			line = self.inf.readline()
+		self.pattern += line
+
 class mcl_to_db:
-	def __init__(self, dir, dbname, schema, report, needcommit=0):
+	def __init__(self, dir, hostname, dbname, schema, type, report, needcommit=0):
 		self.dir = os.path.abspath(dir)
-		self.conn = psycopg.connect('dbname=%s'%dbname)
+		self.conn = psycopg.connect('host=%s dbname=%s'%(hostname, dbname))
 		self.curs = self.conn.cursor()
 		self.curs.execute("set search_path to %s"%schema)
 		try:
@@ -66,12 +90,21 @@ class mcl_to_db:
 			self.curs.execute("set search_path to %s"%schema)
 		self.curs.execute("truncate mcl_result")
 		self.curs.execute("alter sequence mcl_result_mcl_id_seq restart with 1")
+		self.type = type
 		self.report = int(report)
 		self.needcommit = int(needcommit)
 		self.splat_id = ''
 		self.cluster_set = []
 		self.parameter = ''
+		self.connectivity = 0
 		self.mcl_to_be_inserted = []
+		
+		#iterator mapping
+		self.iterator_dict = {'mcl': mcl_iterator,
+			'mcode': mcode_iterator}
+		#parser mapping
+		self.parser_dict = {'mcl': self.mcl_parser,
+			'mcode': self.mcode_parser}
 		
 	def submit(self):
 		f_list = os.listdir(self.dir)
@@ -80,24 +113,24 @@ class mcl_to_db:
 			if fname.find('out') == 0:
 				path = os.path.join(self.dir, fname)
 				inf = open(path, 'r')
-				iter = mcl_result_iterator(inf)
+				iter = self.iterator_dict[self.type](inf)
 				for mcl_cluster_block in iter:
-					self.parse(mcl_cluster_block)
+					self.parser_dict[self.type](mcl_cluster_block)
 					for i in range(len(self.cluster_set)):
 
 						vertex_set = self.cluster_set[i]
 						string_vertex_set = repr(vertex_set)
 						string_vertex_set = string_vertex_set.replace('[','{')
 						string_vertex_set = string_vertex_set.replace(']','}')
-						self.mcl_to_be_inserted.append([self.splat_id, string_vertex_set, self.parameter])
+						self.mcl_to_be_inserted.append([self.splat_id, string_vertex_set, self.parameter, self.connectivity])
 						no+=1
 					if self.report:
 						sys.stderr.write('%s%s'%('\x08'*20, no))
 		if self.needcommit:
 			for entry in self.mcl_to_be_inserted:
 				try:
-					self.curs.execute("insert into mcl_result(splat_id, vertex_set, parameter) \
-					values (%s,'%s','%s')"%(entry[0], entry[1], entry[2]))
+					self.curs.execute("insert into mcl_result(splat_id, vertex_set, parameter, connectivity) \
+					values (%s,'%s','%s',%f)"%(entry[0], entry[1], entry[2], entry[3]))
 				except:
 					sys.stderr.write('Error occured when inserting pattern. Aborted.\n')
 					self.conn.rollback()
@@ -106,7 +139,27 @@ class mcl_to_db:
 			self.conn.commit()
 		sys.stderr.write('\n\tTotal clusters: %d\n'%no)
 
-	def parse(self,inf):
+	def mcode_parser(self, inf):
+		reader = csv.reader(inf, delimiter='\t')
+		#first row is the splat_id
+		row = reader.next()
+		if row == []:
+			#the last empty block returned by mcode_iterator
+			return
+		self.splat_id = row[0]
+		#second row is the vertices delimited by '\t'
+		row = reader.next()
+		#throw away the last empty element caused by the last '\t'
+		row.pop()
+		self.cluster_set = [map(int, row)]
+		#third row is the number of nodes and the number of edges
+		row = reader.next()
+		no_of_nodes = float(row[0])
+		no_of_edges = float(row[1])
+		self.connectivity = 2*no_of_edges/(no_of_nodes*(no_of_nodes-1))
+		
+
+	def mcl_parser(self,inf):
 		line = inf.readline()
 		self.cluster_set = []
 		cluster_begin = 0
@@ -142,30 +195,36 @@ if __name__ == '__main__':
 		sys.exit(2)
 		
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hrcd:k:", ["help", "report", "dbname=", "schema=", "commit"])
+		opts, args = getopt.getopt(sys.argv[1:], "hrcz:d:k:t:", ["help", "report", "hostname=", "dbname=", "schema=", "type=", "commit"])
 	except:
 		print __doc__
 		sys.exit(2)
-	
+
+	hostname = 'zhoudb'
 	dbname = 'graphdb'
 	schema = ''
+	type = 'mcl'
 	commit = 0
 	report = 0
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
 			print __doc__
 			sys.exit(2)
+		elif opt in ("-z", "--hostname"):
+			hostname = arg
 		elif opt in ("-d", "--dbname"):
 			dbname = arg
 		elif opt in ("-k", "--schema"):
 			schema = arg
+		elif opt in ("-t", "--type"):
+			type = arg
 		elif opt in ("-c", "--commit"):
 			commit = 1
 		elif opt in ("-r", "--report"):
 			report = 1
 
 	if schema and len(args)==1:
-		instance = mcl_to_db(args[0], dbname, schema, report, commit)
+		instance = mcl_to_db(args[0], hostname, dbname, schema, type, report, commit)
 		instance.submit()
 
 	else:
