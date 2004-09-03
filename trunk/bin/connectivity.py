f928cb1e2d292df99258721df1dc222515cdc5b6
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-09-03 02:17:18 +0000

    add a source_table parameter to specify the splat table add a target_table parameter to specify the mcl table
    git-svn-id: file:///usr/local/svn/annot@225 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/connectivity.py
+++ trunk/bin/connectivity.py
@@ -6,21 +6,25 @@ Option:
 	-z ..., --hostname=...	the hostname, zhoudb(default)
 	-d ..., --dbname=...	the database name, graphdb(default)
 	-k ..., --schema=...	which schema in the database
-	-t ..., --table=...	'splat' or 'mcl', compute which table's connectivity
+	-s ..., --source_table=...	which splat table
+	-t ..., --target_table=...	which mcl table
 	-c, --commit	commit the database transaction
 	-r, --report	report the progress(a number)
 	-h, --help              show this help
 	
 Examples:
-	connectivity.py -k shu -c -t mcl
-	connectivity.py -k ming1 -r -t splat
+	connectivity.py -k shu -c -s splat_result
+	connectivity.py -k shu -c -s splat_result -t mcl_result
+	connectivity.py -k sc_yh60_splat -r -s splat_result_sup_5 -t mcl_result
 	
 Description:
 	This program computes the connectivity of splat and mcl results.
-	1000 records are done in one time. Reduce the memory usage.
+	5000 records are done in one time. Reduce the memory usage.
 	This is done through postgresql's CURSOR mechanism.
-	Computing connectivity of mcl results is based on splat_id.
-	Because edge_dict is required in advance.
+	If only source_table is given, this program computes the connectivity
+	of the splat patterns. If both source_table and target_table are given, the
+	program computes the connectivity of mcl clusters as the latter depends on
+	splat patterns.
 	
 """
 
@@ -35,8 +39,7 @@ class compute_connectivity:
 	Computing connectivity of mcl results is based on splat_id.
 	Because edge_dict is required in advance.
 	'''
-	def __init__(self, table, hostname, dbname, schema, report, needcommit=0):
-		self.table = table
+	def __init__(self, hostname, dbname, schema, source_table, target_table, report, needcommit=0):
 		self.conn = psycopg.connect('host=%s dbname=%s'%(hostname, dbname))
 		self.curs = self.conn.cursor()
 		self.curs.execute("set search_path to %s"%schema)
@@ -45,47 +48,46 @@ class compute_connectivity:
 		except psycopg.ProgrammingError, error:
 			self.conn.rollback()
 			self.curs.execute("set search_path to %s"%schema)
+		self.source_table = source_table
+		self.target_table = target_table
 		self.report = int(report)
 		self.needcommit = int(needcommit)
 		self.vertex_dict = {}
 		#a structure to store vertices of a splat pattern
 		self.edge_dict = {}
 		#structure to store edges of a splat pattern
-		self.no_of_splat_records = 0
-		self.no_of_mcl_records = 0
+		self.no_of_records = 0
 		self.vertex_pool = kjSet()
 		#store the vertices of all the splat patterns
 		self.log_file = open('/tmp/connectivity.log', 'w')
 					
 	def run(self):
 		self.curs.execute("begin")
-		self.curs.execute("DECLARE crs CURSOR FOR select splat_id,edge_set from splat_result")
+		self.curs.execute("DECLARE crs CURSOR FOR select splat_id,edge_set from %s"%self.source_table)
 		self.curs.execute("fetch 5000 from crs")
 		rows = self.curs.fetchall()
 		while rows:
 			for row in rows:
 				splat_id = row[0]
 				edge_set = row[1]
-				if self.table == 'splat':
-					self.splat_atom_update(splat_id, edge_set)
-				elif self.table == 'mcl':
+				if self.target_table:
 					self.mcl_atom_update(splat_id, edge_set)
+				else:
+					#target_table is not given, so compute only the connectivity of source_table
+					self.splat_atom_update(splat_id, edge_set)
 
 			if self.report:
-				if self.table == 'splat':
-					sys.stderr.write("%s%s"%("\x08"*20,self.no_of_splat_records))
-				elif self.table == 'mcl':
-					sys.stderr.write("%s%s"%("\x08"*20,self.no_of_mcl_records))
+				sys.stderr.write("%s%s"%("\x08"*20,self.no_of_records))
 			self.curs.execute("fetch 5000 from crs")
 			rows = self.curs.fetchall()
 
 		sys.stderr.write("\tNumber of distinct genes in all splat patterns: %d\n"%len(self.vertex_pool) )
 		self.log_file.write(repr(self.vertex_pool))
 		if self.needcommit:
-			self.curs.execute("create index connectivity_mcl_result_idx on mcl_result(connectivity)")
+			if self.target_table:
+				self.curs.execute("create index %s_connectivity_idx on %s(connectivity)"%(self.target_table, self.target_table))
 			self.curs.execute("end")
-			sys.stderr.write('\n\tTotal %d splat records updated\n'%self.no_of_splat_records)
-			sys.stderr.write('\tTotal %d mcl records updated\n'%self.no_of_mcl_records)
+			sys.stderr.write('\n\tTotal %d records updated\n'%self.no_of_records)
 		else:
 			self.conn.rollback()
 			sys.stderr.write('\n\tNo real updates\n')
@@ -95,8 +97,8 @@ class compute_connectivity:
 		Compute the connectivity of all mcl clusters pertaining to the same splat_id.
 		'''
 		self.dstruc_from_edge_set(edge_set)
-		self.curs.execute("select mcl_id, vertex_set from mcl_result where splat_id=%d"%\
-			(splat_id))
+		self.curs.execute("select mcl_id, vertex_set from %s where splat_id=%d"%\
+			(self.target_table, splat_id))
 		rows = self.curs.fetchall()
 		for row in rows:
 			mcl_id = row[0]
@@ -111,12 +113,12 @@ class compute_connectivity:
 						no_of_edges += 1
 			connectivity = 2.0*no_of_edges/((no_of_vertices-1)*no_of_vertices)
 			try:
-				self.curs.execute("update mcl_result set connectivity=%f where mcl_id=%d"% \
-				(connectivity, mcl_id))
+				self.curs.execute("update %s set connectivity=%f where mcl_id=%d"% \
+				(self.target_table, connectivity, mcl_id))
 			except:
 				sys.stderr.write('Error occurred while setting mcl connectivity\n')
 				sys.exit(1)
-			self.no_of_mcl_records += 1
+			self.no_of_records += 1
 
 	def splat_atom_update(self, splat_id, edge_set):
 		self.dstruc_from_edge_set(edge_set)
@@ -124,12 +126,12 @@ class compute_connectivity:
 		no_of_vertices = len(self.vertex_dict)
 		connectivity = 2.0*no_of_edges/((no_of_vertices-1)*no_of_vertices)
 		try:
-			self.curs.execute("update splat_result set connectivity=%f where splat_id=%d"% \
-			(connectivity, splat_id))
+			self.curs.execute("update %s set connectivity=%f where splat_id=%d"% \
+			(self.source_table, connectivity, splat_id))
 		except:
 			sys.stderr.write('Error occurred while setting splat connectivity\n')
 			sys.exit(1)
-		self.no_of_splat_records += 1
+		self.no_of_records += 1
 	
 	def dstruc_from_edge_set(self, edge_set):
 		self.edge_dict = {}
@@ -154,7 +156,8 @@ if __name__ == '__main__':
 		sys.exit(2)
 		
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hrz:d:k:ct:", ["help", "report", "hostname=", "dbname=", "schema=", "commit", "table="])
+		opts, args = getopt.getopt(sys.argv[1:], "hrz:d:k:s:t:c", \
+			["help", "report", "hostname=", "dbname=", "schema=", "source_table=", "target_table=", "commit"])
 	except:
 		print __doc__
 		sys.exit(2)
@@ -162,9 +165,10 @@ if __name__ == '__main__':
 	hostname = 'zhoudb'
 	dbname = 'graphdb'
 	schema = ''
+	source_table = ''
+	target_table = ''
 	commit = 0
 	report = 0
-	table = ''
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
 			print __doc__
@@ -175,15 +179,17 @@ if __name__ == '__main__':
 			dbname = arg
 		elif opt in ("-k", "--schema"):
 			schema = arg
+		elif opt in ("-s", "--source_table"):
+			source_table = arg
+		elif opt in ("-t", "--target_table"):
+			target_table = arg
 		elif opt in ("-c", "--commit"):
 			commit = 1
 		elif opt in ("-r", "--report"):
 			report = 1
-		elif opt in ("-t", "--table"):
-			table = arg
 
-	if schema and table:
-		instance = compute_connectivity(table, hostname, dbname, schema, report, commit)
+	if schema and source_table:
+		instance = compute_connectivity(hostname, dbname, schema, source_table, target_table, report, commit)
 		instance.run()
 
 	else:
