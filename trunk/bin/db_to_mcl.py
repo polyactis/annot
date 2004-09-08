12b35fdbf6d7206b54f2c479bb343a5cfab11825
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-09-08 01:57:06 +0000

    Add a feature to output file in concatenated kMax input(read by kMax_batch_run.py)
    git-svn-id: file:///usr/local/svn/annot@243 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/db_to_mcl.py
+++ trunk/bin/db_to_mcl.py
@@ -8,6 +8,7 @@ Option:
 	-d ..., --dbname=...	the database name, graphdb(default)
 	-k ..., --schema=...	which schema in the database
 	-t ..., --table=...	the table storing the splat results, splat_result(default)
+	-f ..., --format=...	output format, 0(default, mcl), 1(kMax)
 	-s ..., --size=...	the number of splat records in one output file
 	-h, --help              show this help
 	
@@ -17,18 +18,25 @@ Examples:
 Description:
 	Output schema.splat_result into several files. (mcl_input format)
 	Each file is with the number of splat patterns.
+	Output format:
+		0,	MCL(not direct input of MCL. MCL wrapper program: mcl_batch_run.py)
+		1,	kMax(also not direct input of kMax. kMax wrapper program)
 
 """
 
-import sys,os,cStringIO,psycopg,getopt
+import sys, os, psycopg, getopt
+from sets import Set
 
 class db_to_mcl:
-	def __init__(self, hostname, dbname, schema, table):
+	def __init__(self, hostname, dbname, schema, table, format):
 		self.vertex_dict = {}
 		self.conn = psycopg.connect('host=%s dbname=%s'%(hostname, dbname))
 		self.curs = self.conn.cursor()
 		self.curs.execute("set search_path to %s"%schema)
 		self.table = table
+		self.format = int(format)
+		self.format_dict = {0:self.mcl_singleton_write,
+			1:self.kMax_singleton_write}
 
 	def get_from_db(self, outdir, package_size):
 		if not os.path.isdir(outdir):
@@ -44,7 +52,7 @@ class db_to_mcl:
 			path = os.path.join(outdir, outfname)
 			outf = open(path, 'w')
 			for row in rows:
-				self.mcl_singleton_write(row, outf)
+				self.format_dict[self.format](row, outf)
 				no_of_splat_records += 1
 			outf.close()
 			no_of_packages += 1
@@ -86,14 +94,36 @@ class db_to_mcl:
 			out_block += '%s $\n'%' '.join(self.vertex_dict[vertex])
 		out_block += ')\n\n'
 		outf.write(out_block)
-		
+	
+	def kMax_singleton_write(self, row, outf):
+		vertex_set = Set()	#intialize the vertex_set
+		splat_id = row[0]
+		edge_set = row[1]
+		edge_list = edge_set[2:-2].split('},{')
+		for edge in edge_list:
+			vertex_list = edge.split(',')
+			vertex_set.add(int(vertex_list[0]))
+			vertex_set.add(int(vertex_list[1]))
+		outf.write('t %d\n'%splat_id)
+		#cast to list type
+		vertex_list = list(vertex_set)
+		#sort it before output
+		vertex_list.sort()
+		for vertex in vertex_list:
+			outf.write('v %d %d\n'%(vertex, vertex))
+		for edge in edge_list:
+			#replace the comma with a blank.
+			edge = edge.replace(',', ' ')
+			outf.write('e %s 0\n'%(edge))
+		outf.write('\n')
+
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
 		print __doc__
 		sys.exit(2)
 		
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:t:s:", ["help", "hostname=", "dbname=", "schema=", "table=", "size="])
+		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:t:f:s:", ["help", "hostname=", "dbname=", "schema=", "table=", "format=", "size="])
 	except:
 		print __doc__
 		sys.exit(2)
@@ -102,6 +132,7 @@ if __name__ == '__main__':
 	dbname = 'graphdb'
 	schema = ''
 	table = 'splat_result'
+	format = 0
 	size = 0
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
@@ -115,11 +146,13 @@ if __name__ == '__main__':
 			schema = arg
 		elif opt in ("-t", "--table"):
 			table = arg
+		elif opt in ("-f", "--format"):
+			format = int(arg)
 		elif opt in ("-s", "--size"):
 			size = int(arg)
 
 	if schema and size and len(args)==1:
-		instance = db_to_mcl(hostname, dbname, schema, table)
+		instance = db_to_mcl(hostname, dbname, schema, table, format)
 		instance.get_from_db(args[0], size)
 
 	else:
