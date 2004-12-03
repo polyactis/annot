32a1e20834f14fba94bf87b5bb0864e7e9577d5d
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2004-12-03 05:33:34 +0000

    from gene_stat_plot import gene_stat not from gene_stat add a depth cutoff list dstruc_loadin only once, and do no create instance every time.
    git-svn-id: file:///usr/local/svn/annot@322 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/batch_stat.py
+++ trunk/bin/batch_stat.py
@@ -14,6 +14,9 @@ Option:
 	-n ..., --connectivity_list=...	a list of connectivity cut_off, "0.8"(default)
 	-r ..., --recurrence_list=...	a list of recurrence cut_off, "5"(default)
 	-s ..., --size_list=...	a list of cluster_size cut_off, "1000"(default)
+	-e ..., --depth_list=...	the minimum depth for a go node to be valid, 3(default)
+	-f ..., --dir_files=...	the directory containing all the files outputed by cluster_stat.py
+	-j ..., --judger_type=...	how to judge predicted functions, 0(default), 1, 2
 	-l, --leave_one_out	use the leave_one_out stat method(gene_stat).
 			default is gene_stat_on_mcl_result.
 	-w, --wu	Ignore it, default is also wu's strategy.
@@ -36,12 +39,12 @@ Description:
 """
 
 import sys, os, psycopg, getopt
-from gene_stat import gene_stat
+from gene_stat_plot import gene_stat
 #from gene_stat_on_mcl_result import gene_stat_on_mcl_result
 
 class batch_stat:
 	def __init__(self, hostname, dbname, schema, table, mcl_table, tag, p_value_list, unknown_list, connectivity_list,\
-			recurrence_list, size_list, leave_one_out=0, wu=0, dominant=0, needcommit=0):
+			recurrence_list, size_list, leave_one_out, wu, judger_type, depth_list, dir_files=None, dominant=0, needcommit=0):
 		self.hostname = hostname
 		self.dbname = dbname
 		self.conn = psycopg.connect('host=%s dbname=%s'%(hostname, dbname))
@@ -57,6 +60,9 @@ class batch_stat:
 		self.cluster_size_list = size_list
 		self.leave_one_out = int(leave_one_out)
 		self.wu = int(wu)
+		self.judger_type = int(judger_type)
+		self.depth_list = depth_list
+		self.dir_files = dir_files
 		self.dominant = int(dominant)
 		self.needcommit = int(needcommit)
 		#a dictionary mapping the user choices to stat classes
@@ -64,25 +70,35 @@ class batch_stat:
 		#	0: gene_stat_on_mcl_result}
 		self.stat_data = []
 
+
+		#default parameters to invoke gene_stat
+		self.report = 0
+		self.log = 0
+		self.needcommit_of_gene_stat = 0
+		self.gene_table = 'p_gene'
 		
 	def run(self):
+		instance = gene_stat(self.hostname, self.dbname, self.schema, self.table, self.mcl_table, self.p_value_list[0],\
+			self.unknown_list[0], self.connectivity_list[0], self.recurrence_list[0], self.cluster_size_list[0], \
+			self.leave_one_out, self.wu, self.report,\
+			self.log, self.judger_type, self.depth_list[0], self.dir_files, self.needcommit_of_gene_stat,\
+			self.gene_table, self.dominant)
+		instance.dstruc_loadin()
 		for unknown in self.unknown_list:
 			for connectivity in self.connectivity_list:
 				for recurrence in self.recurrence_list:
 					for size in self.cluster_size_list:
 						for p_value in self.p_value_list:
-							instance = gene_stat(self.hostname, self.dbname, schema, table, mcl_table, p_value,\
-								unknown, connectivity, recurrence, size, self.leave_one_out, self.wu, self.dominant)
-							instance.dstruc_loadin()
-							instance.run()
-							if self.needcommit:
-								self.curs.execute("insert into graph.stat_plot_data(tag, p_value_cut_off, unknown_cut_off,\
-								connectivity_cut_off, recurrence_cut_off, cluster_size_cut_off, tp, tp_m, tp1, tp1_m,\
-								tn, fp, fp_m, fn) values ('%s', %1.5f, %1.2f, %1.2f, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d)"%\
-								(self.tag, p_value, unknown, connectivity, recurrence, size, instance.tp, instance.tp_m,\
-								instance.tp1, instance.tp1_m, instance.tn, instance.fp, instance.fp_m, instance.fn))
-								self.conn.commit()
-							del instance
+							for depth in self.depth_list:
+								instance.parameter_reset_and_cleanup(unknown, connectivity, recurrence, size, p_value, depth)
+								instance.run()
+								if self.needcommit:
+									self.curs.execute("insert into graph.stat_plot_data(tag, p_value_cut_off, unknown_cut_off,\
+									connectivity_cut_off, recurrence_cut_off, cluster_size_cut_off, depth_cut_off, tp, tp_m, tp1, tp1_m,\
+									tn, fp, fp_m, fn) values ('%s', %1.5f, %1.2f, %1.2f,  %d, %d, %d, %d,  %d, %d, %d, %d,  %d, %d, %d)"%\
+									(self.tag, p_value, unknown, connectivity, recurrence, size, depth, instance.tp,   instance.tp_m,\
+									instance.tp1, instance.tp1_m, instance.tn,  instance.fp, instance.fp_m, instance.fn))
+									self.conn.commit()
 
 		
 if __name__ == '__main__':
@@ -91,9 +107,9 @@ if __name__ == '__main__':
 		sys.exit(2)
 		
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:t:m:g:p:u:n:r:s:lwvc", ["help", "hostname=", "dbname=", "schema=", "table=",\
+		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:t:m:g:p:u:n:r:s:e:j:f:lwvc", ["help", "hostname=", "dbname=", "schema=", "table=",\
 			"mcl_table=", "tag=", "p_value_list=", "unknown_list=", "connectivity_list=", "recurrence_list=",\
-			"size_list=", "leave_one_out", "wu", "dominant", "commit"])
+			"size_list=", "depth_list=", "judger_type=", "dir_files=", "leave_one_out", "wu", "dominant", "commit"])
 	except:
 		print __doc__
 		sys.exit(2)
@@ -104,11 +120,14 @@ if __name__ == '__main__':
 	table = 'cluster_stat'
 	mcl_table = 'mcl_result'
 	tag = ''
-	p_value_list = [0.1, 0.01, 0.001, 0.0001, 0.00001]
-	unknown_list = [0.25]
-	connectivity_list = [0.8]
+	p_value_list = [0.001]
+	unknown_list = [1.0]
+	connectivity_list = [0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
 	recurrence_list = [5]
-	size_list = [1000]
+	size_list = [10000]
+	depth_list = [4]
+	dir_files = None
+	judger_type = 0
 	leave_one_out = 0
 	wu = 1 
 	dominant = 0
@@ -144,6 +163,15 @@ if __name__ == '__main__':
 		elif opt in ("-s", "--size_list"):
 			size_list = arg.split(',')
 			size_list = map(int, size_list)
+		elif opt in ("-e", "--depth_list"):
+			depth_list = arg.split(',')
+			depth_list = map(int, depth_list)
+		elif opt in ("-f", "--dir_files"):
+			dir_files = arg
+		elif opt in ("-o", "--log"):
+			log = 1
+		elif opt in ("-j", "--judger_type"):
+			judger_type = int(arg)
 		elif opt in ("-l", "--leave_one_out"):
 			leave_one_out = 1
 		elif opt in ("-w", "--wu"):
@@ -158,7 +186,7 @@ if __name__ == '__main__':
 			print __doc__
 			sys.exit(2)
 		instance = batch_stat(hostname, dbname, schema, table, mcl_table, tag, p_value_list, unknown_list,\
-			connectivity_list, recurrence_list, size_list, leave_one_out, wu, dominant, commit)
+			connectivity_list, recurrence_list, size_list, leave_one_out, wu, judger_type, depth_list, dir_files, dominant, commit)
 		instance.run()
 	else:
 		print __doc__
