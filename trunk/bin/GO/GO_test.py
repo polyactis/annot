b3ecd39d0e09f28380eff5f5bc6444976ef4ed04
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-02-18 09:41:02 +0000

    a unittest program. Currently it tests the process_2indices() of go_node_distance.py
    git-svn-id: file:///usr/local/svn/annot@375 4ebff559-900f-0410-a220-daa186eb8a63

---
+++ trunk/bin/GO/GO_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import unittest
+from go_node_distance import go_node_distance
+
+class TestGoNodeDistance(unittest.TestCase):
+	def setUp(self):
+		hostname = 'zhoudb'
+		dbname = 'graphdb'
+		schema = 'sc_54'
+		table = 'node_distance'
+		branch = 1
+		depth = 0
+		all = 0
+		new_table = 0
+		report = 0
+		commit = 0
+		log = 0
+		
+		self.instance = go_node_distance(hostname, dbname, schema, table, branch, depth, all, new_table, report, \
+			commit, log)
+		self.instance.dstruc_loadin()
+	
+	def testprocess_2indices(self):
+		self.instance.debug_process_2indices = 1
+		index_set1 = self.instance.go_id2index[14240]
+		index_set2 = self.instance.go_id2index[14265]
+		for index1 in index_set1:
+			for index2 in index_set2:
+				self.instance.process_2indices(index1, index2)
+	
+if __name__ == '__main__':
+	suite = unittest.TestSuite()
+	suite.addTest(unittest.makeSuite(TestGoNodeDistance))
+	unittest.TextTestRunner(verbosity=2).run(suite)
