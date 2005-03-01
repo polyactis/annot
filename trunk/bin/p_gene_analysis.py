b297d0e0c19695d4059ee878fbbdb4dc7e87425b
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-03-01 10:54:42 +0000

    03-01-05 	add a functionality to get p_value_cut_off from linear_model 03-01-05 	add the gene-p table to record real predictions (p_gene_id: p_value_cut_off)
    git-svn-id: file:///usr/local/svn/annot@412 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/p_gene_analysis.py
+++ trunk/bin/p_gene_analysis.py
@@ -10,7 +10,9 @@ Option:
 	-t ..., --table=...	cluster_stat(default)
 	-m ..., --mcl_table=...	mcl_result(default), mcl_result table corresponding to above table.
 	-g ..., --gene_table=...	table to store the stat results, p_gene(default), needed if commit
-	-p ..., --p_value_cut_off=...	p_value_cut_off
+	-l ,,,, --lm_table=...	the lm_table to store the linear_model results, needed if needcommit
+	-n ..., --gene_p_table=...	the table to store p_gene_id:p_value_cut_off, which are real predictions
+	-p ..., --p_value_cut_off=...	p_value_cut_off. 0 means using the linear model from lm_table
 	-j ..., --judger_type=...	how to judge predicted functions, 0(default), 1, 2
 	-r, --report	report the progress(a number)
 	-c, --commit	commit the database transaction, records down the go_no2accuracy.
@@ -22,17 +24,26 @@ Option:
 Examples:
 	p_gene_analysis.py -k sc_54 -g p_gene_e5 -p 0.001 stat_table
 	p_gene_analysis.py -k sc_54 -p 0.01 -c -j 2  -g p_gene_cluster_stat2 stat_table
+	p_gene_analysis.py -k sc_54 -p 0 -j 1 -g p_gene_repos_2_e5
+		-l lm_p_gene_repos_2_e5_v40 stat_table
+	p_gene_analysis.py -k sc_54 -p 0 -j 1 -g p_gene_repos_2_e5 -c -n gene_p_repos_2_e5
+		-l lm_p_gene_repos_2_e5_v40 stat_table
 
 Description:
 	02-28-05
 	this is the second part of gene_stat_plot.py, which works on p_gene table
 	the first part goes to gene_stat.py
+	03-01-05
+		add a functionality to get p_value_cut_off from linear_model
+	03-01-05
+		add the gene-p table to record real predictions (p_gene_id: p_value_cut_off)
 """
 
 import sys, os, psycopg, getopt, csv, math
 from sets import Set
 from rpy import r
 from numarray import *
+from codense.common import db_connect
 
 class prediction_space_attr:
 	"""
@@ -68,8 +79,16 @@ class accuracy_struc:
 
 class p_gene_analysis:
 	"""
-	dstruc_loadin()
+	02-28-05
+		alter the program towards reusage
+	02-28-05
+		this is the second part of gene_stat_plot.py, which works on p_gene table
+		the first part goes to gene_stat.py
+	03-01-05
+		add a functionality to get p_value_cut_off from linear_model
 	run()
+		--init()
+		--db_connect()
 		--data_fetch()
 			--_p_gene_analysis()
 		--overview_stats()
@@ -77,13 +96,14 @@ class p_gene_analysis:
 		--go_no_accuracy()
 		--table_output()
 	"""
-	def __init__(self, hostname, dbname, schema, table, mcl_table, p_value_cut_off, report=0, \
-		judger_type=0, needcommit=0, gene_table='p_gene', \
-		stat_table_fname=None, debug=0, accuracy_cut_off=0):
-		self.conn = psycopg.connect('host=%s dbname=%s'%(hostname, dbname))
-		self.curs = self.conn.cursor()
-		self.schema = schema
-		self.curs.execute("set search_path to %s"%schema)
+	def __init__(self, hostname='zhoudb', dbname='graphdb', schema=None, \
+		table=None, mcl_table=None, p_value_cut_off=None, report=0, \
+		judger_type=0, needcommit=0, gene_table='p_gene', lm_table=None, \
+		stat_table_fname=None, debug=0, accuracy_cut_off=0, gene_p_table=None):
+		
+		self.hostname = hostname
+		self.dbname = dbname
+		self.schema = schema		
 		self.table = table
 		self.mcl_table = mcl_table
 		self.p_value_cut_off = float(p_value_cut_off)
@@ -91,11 +111,14 @@ class p_gene_analysis:
 		self.judger_type = int(judger_type)
 		self.needcommit = int(needcommit)
 		self.gene_table = gene_table
+		self.lm_table = lm_table
 		self.stat_table_fname = stat_table_fname
 		#debugging flag
 		self.debug = int(debug)
 		self.accuracy_cut_off = float(accuracy_cut_off)
-		
+		self.gene_p_table = gene_p_table
+	
+	def init(self):	
 		#open a file
 		if self.stat_table_fname:
 			self.stat_table_f = csv.writer(open(self.stat_table_fname, 'w'), delimiter='\t')
@@ -108,55 +131,125 @@ class p_gene_analysis:
 		#a counter
 		self.no_of_records = 0
 		
-		#data structures to be loaded in
-		self.known_genes_dict = {}
-		
 		#data structures to be created
 		self.prediction_space2attr = {}
 		self.prediction_pair2attr = {}
 		self.go_no2accuracy = {}
+		self.go_no2lm_results = {}
+		self.general_lm_results = None
+		self.gene_p_list = []
 		
-	def dstruc_loadin(self):
-		sys.stderr.write("Loading Data STructure...")
-		
-		#setup self.known_genes_dict
-		self.curs.execute("select gene_no,go_functions from gene where known=TRUE")
-		rows = self.curs.fetchall()
+	def get_known_genes_dict(self, curs):
+		sys.stderr.write("Getting known_genes_dict...")
+		known_genes_dict = {}
+		curs.execute("select gene_no,go_functions from gene where known=TRUE")
+		rows = curs.fetchall()
 		for row in rows:
 			go_functions_list = row[1][1:-1].split(',')
-			self.known_genes_dict[row[0]] = Set()
+			known_genes_dict[row[0]] = Set()
 			for go_no in go_functions_list:
-				self.known_genes_dict[row[0]].add(int(go_no))
-		
+				known_genes_dict[row[0]].add(int(go_no))
 		sys.stderr.write("Done\n")
+		return known_genes_dict
 
+	def get_go_no2lm_results(self, curs, lm_table):
+		"""
+		03-01-05
+			for each go_no, get its linear model parameters
+			also return 2d matrix for a general linear model(see get_general_lm_results)
+		"""
+		go_no2lm_results = {}
+		lm_results_2d_list = []
+		curs.execute("select go_no, intercept, coeff1, coeff2 from %s"%lm_table)
+		rows = curs.fetchall()
+		for row in rows:
+			go_no = row[0]
+			lm_results = row[1:]		#intercept coeff1 coeff2
+			go_no2lm_results[go_no] = lm_results
+			lm_results_2d_list.append(lm_results)
+		
+		return (go_no2lm_results, lm_results_2d_list)
+		
+	def get_general_lm_results(self, lm_results_2d_list):
+		"""
+		03-01-05
+			return (intercept, coeff1, coeff2) by averaging the known parameters
+		"""
+		no_of_models = len(lm_results_2d_list)
+		lm_results_array = array(lm_results_2d_list)
+		intercept = sum(lm_results_array[:,0])/no_of_models
+		coeff1 = sum(lm_results_array[:,1])/no_of_models
+		coeff2 = sum(lm_results_array[:,2])/no_of_models
+		
+		return (intercept, coeff1, coeff2)
+	
+	def return_p_value_cut_off(self, go_no, recurrence, connectivity):
+		"""
+		03-01-05
+			if go_no in self.go_no2lm_results, go and get it
+			otherwise, use the general linear model
+		"""
+		if go_no in self.go_no2lm_results:
+			lm_results = self.go_no2lm_results[go_no]
+			p_value_cut_off = lm_results[0] + recurrence*lm_results[1] + connectivity*lm_results[2]
+		else:
+			p_value_cut_off = self.general_lm_results[0] + recurrence*self.general_lm_results[1]+\
+				connectivity*self.general_lm_results[2]
+		return p_value_cut_off
+		
 	def run(self):
-		self.data_fetch()
-		self.overview_stats()
-		self.go_no_accuracy()
-		self.table_output()
+		"""
+		02-28-05
+		"""
+		self.init()
+		(conn, curs) = db_connect(self.hostname, self.dbname, self.schema)
+		self.known_genes_dict = self.get_known_genes_dict(curs)
+		if self.p_value_cut_off == 0:
+			if self.lm_table:
+				self.go_no2lm_results, lm_results_2d_list = self.get_go_no2lm_results(curs, self.lm_table)
+				self.general_lm_results = self.get_general_lm_results(lm_results_2d_list)
+			else:
+				sys.stderr.write("p_value_cut_off==0, need the lm_table to get the linear model\n")
+				sys.exit(127)
+		
+		self.data_fetch(curs, self.gene_table)
+		if self.stat_table_fname:
+			self.overview_stats(self.stat_table_f)
+			self.go_no_accuracy(self.prediction_pair2attr, self.stat_table_f, curs)
+			self.table_output(self.stat_table_f)
+		if self.gene_p_table:
+			self.gene_p_table_submit(curs, self.gene_p_table, self.gene_p_list)
+		if self.needcommit:
+			curs.execute("end")
 
-	def data_fetch(self):
+	def data_fetch(self, curs, gene_table):
 		"""
 		02-21-05
 			--_p_gene_analysis()
 		"""
-		self.curs.execute("DECLARE crs CURSOR FOR select gene_no, go_no, mcl_id, %s, avg_p_value, \
-			recurrence_cut_off,connectivity_cut_off, depth_cut_off from %s"\
-			%(self.is_correct_dict[self.judger_type], self.gene_table))
+		curs.execute("DECLARE crs CURSOR FOR select gene_no, go_no, mcl_id, %s, avg_p_value, \
+			recurrence_cut_off,connectivity_cut_off, depth_cut_off,p_gene_id from %s"\
+			%(self.is_correct_dict[self.judger_type], gene_table))
 		
-		self.curs.execute("fetch 5000 from crs")
-		rows = self.curs.fetchall()
+		curs.execute("fetch 5000 from crs")
+		rows = curs.fetchall()
 		while rows:
 			for row in rows:
 				self._p_gene_analysis(row)
 			if self.report:
 				sys.stderr.write('%s%s'%('\x08'*20, self.no_of_records))
 			
-			self.curs.execute("fetch 5000 from crs")
-			rows = self.curs.fetchall()
+			curs.execute("fetch 5000 from crs")
+			rows = curs.fetchall()
 
 	def _p_gene_analysis(self, row):
+		"""
+		02-28-05
+			setup two dictionaries, self.prediction_space2attr and self.prediction_pair2attr
+		03-01-05
+			use the hardcode p_value_cut_off or get it from linear_model
+			depend on whether self.p_value_cut_off==0
+		"""
 		gene_no = row[0]
 		go_no = row[1]
 		mcl_id = row[2]
@@ -165,9 +258,17 @@ class p_gene_analysis:
 		recurrence = row[5]
 		connectivity = row[6]
 		depth_cut_off = row[7]
+		p_gene_id = row[8]
 		
-		if p_value > self.p_value_cut_off:
+		if self.p_value_cut_off == 0:
+			p_value_cut_off = self.return_p_value_cut_off(go_no, recurrence, connectivity)
+		else:
+			p_value_cut_off = self.p_value_cut_off
+		
+		if p_value > p_value_cut_off:
 			return
+		else:
+			self.gene_p_list.append([p_gene_id, p_value_cut_off])
 		
 		prediction_space = (recurrence, connectivity)
 		prediction_pair = (gene_no, go_no)
@@ -202,7 +303,7 @@ class p_gene_analysis:
 		unit_pair.is_correct = is_correct
 		unit_pair.supporting_clusters += 1
 
-	def overview_stats(self):
+	def overview_stats(self, stat_table_f):
 		'''
 		02-21-05
 			get some global statistics
@@ -239,12 +340,12 @@ class p_gene_analysis:
 		no_of_unknown_genes = len(set2)
 		
 		#output
-		self.stat_table_f.writerow(['accuracy', total_accuracy])
-		self.stat_table_f.writerow(['known_predictions', total_known_predictions])
-		self.stat_table_f.writerow(['unknown_predictions', total_unknown_predictions])
-		self.stat_table_f.writerow(['accuracy_pair', total_accuracy_pair])
-		self.stat_table_f.writerow(['known_predictions-pair', total_known_predictions_pair])
-		self.stat_table_f.writerow(['unknown_predictions-pair', total_unknown_predictions_pair])
+		stat_table_f.writerow(['accuracy', total_accuracy])
+		stat_table_f.writerow(['known-predictions', total_known_predictions])
+		stat_table_f.writerow(['unknown-predictions', total_unknown_predictions])
+		stat_table_f.writerow(['accuracy-pair', total_accuracy_pair])
+		stat_table_f.writerow(['known-predictions-pair', total_known_predictions_pair])
+		stat_table_f.writerow(['unknown-predictions-pair', total_unknown_predictions_pair])
 		
 		self.stat_table_f.writerow(['known genes', no_of_known_genes])
 		self.stat_table_f.writerow(['unknown genes', no_of_unknown_genes])
@@ -261,57 +362,58 @@ class p_gene_analysis:
 		return (known_gene_set, unknown_gene_set)
 	
 
-	def go_no_accuracy(self):
+	def go_no_accuracy(self, prediction_pair2attr, stat_table_f, curs):
 		"""
 		this function computes the prediction accuracy for each go function
 		"""
-		for prediction_pair in self.prediction_pair2attr:
+		for prediction_pair in prediction_pair2attr:
 			go_no = prediction_pair[1]
-			is_correct = self.prediction_pair2attr[prediction_pair].is_correct
+			is_correct = prediction_pair2attr[prediction_pair].is_correct
 			if go_no not in self.go_no2accuracy:
 				self.go_no2accuracy[go_no] = accuracy_struc()
 			self.go_no2accuracy[go_no].correct += (is_correct+1)/2	#increment when is_correct=1
 			self.go_no2accuracy[go_no].known += int(is_correct>=0)	#increment when is_correct=0 or 1
 			self.go_no2accuracy[go_no].unknown += -(is_correct-1)/2	#increment when is_correct = -1
 		
+		"""
 		if self.needcommit:
 			#this table is easy for linking.
 			go_acc_table = '%s_acc_%s'%(self.gene_table, self.is_correct_dict[self.judger_type])
-			self.curs.execute("create table %s(\
+			curs.execute("create table %s(\
 				go_no	integer,\
 				accuracy	float,\
 				known	integer,\
 				unknown	integer\
 				)"%go_acc_table)
+		"""
 		
 		#finally, compute all the ratios and output
 		go_no_list = self.go_no2accuracy.keys()
 		go_no_list.sort()
-		self.stat_table_f.writerow([])
-		self.stat_table_f.writerow(['go_no', 'accuracy', 'known', 'unknown'])
+		stat_table_f.writerow([])
+		stat_table_f.writerow(['go_no', 'accuracy', 'known', 'unknown'])
 		for go_no in go_no_list:
 			unit = self.go_no2accuracy[go_no]
 			if unit.known !=0:
 				unit.ratio = unit.correct/unit.known
 			else:
 				unit.ratio = -1
-			self.stat_table_f.writerow([go_no, unit.ratio, unit.known, unit.unknown])
+			stat_table_f.writerow([go_no, unit.ratio, unit.known, unit.unknown])
+			"""
 			if self.needcommit:
-				self.curs.execute("insert into %s(go_no, accuracy, known, unknown)\
+				curs.execute("insert into %s(go_no, accuracy, known, unknown)\
 					values(%s, %f, %d, %d)"%(go_acc_table,\
 					go_no, unit.ratio, unit.known, unit.unknown))
-		
-		if self.needcommit:
-			self.curs.execute("end")
+			"""
 
-	def table_output(self):
+	def table_output(self, stat_table_f):
 		'''
 		
 		'''
 
 		header = ['recurrence', 'connectivity', 'accuracy', 'known_predictions', 'unknown_predictions',\
 		'accuracy_pair', 'known_predictions_pair', 'unknown_predictions_pair', 'known genes', 'unknown genes']
-		self.stat_table_f.writerow(header)
+		stat_table_f.writerow(header)
 		prediction_space_list = self.prediction_space2attr.keys()
 		prediction_space_list.sort()
 		for prediction_space in prediction_space_list:
@@ -332,9 +434,18 @@ class p_gene_analysis:
 			row = [recurrence, connectivity, accuracy, unit.known_predictions, unit.unknown_predictions,\
 				accuracy_pair, unit.known_predictions_pair, unit.unknown_predictions_pair, no_of_known_genes,\
 				no_of_unknown_genes]
-			self.stat_table_f.writerow(row)
+			stat_table_f.writerow(row)
 		
-
+	def gene_p_table_submit(self, curs, gene_p_table, gene_p_list):
+		sys.stderr.write("Submiting gene_p_table...")
+		curs.execute("create table %s(\
+			p_gene_id	integer,\
+			p_value_cut_off	float)"%gene_p_table)
+			
+		for (p_gene_id, p_value_cut_off) in gene_p_list:
+			curs.execute("insert into %s(p_gene_id, p_value_cut_off)\
+					values(%d, %f)"%(gene_p_table, p_gene_id, p_value_cut_off))
+		sys.stderr.write("done.\n")
 
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
@@ -342,9 +453,10 @@ if __name__ == '__main__':
 		sys.exit(2)
 	
 	long_options_list = ["help", "hostname=", "dbname=", "schema=", "table=", "mcl_table=", "p_value_cut_off=",\
-		"judger_type=", "report", "commit", "gene_table=", "debug", "accuracy_cut_off="]
+		"judger_type=", "report", "commit", "gene_table=", "lm_table=", "debug", "accuracy_cut_off=",\
+		"gene_p_table="]
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:t:m:p:j:rcg:ba:", long_options_list)
+		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:t:m:p:j:rcg:l:ba:n:", long_options_list)
 	except:
 		print __doc__
 		sys.exit(2)
@@ -359,9 +471,11 @@ if __name__ == '__main__':
 	report = 0
 	commit = 0
 	gene_table = 'p_gene'
+	lm_table = None
 	debug = 0
 	accuracy_cut_off = 0
 	stat_table_fname = None
+	gene_p_table = None
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
 			print __doc__
@@ -386,17 +500,21 @@ if __name__ == '__main__':
 			commit = 1
 		elif opt in ("-g", "--gene_table"):
 			gene_table = arg
+		elif opt in ("-l", "--lm_table"):
+			lm_table = arg
 		elif opt in ("-b", "--debug"):
 			debug = 1
 		elif opt in ("-a", "--accuracy_cut_off="):
 			accuracy_cut_off = float(arg)
+		elif opt in ("-n", "--gene_p_table="):
+			gene_p_table = arg
 	if len(args) == 1:
 		stat_table_fname = args[0]
 			
-	if schema and p_value_cut_off and stat_table_fname:
+	if schema and p_value_cut_off!=None and stat_table_fname:
 		instance = p_gene_analysis(hostname, dbname, schema, table, mcl_table, p_value_cut_off,\
-			report, judger_type, commit, gene_table, stat_table_fname, debug, accuracy_cut_off)
-		instance.dstruc_loadin()
+			report, judger_type, commit, gene_table, lm_table, stat_table_fname, debug, \
+			accuracy_cut_off, gene_p_table)
 		instance.run()
 	else:
 		print __doc__
