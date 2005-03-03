b02bba84d0aeda104d4a38bf9e992a77a6823532
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-03-03 10:58:48 +0000

    03-02-05 	submit cor_vector + sig_vector
    git-svn-id: file:///usr/local/svn/annot@423 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/codense/haiyan_cor_vector2db.py
+++ trunk/bin/codense/haiyan_cor_vector2db.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 """
-Usage: haiyan_cor_vector2db.py -k SCHEMA -p MAPPING_FILE INPUT_FILE OUTPUT_FILE
+Usage: haiyan_cor_vector2db.py -k SCHEMA -p MAPPING_FILE -i COR_VECTOR_FILE -s SIGNIFICANCE_FILE
 
 Option:
-	INPUT_FILE is the file containing all the clusters(Haiyan's format).
-	OUTPUT_FILE is the file to contain all the clusters in Kangyu's format.
 	-z ..., --hostname=...	the hostname, zhoudb(default)
 	-d ..., --dbname=...	the database name, graphdb(default)
 	-k ..., --schema=...	which schema in the database
 	-t ..., --table=...	the splat_result table (edge_set)
 	-p ..., --mapping_file=...	the file to get the mapping between haiyan's index and my gene_no
+	-i ..., --input_file=...	edge correlation vector file, (one input of codense and copath)
+	-s ..., --significance_file=...	the edge significance flag vector file,
 	-c, --commit	commit this database transaction
 	
 Examples:
-	./script/annot/bin/haiyan_cor_vector2db.py -c -k sc_54  -p sc_54_gene_id2no /tmp/sv_f.txt /tmp/cor_vector
+	./script/annot/bin/haiyan_cor_vector2db.py -c -k sc_54  -p sc_54_gene_id2no 
+		-i /tmp/sv_f.txt -s /tmp/sig_vector
 
 Description:
 	Submit Haiyan's cor_vector file into a database table.
@@ -26,7 +27,11 @@ from common import *
 
 	
 class haiyan_cor_vector2db:
-	def __init__(self, hostname, dbname, schema, table, mapping_file, needcommit, infname, outfname):
+	"""
+	03-02-05
+		submit cor_vector + sig_vector
+	"""
+	def __init__(self, hostname, dbname, schema, table, mapping_file, needcommit, infname, significance_file):
 		self.conn = psycopg.connect('host=%s dbname=%s'%(hostname, dbname))
 		self.curs = self.conn.cursor()
 		self.curs.execute("set search_path to %s"%schema)
@@ -34,12 +39,14 @@ class haiyan_cor_vector2db:
 		self.mapping_file = mapping_file
 		self.needcommit = int(needcommit)
 		self.inf = csv.reader(open(infname, 'r'), delimiter='\t')
-		self.outf = csv.writer(open(outfname, 'w'), delimiter = '\t')
+		self.sig_f = csv.reader(open(significance_file, 'r'), delimiter='\t')
 	
 	def run(self):
 		#setup the self.haiyan_no2gene_no	
 		self.haiyan_no2gene_no = get_haiyan_no2gene_no(self.mapping_file)
 		for row in self.inf:
+			#get a row from the significance_file
+			sig_row = self.sig_f.next()
 			edge = row[:2]
 			edge = map(int, edge)
 			edge = dict_map(self.haiyan_no2gene_no, edge)
@@ -48,13 +55,14 @@ class haiyan_cor_vector2db:
 			cor_vector = row[2:]
 			cor_vector = map(divided_by_1000, cor_vector)
 			cor_vector = '{' + ','.join(cor_vector) + '}'
-			new_row = [edge, cor_vector]
-			#self.outf.writerow(new_row)
+			sig_vector = sig_row[2:]
+			sig_vector = '{' + ','.join(sig_vector) + '}'
+			
 			try:
 				#inserting into the splat_table
-				self.curs.execute("insert into %s(edge_name, cor_vector) \
-							values ('%s', '%s')"%\
-							(self.table, edge, cor_vector))
+				self.curs.execute("insert into %s(edge_name, cor_vector, sig_vector) \
+							values ('%s', '%s', '%s')"%\
+							(self.table, edge, cor_vector, sig_vector))
 			except:
 				sys.stderr.write('Error occurred when inserting pattern. Aborted.\n')
 				self.conn.rollback()
@@ -68,10 +76,11 @@ if __name__ == '__main__':
 		print __doc__
 		sys.exit(2)
 	
-	long_options_list = ["help", "hostname=", "dbname=", "schema=", "table=", "mapping_file=", "commit"]
+	long_options_list = ["help", "hostname=", "dbname=", "schema=", "table=", "mapping_file=", \
+		"input_file=", "significance_file=", "commit"]
 	
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:t:p:c", long_options_list)
+		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:t:p:i:s:c", long_options_list)
 	except:
 		print __doc__
 		sys.exit(2)
@@ -82,6 +91,8 @@ if __name__ == '__main__':
 	commit = 0
 	table = 'edge_cor_vector'
 	mapping_file = None
+	input_file = None
+	significance_file = None
 	
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
@@ -97,11 +108,15 @@ if __name__ == '__main__':
 			table = arg
 		elif opt in ("-p", "--mapping_file"):
 			mapping_file = arg
+		elif opt in ("-i", "--input_file"):
+			input_file = arg
+		elif opt in ("-s", "--significance_file"):
+			significance_file = arg
 		elif opt in ("-c", "--commit"):
 			commit = 1
 
-	if schema and mapping_file and len(args) == 2:
-		instance = haiyan_cor_vector2db(hostname, dbname, schema, table, mapping_file, commit, args[0], args[1])
+	if schema and mapping_file and input_file and significance_file:
+		instance = haiyan_cor_vector2db(hostname, dbname, schema, table, mapping_file, commit, input_file, significance_file)
 		instance.run()
 	else:
 		print __doc__
