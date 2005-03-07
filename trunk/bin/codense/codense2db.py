0bc5a3c4934f683c32cf335e11f9ce24a179aa52
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-03-07 01:59:21 +0000

    03-03-05 	get recurrence_array by passing the combined_sig_vetor 	get a fair recurrence_array(see 'redefine recurrence and connectivity' in log_05) 	the connectivity of the summary subgraph is passed to splat_connectivity
    	return (combined_cor_vector, combined_sig_vetor)
    
    	replace the combined_vector with combined_sig_vetor
    	get a fair recurrence_array(see 'redefine recurrence and connectivity' in log_05)
    
    	parsing the combined_sig_vetor and return connectivity
    
    	parser for codense results
    
    
    git-svn-id: file:///usr/local/svn/annot@440 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/codense/codense2db.py
+++ trunk/bin/codense/codense2db.py
@@ -11,8 +11,9 @@ Option:
 	-m ..., --mcl_table=...	the mcl_result table (vertex_set)
 	-p ..., --mapping_file=...	the file to get the mapping between haiyan's index and my gene_no
 		(02-19-05)if not given, regard the input graph uses gene_no instead of haiyan's index
-	-o ..., --cor_cut_off=...	the cor_cut_off for an edge to be valid, 0.6(default)
+	-o ..., --cor_cut_off=...	the cor_cut_off for an edge to be valid, 0(default)
 		NOTICE: 0 means the binary conversion won't be used, just summing the floats.
+	-y ..., --parser_type=...	the type of parser to use, 1(copath, default), 2(codense)
 	-c, --commit	commit this database transaction
 	-r, --report	report the progress(a number)
 	-h, --help	show this help
@@ -44,15 +45,22 @@ class cluster_dstructure:
 		self.edge_set_string = None
 		self.no_of_edges = None
 		self.recurrence_array = None
-		self.connectivity = None
+		self.splat_connectivity = None	#03-03-05 this is the connectivity of the summary subgraph
+		self.connectivity = None	#03-03-05 this is by averaging connectivities of the summary subgraph in each individual dataset
 
 		
 class codense2db:
 	'''
+	run()
+		--init
+		--get_haiyan_no2gene_no
+		--create_tables
+		--parser_dict[parser_type]
+		--db_submit
 	'''
-	def __init__(self, infname=None, hostname=None, dbname=None, schema=None, table=None,\
-			mcl_table=None, mapping_file=None, cor_cut_off=0.6,\
-			report=0, needcommit=0):
+	def __init__(self, infname=None, hostname='zhoudb', dbname='graphdb', schema=None, table=None,\
+			mcl_table=None, mapping_file=None, cor_cut_off=0,\
+			parser_type=1, needcommit=0, report=0):
 		"""
 		02-25-05
 			modify the interface of the class and 2 member functions(get_combined_cor_vector, parse_recurrence)
@@ -66,24 +74,36 @@ class codense2db:
 		self.mcl_table = mcl_table
 		self.mapping_file = mapping_file
 		self.cor_cut_off = float(cor_cut_off)
+		self.parser_type = int(parser_type)
+		self.needcommit = int(needcommit)
 		self.report = int(report)
-		self.needcommit = int(needcommit)		
-		self.haiyan_no2gene_no = {}
 	
-	def init(self):
-		self.inf = csv.reader(open(self.infname, 'r'), delimiter='\t')
-		self.conn = psycopg.connect('host=%s dbname=%s'%(self.hostname, self.dbname))
-		self.curs = self.conn.cursor()
-		self.curs.execute("set search_path to %s"%self.schema)
+		self.debug = 0
+		self.parser_dict = {1:self.copath_parser,
+			2: self.codense_parser}
+
 	
-	def parse(self, row):
+	def copath_parser(self, row, argument=None, argument2=None):
+		"""
+		03-03-05
+			get recurrence_array by passing the combined_sig_vetor
+			get a fair recurrence_array(see 'redefine recurrence and connectivity' in log_05)
+			
+			the connectivity of the summary subgraph is passed to splat_connectivity
+			
+			--get_combined_cor_vector
+			--parse_recurrence
+			--parse_connectivity
+		"""
+		haiyan_no2gene_no = argument
+		curs = argument2
 		cluster = cluster_dstructure()
 		cluster.cluster_id = int(row[0])
-		cluster.connectivity = float(row[1])
+		cluster.splat_connectivity = float(row[1])
 		cluster.vertex_set = row[2][1:-2].split(';')
 		cluster.vertex_set = map(int, cluster.vertex_set)
 		if self.mapping_file != None:
-			cluster.vertex_set = dict_map(self.haiyan_no2gene_no, cluster.vertex_set)
+			cluster.vertex_set = dict_map(haiyan_no2gene_no, cluster.vertex_set)
 		#in ascending order
 		cluster.vertex_set.sort()
 		#cluster.vertex_set_string = row[2][1:-2].replace(';',',')
@@ -96,21 +116,28 @@ class codense2db:
 			edge = edge.split(',')
 			edge = map(int, edge)
 			if self.mapping_file!=None:
-				edge = dict_map(self.haiyan_no2gene_no, edge)
+				edge = dict_map(haiyan_no2gene_no, edge)
 			#in ascending order
 			edge.sort()
 			cluster.edge_set.append(edge)
 		cluster.no_of_edges = len(cluster.edge_set)
-		combined_cor_vector = self.get_combined_cor_vector(self.curs, cluster.edge_set)
-		cluster.recurrence_array = self.parse_recurrence(combined_cor_vector, cluster.no_of_edges, self.cor_cut_off)
+		(combined_cor_vector, combined_sig_vetor) = self.get_combined_cor_vector(curs, cluster.edge_set)
+		cluster.connectivity = self.parse_connectivity(combined_sig_vetor, cluster.no_of_edges, len(cluster.vertex_set))
+		
+		cluster.recurrence_array = self.parse_recurrence(combined_sig_vetor, cluster.no_of_edges, self.cor_cut_off)
 		
 		return cluster
 	
 	def get_combined_cor_vector(self, curs, edge_set):
+		"""
+		03-03-05
+			return (combined_cor_vector, combined_sig_vetor)
+		"""
 		combined_cor_vector = []
+		combined_sig_vetor = []
 		for edge in edge_set:
 			edge_string = '{' + repr(edge)[1:-1] + '}'
-			curs.execute("select cor_vector from edge_cor_vector where edge_name='%s'"%edge_string)
+			curs.execute("select cor_vector, sig_vector from edge_cor_vector where edge_name='%s'"%edge_string)
 			rows = curs.fetchall()
 			if len(rows) == 0:
 				sys.stderr.write('%s not found in edge_cor_vector\n'%edge_string)
@@ -118,10 +145,19 @@ class codense2db:
 			cor_vector = rows[0][0][1:-1].split(',')
 			cor_vector = map(float, cor_vector)
 			combined_cor_vector += cor_vector
-		return combined_cor_vector
+			
+			sig_vector = rows[0][1][1:-1].split(',')
+			sig_vector = map(int, sig_vector)
+			combined_sig_vetor += sig_vector			
+		return (combined_cor_vector, combined_sig_vetor)
 
-	def parse_recurrence(self, combined_cor_vector, no_of_edges, cor_cut_off):
-		cor_array = numarray.array(combined_cor_vector)
+	def parse_recurrence(self, combined_vector, no_of_edges, cor_cut_off=0):
+		"""
+		03-03-05
+			replace the combined_vector with combined_sig_vetor
+			get a fair recurrence_array(see 'redefine recurrence and connectivity' in log_05)
+		"""
+		cor_array = numarray.array(combined_vector)
 		y_dimension = len(cor_array)/no_of_edges
 		cor_array = numarray.reshape(cor_array, (no_of_edges, y_dimension))
 		recurrence_array = []
@@ -132,26 +168,73 @@ class codense2db:
 			else:
 				edge_cor_in_one_dataset = cor_array[:,i]
 			recurrence_array.append(sum(edge_cor_in_one_dataset)/float(no_of_edges))
+			if self.debug:
+				print edge_cor_in_one_dataset
+				print recurrence_array
 		#handle this in gene_stat_plot.py
 		#recurrence_array = numarray.greater_equal(recurrence_array, self.subgraph_cut_off)
 		return recurrence_array
 	
-	def create_tables(self):
+	def parse_connectivity(self, combined_vector, no_of_edges, no_of_nodes):
+		"""
+		03-03-05
+			parsing the combined_sig_vetor and return connectivity
+		"""
+		sig_array = numarray.array(combined_vector)
+		y_dimension = len(sig_array)/no_of_edges
+		sig_array = numarray.reshape(sig_array, (no_of_edges, y_dimension))
+		connectivity_list = []
+		for i in range(y_dimension):
+			no_of_edges_in_one_dataset = sum(sig_array[:,i])
+			if self.debug:
+				print sig_array[:,i]
+				
+			connectivity_list.append(2.0*no_of_edges_in_one_dataset/(no_of_nodes*(no_of_nodes-1)) )
+		if self.debug:
+			print connectivity_list
+		return sum(connectivity_list)/y_dimension
+		
+	
+	def codense_parser(self, row, argument=None, argument2=None):
+		"""
+		03-04-05
+			parser for codense results
+		"""
+		gene_id2gene_no = argument
+		cluster = cluster_dstructure()
+		cluster.cluster_id = int(row[0])
+		no_of_nodes = int(row[1])
+		no_of_edges = int(row[2])
+		cluster.splat_connectivity = float(row[3])	#row[3] should be equal to the below one got from no_of_edges and no_of_nodes
+		cluster.connectivity = 2*float(no_of_edges)/(no_of_nodes*(no_of_nodes-1))
+		cluster.no_of_edges = no_of_edges
+		cluster.vertex_set = []
+		for i in range(4,len(row), 2):
+			if row[i]:
+				#to avoid the last blank field
+				cluster.vertex_set.append(row[i])
+		cluster.vertex_set = dict_map(gene_id2gene_no, cluster.vertex_set)
+		cluster.edge_set = [[1,1]]	#dummy edge
+		cluster.recurrence_array = [1]	#dummy recurrence
+		
+		return cluster
+	
+	def create_tables(self, curs, table, mcl_table):
 		#create tables if necessary
-		if self.table != 'splat_result':
+		if table != 'splat_result':
 			try:
-				self.curs.execute("create table %s(\
+				curs.execute("create table %s(\
 					splat_id		serial primary key,\
 					no_of_edges	integer,\
 					recurrence_pattern	bit varying(200),\
 					recurrence_array	float[],\
 					edge_set	integer[][],\
-					connectivity	float)"%self.table)
+					connectivity	float)"%table)
 			except:
-				sys.stderr.write("Error occurred when creating table %s\n"%self.table)
-		if self.mcl_table != 'mcl_result':
+				sys.stderr.write("Error occurred when creating table %s\n"%table)
+		if mcl_table != 'mcl_result':
 			try:
-				self.curs.execute("create table %s(\
+				curs.execute("create table %s(\
 					mcl_id	serial primary key,\
 					splat_id	integer,\
 					vertex_set	integer[],\
@@ -160,21 +243,25 @@ class codense2db:
 					p_value_min	float,\
 					go_no_vector	integer[],\
 					unknown_gene_ratio	float,\
-					recurrence_array	float[])"%self.mcl_table)
+					recurrence_array	float[])"%mcl_table)
 			except:
-				sys.stderr.write("Error occurred when creating table %s\n"%self.mcl_table)	
+				sys.stderr.write("Error occurred when creating table %s\n"%mcl_table)	
 
-	def db_submit(self, cluster):
+	def db_submit(self, curs, cluster, table, mcl_table):
+		"""
+		03-03-05
+			splat table's connectivity is the splat_connectivity, see doc above
+		"""
 		try:
 			#inserting into the splat_table
-			self.curs.execute("insert into %s(splat_id, no_of_edges, \
+			curs.execute("insert into %s(splat_id, no_of_edges, \
 						recurrence_array, edge_set, connectivity) values (%d, %d, ARRAY%s, ARRAY%s, %f)"%\
-						(self.table, cluster.cluster_id, cluster.no_of_edges, repr(cluster.recurrence_array),\
-						repr(cluster.edge_set), cluster.connectivity))
+						(table, cluster.cluster_id, cluster.no_of_edges, repr(cluster.recurrence_array),\
+						repr(cluster.edge_set), cluster.splat_connectivity))
 			#inserting into the mcl_table
-			self.curs.execute("insert into %s(mcl_id, splat_id, vertex_set, connectivity, recurrence_array)\
+			curs.execute("insert into %s(mcl_id, splat_id, vertex_set, connectivity, recurrence_array)\
 							values (%d, %d, ARRAY%s, %f, ARRAY%s)"%\
-							(self.mcl_table, cluster.cluster_id, cluster.cluster_id, repr(cluster.vertex_set),\
+							(mcl_table, cluster.cluster_id, cluster.cluster_id, repr(cluster.vertex_set),\
 							cluster.connectivity, repr(cluster.recurrence_array)) )
 		except:
 			sys.stderr.write('Error occurred when inserting pattern. Aborted.\n')
@@ -183,25 +270,30 @@ class codense2db:
 
 	def run(self):
 		
-		#conn, curs, inf ... initialization
-		self.init()
+
+		inf = csv.reader(open(self.infname, 'r'), delimiter='\t')
+		(conn, curs) = db_connect(self.hostname, self.dbname, self.schema)
 		
-		#setup the self.haiyan_no2gene_no
+		#setup the haiyan_no2gene_no
 		if self.mapping_file != None:
-			self.haiyan_no2gene_no = get_haiyan_no2gene_no(self.mapping_file)
-		
-		self.create_tables()
+			haiyan_no2gene_no = get_haiyan_no2gene_no(self.mapping_file)
+		else:
+			haiyan_no2gene_no = {}	#a blank dictionary, 
+		gene_id2gene_no = get_gene_id2gene_no(curs)
+		mapping_dict = {1:haiyan_no2gene_no,
+			2:gene_id2gene_no}
+		self.create_tables(curs, self.table, self.mcl_table)
 		no = 0
-		for row in self.inf:
-			cluster = self.parse(row)
-			self.db_submit(cluster)			
+		for row in inf:
+			cluster = self.parser_dict[self.parser_type](row, mapping_dict[self.parser_type], curs)
+			self.db_submit(curs, cluster, self.table, self.mcl_table)
 			no+=1
 			if self.report and no%1000==0:
 				sys.stderr.write('%s%d'%('\x08'*20, no))
 		if self.report:
 			sys.stderr.write('%s%d'%('\x08'*20, no))
 		if self.needcommit:
-			self.conn.commit()
+			conn.commit()
 		sys.stderr.write('\n\tTotal patterns: %d\n'%no)
 
 if __name__ == '__main__':
@@ -210,9 +302,9 @@ if __name__ == '__main__':
 		sys.exit(2)
 		
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:t:m:p:o:cr", ["help", "hostname=", \
+		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:t:m:p:o:y:cr", ["help", "hostname=", \
 			"dbname=", "schema=", "table=", "mcl_table=", "mapping_file=", "cor_cut_off=",\
-			"commit", "report"])
+			"parser_type=", "commit", "report"])
 	except:
 		print __doc__
 		sys.exit(2)
@@ -223,7 +315,8 @@ if __name__ == '__main__':
 	table = 'splat_result'
 	mcl_table = 'mcl_result'
 	mapping_file = None
-	cor_cut_off = 0.6
+	cor_cut_off = 0
+	parser_type = 1
 	commit = 0
 	report = 0
 	for opt, arg in opts:
@@ -244,13 +337,15 @@ if __name__ == '__main__':
 			mapping_file = arg
 		elif opt in ("-o", "--cor_cut_off"):
 			cor_cut_off = float(arg)
+		elif opt in ("-y", "--parser_type"):
+			parser_type = int(arg)
 		elif opt in ("-c", "--commit"):
 			commit = 1
 		elif opt in ("-r", "--report"):
 			report = 1
 	if schema and len(args)==1:
 		instance = codense2db(args[0], hostname, dbname, schema, table, mcl_table, mapping_file, cor_cut_off,\
-			report, commit)
+			parser_type, commit, report)
 		instance.run()
 	else:
 		print __doc__
