1afc48d97a4537d692b3da1b267412d60e42d8b3
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-04-14 08:21:56 +0000

    output the matrix read in by the class and other modifications along with the change in biclustering.cc.
    git-svn-id: file:///usr/local/svn/annot@525 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/graph/test_biclustering.py
+++ trunk/bin/graph/test_biclustering.py
@@ -27,12 +27,29 @@ for row in reader:
 		ls.append(row[1:])
 	else:
 		ls.append(row[1:-1])
-ls[0]
-ls[432]
-len(ls)
 instance.data_read_in(ls)
 ls1 = instance.return_matrix_data()
+
+#output the matrix read in by the class
+writer = csv.writer(open('/tmp/test_biclustering.out', 'w'), delimiter='\t')
+for row in ls1:
+	writer.writerow(row)
+del writer
+
+print "the first row is "
+print ls1[0]
+print "the penultimate row is "
+print ls1[-2]
+print "the final row is "
+print ls1[-1]
+print "total number of rows is "
+print len(ls)
 result = instance.getbicluster()
-result = instance.getbicluster()
-result = instance.getbicluster()
-print result
+while result:
+	print "Score is %s"%result.score
+	print "%s rows and their indices are %s"%(len(result.row_index_list), repr(result.row_index_list))
+	print "%s columns and their indices are %s"%(len(result.column_index_list), repr(result.column_index_list))
+	y = raw_input("Continue:(Y/n)?")
+	if y=="n":
+		break
+	result = instance.getbicluster()
