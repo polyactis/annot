823316b7c285f3aa1d0f440915ca990eedf8951d
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-04-14 08:14:35 +0000

    04-14-05 struct bicluster: 	data structure to hold the information getbicluster(): 	return structure bicluster, not tuple. data_read_in(): 	tricks related to the data type coersion.
    git-svn-id: file:///usr/local/svn/annot@523 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/graph/biclustering.cc
+++ trunk/bin/graph/biclustering.cc
@@ -17,6 +17,14 @@ typedef vector<double> vd;
 typedef vector<bool> vb;
 typedef vector<int> vi;
 
+//data structure to hold the information
+struct bicluster
+{
+	boost::python::list row_index_list;
+	boost::python::list column_index_list;
+	double score;
+};
+
 class biclustering
 {
 	public:
@@ -25,7 +33,7 @@ class biclustering
 		
 		void data_read_in(boost::python::list list_2d);
 		void scoring();
-		boost::python::tuple getbicluster();
+		bicluster getbicluster();
 		boost::python::list return_matrix_data();
 	
 		int numberOfColumns;
@@ -67,7 +75,7 @@ biclustering::biclustering(int mxScore, int mnHeight, int mnWidth, int bThreshol
 biclustering::~biclustering()
 {
 	//do nothing
-	std::cout<<"End"<<endl;
+	std::cout<<"Biclustering c++ exits"<<endl;
 }
 
 void biclustering::data_read_in(boost::python::list list_2d)
@@ -95,7 +103,14 @@ void biclustering::data_read_in(boost::python::list list_2d)
 			if (value_string=="NA")
 				value = int(gsl_rng_uniform_int(r, 1600)-800);
 			else
-				value = int(atof(value_string.c_str())*100);
+			{
+				float f_value = atof(value_string.c_str())*100;
+				value = int(f_value);	//see log on 04-14-05 in section of c++/c. tricks in the data type coersion.
+				#if defined(DEBUG)
+					std::cout << "Original: "<<value_string<<"Half: "<<f_value<<" New: "<<value<<endl;
+				#endif
+			}
+			
 			row_vector.push_back(value);
 		}
 		matrix.push_back(row_vector);
@@ -168,8 +183,13 @@ void biclustering::scoring()
 			columnScore[j] /= smHeight;
 }
 
-boost::python::tuple biclustering::getbicluster()
+bicluster biclustering::getbicluster()
+/*
+*04-13-05
+*	return structure bicluster, not tuple.
+*/
 {
+	bicluster bicluster_to_return;
 	for (int i = 0; i < numberOfRows; i++)
 		remainingR[i] = true;
 	for (int j = 0; j < numberOfColumns; j++)
@@ -243,7 +263,10 @@ boost::python::tuple biclustering::getbicluster()
 		if(remainingC[j])
 			column_index_list.append(j);
 
-	return make_tuple(row_index_list, column_index_list);
+	bicluster_to_return.row_index_list = row_index_list;
+	bicluster_to_return.column_index_list = column_index_list;
+	bicluster_to_return.score = HScore;
+	return bicluster_to_return;
 }
 
 boost::python::list biclustering::return_matrix_data()
@@ -272,5 +295,10 @@ BOOST_PYTHON_MODULE(biclustering)
 		.def_readonly("minHeight", &biclustering::minHeight)
 		.def_readonly("minWidth", &biclustering::minWidth)
 		.def_readonly("batchThreshold", &biclustering::batchThreshold)
+	;
+	boost::python::class_<bicluster>("bicluster")
+		.def_readonly("row_index_list", &bicluster::row_index_list)
+		.def_readonly("column_index_list", &bicluster::column_index_list)
+		.def_readonly("score", &bicluster::score)
 	;
 }
