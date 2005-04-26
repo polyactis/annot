9d64a80d2a717d3a917212a1898c878f58a59091
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-04-26 22:22:21 +0000

    04-26-05 	procedures to check output examples of MpiBiclustering.py
    git-svn-id: file:///usr/local/svn/annot@555 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/graph/test_biclustering.py
+++ trunk/bin/graph/test_biclustering.py
@@ -65,3 +65,59 @@ while result:
 	if y=="n":
 		break
 	result = instance.getbicluster()
+
+"""
+04-26-05
+	procedures to check output examples of MpiBiclustering.py
+#########check the biclustering score (Cheng2000), first through python
+
+from graph import biclustering
+from codense.common import get_edge_vector_by_id, db_connect
+conn,curs = db_connect("zhoudb","graphdb","sc_54_6661")
+import Numeric
+import MLab
+edge_id_list = [3450, 4360, 4412, 4602, 4691]
+col_index_list =   [0, 4, 30, 34, 35, 38]
+
+cor_2d_list, sig_2d_list = get_edge_vector_by_id(curs, edge_id_list)
+cor_2d_array = Numeric.array(cor_2d_list)
+
+
+
+seed_array = Numeric.take(cor_2d_array, col_index_list,1)
+colMean = MLab.mean(seed_array)
+rowMean = MLab.mean(seed_array,1)
+mean = sum(sum(seed_array))/(seed_array.shape[0]*seed_array.shape[1])
+score_array = MLab.transpose(seed_array-colMean)-rowMean +mean
+
+Hscore = 0
+for i in range(seed_array.shape[0]):
+	for j in range(seed_array.shape[1]):
+		r = seed_array[i,j]-rowMean[i]-colMean[j]+mean
+		r = r*r
+		Hscore += r
+
+print Hscore/(seed_array.shape[0]*seed_array.shape[1])
+
+##########check the extended candidate edges
+
+cand_id_list =  [757, 760, 1110, 1651, 3278, 3641, 3730, 4310]
+cor_2d_list, sig_2d_list = get_edge_vector_by_id(curs, edge_id_list)
+cor_2d_array = Numeric.array(cor_2d_list)
+cand_array = Numeric.take(cor_2d_array, col_index_list,1)
+
+from rpy import r
+
+r.dist(r.rbind(can_array[0,:],colMean))
+r.cor(can_array[0,:],colMean)
+
+##########check the c++ module
+
+instance = biclustering.biclustering(0.01,5,6,100)
+
+biclustering.set_module_and_type('Numeric', 'ArrayType')
+instance.data_read_in(seed_array)
+ls1 = instance.return_matrix_data()
+result = instance.getbicluster()
+
+"""
