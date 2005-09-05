29d36ea979b0cd69771c30962a75d2ba7382ba20
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-09-05 21:45:25 +0000

    A program to test the ClusterByEBC algorithm from cc_from_edge_list.cc
    git-svn-id: file:///usr/local/svn/annot@705 4ebff559-900f-0410-a220-daa186eb8a63

---
+++ trunk/bin/graph/ClusterByEBC_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+Usage:	ClusterByEBC_test.py size_cutoff connectivity_cutoff
+
+A program to test the ClusterByEBC algorithm from cc_from_edge_list.cc
+
+"""
+
+
+import sys,os
+
+if len(sys.argv)==3:
+	from cc_from_edge_list import ClusterByEBC
+	cf_instance =ClusterByEBC()
+	edge_list=[(0, 5), (0, 1), (0, 6), (1, 2), (1, 3), (1, 4), (2, 3), (4, 5), (6, 8), (6, 7), (7, 8)]
+	
+	cf_instance.run(edge_list,int(sys.argv[1]),float(sys.argv[2]))
+	print "final clusters is"
+	print cf_instance.cc_list
+else:
+	print __doc__
+	sys.exit(2)
