4b9250abbd6e85fd766ae2d9063d70d007e14dd3
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-09-05 20:58:09 +0000

    ClusterByEBC: a clustering algorithm based  on edge betweenness_centrality
    git-svn-id: file:///usr/local/svn/annot@704 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/graph/cc_from_edge_list.cc
+++ trunk/bin/graph/cc_from_edge_list.cc
@@ -92,6 +92,85 @@ boost::python::list cc_from_edge_list::subgraph2list(Graph &subgraph, Graph &gra
 	return subgraph_edge_list;
 }
 
+std::vector<Graph> cc_from_edge_list::subgraph_components(Graph &subgraph, Graph &graph, std::vector<int> component, int no_of_components)
+/*
+*09-05-05
+*	copied from clustering.cc, dynamic .so can't find the definition of the function by just including the header file
+*/
+{
+	int global_index;
+	//initialize the vector_subgraph with the number of components
+	std::vector<Graph> vector_subgraph(no_of_components);
+	for(int i=0;i<no_of_components;i++)
+		vector_subgraph[i] = graph.create_subgraph();
+	//two vertex_descriptor
+	boost::graph_traits<Graph>::vertex_descriptor
+	vertex_local, vertex_global;
+	//the vertex_index_global map is used to translate the global descriptor to the global index.
+	boost::property_map<Graph, vertex_index_t>::type
+	vertex_index_global;
+	vertex_index_global = get(vertex_index, graph);
+	for(int i=0; i<component.size(); i++)
+	{
+		int component_no = component[i];
+		//i is the local index, get a descriptor from it
+		vertex_local = vertex(i, subgraph);
+		//find the global descriptor
+		vertex_global = subgraph.local_to_global(vertex_local);
+		//get the global index and add it to the subgraph
+		global_index = get(vertex_index_global, vertex_global);
+		#if defined(DEBUG)
+			std::cerr<<global_index<<" goes to component "<<component_no<<std::endl;
+		#endif
+		add_vertex(global_index, vector_subgraph[component_no]);
+	}
+	
+	return vector_subgraph;
+}
+
+std::vector<Graph> cc_from_edge_list::graph_components(Graph &graph, std::vector<int> component, int no_of_components)
+/*
+*09-05-05
+*	create component-graph-vector from a graph
+*/
+{
+	vertex_name_type global_vertex2name = get(vertex_name, graph);
+	std::map<int, vertexDescriptor> vertex_int2comp_graph_descriptor;	//to store the global vertex_index to local vertex_index
+	std::vector<Graph> vector_graph(no_of_components);
+	for(int i=0;i<no_of_components;i++)
+	{
+		Graph tmp_g;
+		vector_graph[i] = tmp_g;
+	}
+	vertexDescriptor u, vertex1, vertex2;
+	for(int i=0; i<component.size(); i++)
+	{
+		int component_no = component[i];
+		u = add_vertex(vector_graph[component_no]);
+		//preserve the name mapping
+		vertex_name_type local_vertex2name = get(vertex_name, vector_graph[component_no]);
+		local_vertex2name[u] = get(global_vertex2name, vertex(i,graph));
+		//global to local vertex_index mapping
+		vertex_int2comp_graph_descriptor[i] = u;
+	}
+	
+	graph_traits<Graph>::edge_iterator ei, ei_end;
+	for (tie(ei,ei_end) = edges(graph); ei != ei_end; ++ei)
+	{
+		vertex1 = source(*ei, graph);
+		vertex2 = target(*ei, graph);
+		int component_no = component[vertex1];
+		#ifdef DEBUG
+			int component_no2 = component[vertex2];
+			if (component_no!=component_no2)
+				std::cerr<<"Error: two vertices of one edge from two different component"<<std::endl;
+		#endif
+		add_edge(vertex_int2comp_graph_descriptor[vertex1], vertex_int2comp_graph_descriptor[vertex2], vector_graph[component_no]);
+	}
+	return vector_graph;
+}
+
+
 void cc_from_edge_list::run(boost::python::list edge_list)
 {
 	init_graph_from_edge_list(edge_list, g);
@@ -108,11 +187,163 @@ void cc_from_edge_list::run(boost::python::list edge_list)
 	}
 }
 
+/*
+*09-04-05
+*	a clustering algorithm based  on edge betweenness_centrality
+*
+*/
+
+
+void ClusterByEBC::cut_by_betweenness_centrality(Graph &graph, int size_cutoff, float conn_cutoff)
+/*
+*09-04-05
+*	cut the graph with the edge of maximum betweenness_centrality
+*/
+{
+	int no_of_vertices = num_vertices(graph);
+	int no_of_edges = num_edges(graph);
+	#ifdef DEBUG
+		std::cerr<<"no_of_vertices: "<<no_of_vertices<<std::endl;
+		std::cerr<<"no_of_edges: "<<no_of_edges<<std::endl;
+	#endif
+	if (no_of_vertices>=size_cutoff)
+	{
+		float connectivity = 2.0*no_of_edges/(no_of_vertices*(no_of_vertices-1));
+		#ifdef DEBUG
+			std::cerr<<"connectivity: "<<connectivity<<std::endl;
+		#endif
+		if (connectivity>=conn_cutoff)
+		{
+			#ifdef DEBUG
+				std::cerr<<"good subgraph "<<std::endl;
+			#endif
+			good_subgraph_vector.push_back(graph);
+		}
+		else
+		{
+			vector<double> edge_centrality(no_of_edges);
+			EdgeCentralityMap ec_map(edge_centrality.begin(), get(edge_index, graph));
+				//"make_iterator_property_map(edge_centrality.begin(), get(edge_index, subgraph), double())" also works.
+			indirect_cmp<EdgeCentralityMap, std::less<centrality_type> > cmp(ec_map);
+			#ifdef DEBUG
+				std::cerr<<"running brandes_betweenness_centrality... ";
+			#endif
+			brandes_betweenness_centrality(graph, edge_centrality_map(ec_map)); 
+			#ifdef DEBUG
+				std::cerr<<"done."<<std::endl;
+			#endif
+			edgeDescriptor e = *max_element(edges(graph).first, edges(graph).second, cmp);
+			centrality_type max_centrality = get(ec_map, e);
+			#ifdef DEBUG
+				std::cerr<<"max_centrality is "<<max_centrality<<std::endl;
+			#endif
+			remove_edge(e, graph);
+			#ifdef DEBUG
+				std::cerr<<"after removal the subgraph has "<<num_edges(graph)<<" edges."<<std::endl;
+			#endif
+			std::vector<int> component(num_vertices(graph));
+			int no_of_components = connected_components(graph, &component[0]);
+			if (no_of_components==1)	//keep cutting
+			{
+				#ifdef DEBUG
+					std::cerr<<"only one component, keep cutting "<<std::endl;
+				#endif
+				cut_by_betweenness_centrality(graph, size_cutoff, conn_cutoff);
+			}
+			else	//first get the connected_components into a vector_sub_subgraph and cut them separately
+			{
+				#ifdef DEBUG
+					std::cerr<<"get the connected_components into a vector_graph and cut them separately "<<std::endl;
+				#endif
+				std::vector<Graph> vector_graph = graph_components(graph, component, no_of_components);
+				std::vector<Graph>::iterator g_iterator;
+				for(g_iterator=vector_graph.begin();g_iterator!=vector_graph.end();++g_iterator)
+				{
+					cut_by_betweenness_centrality(*g_iterator, size_cutoff, conn_cutoff);
+				}
+			}
+		}
+	}
+	#ifdef DEBUG
+	else
+		std::cerr<<"Bad graph, too small,"<<no_of_vertices<<"vertices"<<std::endl;
+	#endif
+	
+}
+
+boost::python::list ClusterByEBC::graph2list(Graph &graph)
+/*
+*09-05-05
+*	similar to subgraph2list()
+*/
+{
+	boost::python::list graph_edge_list;	//store the egdes
+	
+	vertex_name_type vertex2name = get(vertex_name, graph);	//mapping
+	
+	boost::graph_traits<Graph>::vertex_descriptor
+	vertex1, vertex2;
+	#if defined(DEBUG)
+		std::cout << "edges(g) = ";
+	#endif
+	graph_traits<Graph>::edge_iterator ei, ei_end;
+	for (tie(ei,ei_end) = edges(graph); ei != ei_end; ++ei)
+	{
+		vertex1 = source(*ei, graph);
+		vertex2 = target(*ei, graph);
+		#if defined(DEBUG)
+			std::cout << "(" << get(vertex2name, vertex1)
+			<< "," << get(vertex2name, vertex2) << ") ";
+		#endif
+		graph_edge_list.append(boost::python::make_tuple(get(vertex2name, vertex1), get(vertex2name, vertex2)));
+
+	}
+	#if defined(DEBUG)
+		std::cout << std::endl;
+	#endif
+	return graph_edge_list;
+}
+
+void ClusterByEBC::run(boost::python::list edge_list, int size_cutoff, float conn_cutoff)
+/*
+*09-04-05
+*	override the run() of the parent class
+*/
+{
+	#ifdef DEBUG
+		std::cerr<<"starting ClusterByEBC...";
+	#endif
+	init_graph_from_edge_list(edge_list, g);
+	//get the connected_components out of g
+	std::vector<int> component(num_vertices(g));
+	int no_of_components = connected_components(g, &component[0]);
+	std::vector<Graph> vector_graph = graph_components(g, component, no_of_components);
+
+	std::vector<Graph>::iterator g_iterator;
+	#ifdef DEBUG
+		std::cerr<<"No. of components in graph is: "<<vector_graph.size()<<std::endl;
+	#endif
+	for(g_iterator=vector_graph.begin();g_iterator!=vector_graph.end();++g_iterator)
+	{
+		cut_by_betweenness_centrality(*g_iterator, size_cutoff, conn_cutoff);
+	}
+	for(g_iterator=good_subgraph_vector.begin();g_iterator!=good_subgraph_vector.end();++g_iterator)
+	{
+		boost::python::list graph_edge_list = graph2list(*g_iterator);
+		cc_list.append(graph_edge_list);
+	}
+}
+
 BOOST_PYTHON_MODULE(cc_from_edge_list)
 {
 	class_<cc_from_edge_list>("cc_from_edge_list")
 		.def("init_graph_from_edge_list", &cc_from_edge_list::init_graph_from_edge_list)
 		.def("run", &cc_from_edge_list::run)
 		.def_readonly("cc_list", &cc_from_edge_list::cc_list)
+	;
+	
+	class_<ClusterByEBC, bases<cc_from_edge_list> >("ClusterByEBC")
+		.def("run", &ClusterByEBC::run)
+		.def_readonly("cc_list", &ClusterByEBC::cc_list)
 	;
 }
--- trunk/bin/graph/cc_from_edge_list.h
+++ trunk/bin/graph/cc_from_edge_list.h
@@ -13,6 +13,9 @@
 
 #include <iostream>			// for std::cout
 #include <vector>			//for vector
+#include <boost/property_map.hpp>	//09-05-05 for iterator_property_map
+#include <boost/graph/betweenness_centrality.hpp>	//09-05-05 for betweenness_centrality
+#include <boost/pending/indirect_cmp.hpp>	//Mon Sep  5 11:38:25 2005 for indirect_cmp
 
 using namespace boost;
 using namespace boost::python;
@@ -23,12 +26,21 @@ typedef subgraph<adjacency_list<vecS, vecS, undirectedS,
 typedef graph_traits < Graph >::vertex_descriptor vertexDescriptor;
 typedef property_map<Graph, vertex_name_t>::type vertex_name_type;
 
+//Mon Sep  5 11:38:25 2005
+typedef property_map<Graph, boost::edge_index_t>::type EdgeIndexMap;
+typedef graph_traits<Graph>::edge_descriptor edgeDescriptor;
+typedef	boost::iterator_property_map<vector<double>::iterator,\
+	EdgeIndexMap, double, double&> EdgeCentralityMap;
+typedef property_traits<EdgeCentralityMap>::value_type centrality_type;
+
 class cc_from_edge_list
 {
 	public:
 	void init_graph_from_edge_list(boost::python::list edge_list, Graph &graph);
 	std::vector<Graph> cc2subgraph(Graph &graph);
 	boost::python::list subgraph2list(Graph &subgraph, Graph &graph);
+	std::vector<Graph> subgraph_components(Graph &subgraph, Graph &graph, std::vector<int> component, int no_of_components);
+	std::vector<Graph> graph_components(Graph &graph, std::vector<int> component, int no_of_components);
 	void run(boost::python::list edge_list);
 	
 	//for input purpose
@@ -36,4 +48,13 @@ class cc_from_edge_list
 	Graph g;
 	std::vector<Graph> vector_subgraph;
 	boost::python::list cc_list;
+};
+
+class ClusterByEBC:public cc_from_edge_list
+{
+	public:
+		void cut_by_betweenness_centrality(Graph &graph, int size_cutoff, float conn_cutoff);
+		void run(boost::python::list edge_list, int size_cutoff, float conn_cutoff);
+		boost::python::list graph2list(Graph &graph);
+		std::vector<Graph> good_subgraph_vector;
 };
