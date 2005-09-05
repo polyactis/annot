0ecb5bc5476a12e690933e3392e3483106ddd945
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-09-05 00:55:11 +0000

    *09-04-05 *	class definition and included header files go to cc_from_edge_list.h
    git-svn-id: file:///usr/local/svn/annot@702 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/graph/cc_from_edge_list.cc
+++ trunk/bin/graph/cc_from_edge_list.cc
@@ -1,42 +1,11 @@
 /*
 *04-29-05
 *	a module to extract connected components(cc) from an edge list.
-*
+*09-04-05
+*	class definition and included header files go to cc_from_edge_list.h
 */
 
-#include <boost/graph/adjacency_list.hpp>	//for graph definition
-#include <boost/graph/connected_components.hpp>	//for connected_components
-#include <boost/graph/subgraph.hpp>	//for boost::subgraph
-#include <boost/graph/graph_traits.hpp>  // for boost::graph_traits
-#include <boost/utility.hpp>             // for boost::tie
-#include <boost/python.hpp>	//for python module and dict, tuple, make_tuple
-
-#include <iostream>			// for std::cout
-#include <vector>			//for vector
-
-using namespace boost;
-using namespace boost::python;
-using namespace std;
-
-typedef subgraph<adjacency_list<vecS, vecS, undirectedS,
-	property<vertex_name_t, int>, property<edge_index_t, int> > > Graph;	//watch subgraph<>
-typedef graph_traits < Graph >::vertex_descriptor vertexDescriptor;
-typedef property_map<Graph, vertex_name_t>::type vertex_name_type;
-
-class cc_from_edge_list
-{
-	public:
-	void init_graph_from_edge_list(boost::python::list edge_list, Graph &graph);
-	std::vector<Graph> cc2subgraph(Graph &graph);
-	boost::python::list subgraph2list(Graph &subgraph, Graph &graph);
-	void run(boost::python::list edge_list);
-	
-	//for input purpose
-	std::map<int, vertexDescriptor> geneNoMap;
-	Graph g;
-	std::vector<Graph> vector_subgraph;
-	boost::python::list cc_list;
-};
+#include "cc_from_edge_list.h"
 
 void cc_from_edge_list::init_graph_from_edge_list(boost::python::list edge_list, Graph &graph)
 {
