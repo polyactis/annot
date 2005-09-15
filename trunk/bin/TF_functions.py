6678caf1e36a7ab7acd2b94acf4130eb4ca7216d
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-09-15 05:28:45 +0000

    tf_combo_stat() first,only tf with >=1/3 associated genes are selected to do combo analysis second, for each pair, get its genome-wide prob and joint prob 3rd, calculate the likelihood seeing #pairs in the cluster and whole genome 4th, likelihood ratio
    git-svn-id: file:///usr/local/svn/annot@719 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/TF_functions.py
+++ trunk/bin/TF_functions.py
@@ -36,10 +36,13 @@ def get_gene_id2tf_set(curs, table_name, organism):
 def map2gene_no_reverse_map(gene_id2tf_set, gene_id2no):
 	"""
 	09-08-05
+	09-14-05
+		add tf2gene_no_set
 	"""
 	sys.stderr.write("Mapping gene_id2tf_set to gene_no2tf_set...")
 	gene_no2tf_set = {}
 	tf2no_of_genes = {}
+	tf2gene_no_set = {}
 	for gene_id, tf_set in gene_id2tf_set.iteritems():
 		if gene_id in gene_id2no:
 			gene_no = gene_id2no[gene_id]
@@ -47,9 +50,11 @@ def map2gene_no_reverse_map(gene_id2tf_set, gene_id2no):
 			for tf in tf_set:
 				if tf not in tf2no_of_genes:
 					tf2no_of_genes[tf] = 0
+					tf2gene_no_set[tf] = Set()
 				tf2no_of_genes[tf]+=1
+				tf2gene_no_set[tf].add(gene_no)
 	sys.stderr.write("Done\n")
-	return gene_no2tf_set, tf2no_of_genes
+	return gene_no2tf_set, tf2no_of_genes, tf2gene_no_set
 
 def tf_stat(gene_no2tf_set, gene_no_list, tf2no_of_genes, no_of_total_genes):
 	"""
@@ -91,7 +96,41 @@ def get_gene_no_list(inputfile):
 		gene_no_list.append(int(row[0]))
 	del reader, inf
 	return gene_no_list
+
+def tf_combo_stat(tf2gene_no_set_global, tf_stat_list, no_of_total_genes, cluster_size):
+	"""
+	09-14-05
+		first,only tf with >=1/3 associated genes are selected to do combo analysis
+		second, for each pair, get its genome-wide prob and joint prob
+		3rd, calculate the likelihood seeing #pairs in the cluster and whole genome
+		4th, likelihood ratio
+	"""
+	tf_gene_no_set_list = []
+	for row in tf_stat_list:
+		if row[1] >=(cluster_size/3.0):
+			tf_gene_no_set_list.append([row[3],Set(row[2])])
+	tf_combo_stat_list = []
+	for i in range(len(tf_gene_no_set_list)):
+		for j in range(i+1, len(tf_gene_no_set_list)):
+			tf1, tf1_gene_no_set = tf_gene_no_set_list[i]
+			tf2, tf2_gene_no_set = tf_gene_no_set_list[j]
+			combo_size_local = len(tf1_gene_no_set&tf2_gene_no_set)
+			tf1_gene_no_set_global = tf2gene_no_set_global[tf1]
+			tf2_gene_no_set_global = tf2gene_no_set_global[tf2]
+			prob_tf1 = len(tf1_gene_no_set_global)/float(no_of_total_genes)
+			prob_tf2 = len(tf2_gene_no_set_global)/float(no_of_total_genes)
+			prob_combo = prob_tf1*prob_tf2
+			combo_size_global = len(tf1_gene_no_set_global&tf2_gene_no_set_global)
+			likeli1 = r.dbinom(combo_size_local, cluster_size, prob_combo)
+			likeli2 = r.dbinom(combo_size_global, no_of_total_genes, prob_combo)
+			if likeli2==0:
+				l_ratio = 1
+			else:
+				l_ratio = likeli1/likeli2
+			tf_combo_stat_list.append([l_ratio, combo_size_local, cluster_size, combo_size_global, no_of_total_genes, prob_tf1, prob_tf2, prob_combo, likeli1, likeli2, list(tf1_gene_no_set&tf2_gene_no_set), (tf1,tf2)])
+	return tf_combo_stat_list
 	
+
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
 		print __doc__
@@ -107,16 +146,26 @@ if __name__ == '__main__':
 		gene_no_list = get_gene_no_list(inputfile)
 		long_organism = org_short2long(organism)
 		(conn, curs) =  db_connect(hostname, dbname)
+		
 		gene_id2tf_set = get_gene_id2tf_set(curs, 'gene_id2tf', long_organism)
 		gene_id2no = get_gene_id2gene_no(curs, schema)
-		gene_no2tf_set, tf2no_of_genes = map2gene_no_reverse_map(gene_id2tf_set, gene_id2no)
+		gene_no2tf_set, tf2no_of_genes, tf2gene_no_set = map2gene_no_reverse_map(gene_id2tf_set, gene_id2no)
+		
 		tf_stat_list, no_tf_gene_no_list = tf_stat(gene_no2tf_set, gene_no_list, tf2no_of_genes, len(gene_id2no))
+		
+		tf_combo_stat_list = tf_combo_stat(tf2gene_no_set, tf_stat_list, len(gene_id2no), len(gene_no_list))
+		
+		#output
 		writer = csv.writer(sys.stdout, delimiter='\t')
 		for row in tf_stat_list:
 			row[2] = repr(row[2])[1:-1]
 			writer.writerow(row)
 		writer.writerow(["Genes_with_no_tf"]+no_tf_gene_no_list)
+		tf_combo_stat_list.sort()
+		for row in tf_combo_stat_list:
+			writer.writerow(row)
 		del writer
+		
 	else:
 		print __doc__
 		sys.exit(2)
