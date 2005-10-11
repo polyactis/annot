539178c90f57f01efc76b161c904d29ab1449566
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-10-11 08:36:36 +0000

    more reasonable replacing
    git-svn-id: file:///usr/local/svn/annot@787 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/arguments2string.py
+++ trunk/bin/arguments2string.py
@@ -4,11 +4,15 @@ Usage: arguments2string.py ARGUMENTS
 
 	
 Examples:
-	arguments2string.py -s 100 -m -t 0.58 -u 0.2 -i das
-		--prints s100mt58u2idas
+	arguments2string.py -s 100 -m -t 0.58 -u 0,2 -i da-s
+		--prints s100mt0_58u0_2ida_s
 	
 Description:
 	Return a string form of the arguments.
+	Warning: Front - can only be used in option,
+	not argument. Otherwise, that argument
+	is regarded as argument. i.e -c -1
+	-1 is regarded as option. Prints c1.
 
 """
 
@@ -26,13 +30,15 @@ def arguments2string(arguments_list):
 			else:	#short option
 				string += argument[1:]
 		else:	#this is argument
-			if argument[:2] == '0.':	#digit
-				argument = argument[2:]
-			argument = argument.replace('-','n')	#replace '-' with 'n'
+			argument = argument.replace('-','_')	#replace '-' with '_'
 			argument = argument.replace('.','_')	#replace '.' with '_'
 			string += argument.replace(',','_')	#replace ',' with '_'
 			
 	return string
 
 if __name__ == '__main__':
-	print arguments2string(sys.argv[1:])
+	if len(sys.argv)==1:
+		print __doc__
+		sys.exit(0)
+	else:
+		print arguments2string(sys.argv[1:])
