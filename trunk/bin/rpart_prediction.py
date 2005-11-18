b38cc6441bcf73e4f79c496055350693fff672fb
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-11-18 10:26:20 +0000

    add functions: 	sample_data() 	cal_accuracy() 	rpart_validation
    for parameter tuning, cross-validation, called by MpiRpartValidation.py
    
    
    git-svn-id: file:///usr/local/svn/annot@931 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/rpart_prediction.py
+++ trunk/bin/rpart_prediction.py
@@ -7,12 +7,13 @@ Option:
 	-d ..., --dbname=...	the database name, graphdb(default)
 	-k ..., --schema=...	which schema in the database
 	-i ...,	fname of setting1
-	-l ...,	lm_bit of setting1, ('00001', default, IGNORE)
 	-j ...,	fname of setting2
-	-m ...,	lm_bit of setting2, ('00001' default, IGNORE)
 	-f ...,	filter type
 	-y ...,	is_correct type (2 lca, default)
 	-p ...,	rpart cp value(0.01, default)
+	-l ...,	loss matrix, 0,1,1,0 (default)
+	-o ...,	prior prob for the 1st class, None (default, i.e. 0.65)
+	-s ...,	percentage of data selected to do training, 0.8(default)
 	-g, 	calculate the hypergeometric p-value to replace p_value_cut_off(gradient)
 	-b,	enable debug flag
 	-c,	commit the database transaction
@@ -26,18 +27,18 @@ Examples:
 Description:
 	Program to do rpart fittng and prediction.
 """
-import sys, os, getopt, csv
+import sys, os, getopt, csv, random
 sys.path += [os.path.expanduser('~/script/annot/bin')]
 from codense.common import db_connect, form_schema_tables, cal_hg_p_value, \
 	get_go_no2gene_no_set, get_no_of_total_genes
 from MpiPredictionFilter import prediction_attributes, MpiPredictionFilter
 from numarray import array
 from rpy import r, set_default_mode,NO_CONVERSION,BASIC_CONVERSION
-
+from sets import Set
 
 class rpart_prediction:
-	def __init__(self, hostname='zhoudb', dbname='graphdb', schema=None, fname1=None, \
-		lm_bit1=None, fname2=None, lm_bit2=None, filter_type=1, is_correct_type=2, rpart_cp=0.01, \
+	def __init__(self, hostname='zhoudb', dbname='graphdb', schema=None, fname1=None, fname2=None, \
+		filter_type=1, is_correct_type=2, rpart_cp=0.01, loss_matrix=[0,1,1,0], prior_prob=None, training_perc=0.8,\
 		need_cal_hg_p_value=0, debug=0, commit=0, report=0):
 		"""
 		11-09-05 add rpart_cp
@@ -46,19 +47,20 @@ class rpart_prediction:
 		self.dbname = dbname
 		self.schema = schema
 		self.fname1 = fname1
-		self.lm_bit1 = lm_bit1
 		self.fname2 = fname2
-		self.lm_bit2 = lm_bit2
 		self.filter_type = int(filter_type)
 		self.is_correct_type = int(is_correct_type)
-		self.rpart_cp = float(rpart_cp)
+		self.rpart_cp = rpart_cp
+		self.loss_matrix = loss_matrix
+		self.prior_prob = prior_prob
+		self.training_perc = float(training_perc)
 		self.need_cal_hg_p_value = int(need_cal_hg_p_value)
 		self.debug = int(debug)
 		self.commit = int(commit)
 		self.report = int(report)
 	
 	def data_fetch(self, curs, schema_instance, filter_type, is_correct_type, \
-		no_of_total_genes, go_no2gene_no_set, need_cal_hg_p_value):
+		no_of_total_genes, go_no2gene_no_set, need_cal_hg_p_value=0):
 		"""
 		11-09-05
 			1st get the data from p_gene_table and remove redundancy given filter_type
@@ -116,22 +118,24 @@ class rpart_prediction:
 			
 			is_correct = p_attr_instance.is_correct_dict[is_correct_type]
 			data_row = [p_attr_instance.p_value_cut_off, p_attr_instance.recurrence_cut_off, p_attr_instance.connectivity_cut_off,\
-				p_attr_instance.cluster_size_cut_off, p_attr_instance.edge_gradient, is_correct]
+				p_attr_instance.cluster_size_cut_off, p_attr_instance.edge_gradient, p_attr_instance.gene_no, p_attr_instance.go_no, \
+				is_correct]
 			all_data.append(data_row)
 			if is_correct!=-1:
 				known_data.append(data_row)	#to do fitting
-		all_data = array(all_data)
-		known_data = array(known_data)
 		
 		sys.stderr.write("Done fetching data.\n")
 		return prediction_ls, all_data, known_data
 	
-	def rpart_fit_and_predict(self, all_data, known_data, rpart_cp, bit_string='11111'):
+	def rpart_fit_and_predict(self, all_data, known_data, rpart_cp, loss_matrix, prior_prob, bit_string='11111'):
 		"""
 		11-09-05
 			1st use known_data to get the fit model
 			2nd use the fit model to do prediction on all_data, result is prob for each class
 		11-09-05 add rpart_cp
+		11-17-05
+			add loss_matrix, prior_prob
+			return two pred
 		"""
 		sys.stderr.write("rpart fitting and predicting...\n")
 		r.library("rpart")
@@ -140,17 +144,33 @@ class rpart_prediction:
 		for i in range(len(bit_string)):
 			if bit_string[i] == '1':
 				formula_list.append(coeff_name_list[i])
+		#11-17-05 transform into array
+		all_data = array(all_data)
+		known_data = array(known_data)
+		
 		set_default_mode(NO_CONVERSION)
 		data_frame = r.as_data_frame({"p_value":known_data[:,0], "recurrence":known_data[:,1], "connectivity":known_data[:,2], \
 			"cluster_size":known_data[:,3], "gradient":known_data[:,4], "is_correct":known_data[:,-1]})
-		fit = r.rpart(r("is_correct~%s"%'+'.join(formula_list)), data=data_frame, method="class", control=r.rpart_control(cp=rpart_cp))
+		if prior_prob:
+			prior_prob = [prior_prob, 1-prior_prob]	#get the full list
+			fit = r.rpart(r("is_correct~%s"%'+'.join(formula_list)), data=data_frame, method="class", control=r.rpart_control(cp=rpart_cp),\
+				parms=r.list(prior=prior_prob, loss=r.matrix(loss_matrix) ) )
+		else:
+			fit = r.rpart(r("is_correct~%s"%'+'.join(formula_list)), data=data_frame, method="class", control=r.rpart_control(cp=rpart_cp),\
+				parms=r.list(loss=r.matrix(loss_matrix) ) )
+		
+		set_default_mode(BASIC_CONVERSION)
+		pred_training = r.predict(fit, data_frame, type=["class"])	
 		del data_frame
+		
+		set_default_mode(NO_CONVERSION)
 		all_data_frame = r.as_data_frame({"p_value":all_data[:,0], "recurrence":all_data[:,1], "connectivity":all_data[:,2], \
 			"cluster_size":all_data[:,3], "gradient":all_data[:,4], "is_correct":all_data[:,-1]})
-		set_default_mode(BASIC_CONVERSION) #04-07-05
-		pred = r.predict(fit, all_data_frame)
+		set_default_mode(BASIC_CONVERSION)
+		pred = r.predict(fit, all_data_frame, type=["class"])	#11-17-05 type=c("class")
+		del all_data_frame
 		sys.stderr.write("Done rpart fitting and predicting.\n")
-		return pred
+		return pred, pred_training
 	
 	def get_vertex_list(self, curs, schema_instance, mcl_id):
 		"""
@@ -166,14 +186,71 @@ class rpart_prediction:
 	def record_data(self, curs, MpiPredictionFilter_instance, prediction_ls, pred, schema_instance):
 		"""
 		11-09-05
+		11-18-05 pred is type="class"
 		"""
 		sys.stderr.write("Recording prediction...\n")
 		for i in range(len(prediction_ls)):
 			p_attr_instance = prediction_ls[i]
-			p_attr_instance.vertex_gradient = pred[i][1]			
+			p_attr_instance.vertex_gradient = pred[repr(i+1)]	#WATCH R's index starts from 1
 			MpiPredictionFilter_instance.submit_to_p_gene_table(curs, schema_instance.p_gene_table, p_attr_instance)
 		sys.stderr.write("Done recoding prediction...\n")
 	
+	
+	
+	####11-17-05  for parameter tuning purpose
+	def sample_data(self, data,  training_perc):
+		"""
+		11-17-05
+			for parameter tuning purpose
+		"""
+		total_no_of_cases = len(data)
+		sample_indices = random.sample(range(total_no_of_cases), int(training_perc*total_no_of_cases))
+		training_data = []
+		testing_data  = []
+		sample_indices_set = Set(sample_indices)
+		for i in range(total_no_of_cases):
+			if i in sample_indices_set:
+				training_data.append(data[i])
+			else:
+				testing_data.append(data[i])
+		return training_data, testing_data
+	
+	def cal_accuracy(self, data, pred):
+		"""
+		11-17-05
+			for parameter tuning purpose
+		"""
+		no_of_total_predictions = 0
+		no_of_correct_predictions = 0
+		gene_set = Set()
+		for i in range(len(data)):
+			p_value, recurrence, connectivity, cluster_size, edge_gradient, gene_no, go_no, is_correct = data[i]
+			key_in_pred = repr(i+1)	#WATCH R's index starts from 1
+			if pred[key_in_pred] == '1':
+				no_of_total_predictions += 1
+				gene_set.add(gene_no)
+				if is_correct==1:
+					no_of_correct_predictions += 1
+		if no_of_total_predictions>0:
+			accuracy = float(no_of_correct_predictions)/no_of_total_predictions
+		else:
+			accuracy = 0
+		return [accuracy, no_of_correct_predictions, no_of_total_predictions, len(gene_set)]
+	
+	def rpart_validation(self, known_data, training_perc, rpart_cp, loss_matrix, prior_prob):
+		"""
+		11-17-05
+			for parameter tuning purpose
+		"""
+		training_acc_ls = []
+		testing_acc_ls = []
+		for i in range(10):
+			training_data, testing_data = self.sample_data(known_data, training_perc)
+			pred_testing, pred_training = self.rpart_fit_and_predict(testing_data, training_data, rpart_cp, loss_matrix, prior_prob)
+			testing_acc_ls.append(self.cal_accuracy(testing_data, pred_testing))
+			training_acc_ls.append(self.cal_accuracy(training_data, pred_training))
+		return testing_acc_ls, training_acc_ls
+		
 	def run(self):
 		"""
 		11-09-05
@@ -193,15 +270,23 @@ class rpart_prediction:
 			--record_data()
 		"""
 		(conn, curs) =  db_connect(self.hostname, self.dbname, self.schema)
-		old_schema_instance = form_schema_tables(self.fname1, self.lm_bit1)
-		new_schema_instance = form_schema_tables(self.fname2, self.lm_bit2)
+		old_schema_instance = form_schema_tables(self.fname1)
+		new_schema_instance = form_schema_tables(self.fname2)
 		
 		no_of_total_genes = get_no_of_total_genes(curs)
 		go_no2gene_no_set = get_go_no2gene_no_set(curs)
 		
 		prediction_ls, all_data, known_data = self.data_fetch(curs, old_schema_instance, self.filter_type, self.is_correct_type, \
 			no_of_total_genes, go_no2gene_no_set, need_cal_hg_p_value)
-		pred = self.rpart_fit_and_predict(all_data, known_data, self.rpart_cp)
+		
+		"""
+		testing_acc_ls, training_acc_ls = self.rpart_validation(known_data, self.training_perc, self.rpart_cp, \
+			self.loss_matrix, self.prior_prob)
+		print testing_acc_ls
+		print training_acc_ls
+		"""
+		pred, pred_training = self.rpart_fit_and_predict(all_data, known_data, self.rpart_cp, self.loss_matrix, self.prior_prob)
+		
 		MpiPredictionFilter_instance = MpiPredictionFilter()
 		MpiPredictionFilter_instance.view_from_table(curs, old_schema_instance.splat_table, new_schema_instance.splat_table)
 		MpiPredictionFilter_instance.view_from_table(curs, old_schema_instance.mcl_table, new_schema_instance.mcl_table)
@@ -210,6 +295,7 @@ class rpart_prediction:
 		self.record_data(curs, MpiPredictionFilter_instance, prediction_ls, pred, new_schema_instance)
 		if self.commit:
 			curs.execute("end")
+		"""
 
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
@@ -217,7 +303,7 @@ if __name__ == '__main__':
 		sys.exit(2)
 		
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:i:l:j:m:f:y:p:gbcr", ["help", "hostname=", \
+		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:i:j:f:y:p:l:o:s:gbcr", ["help", "hostname=", \
 			"dbname=", "schema="])
 	except:
 		print __doc__
@@ -227,12 +313,13 @@ if __name__ == '__main__':
 	dbname = 'graphdb'
 	schema = ''
 	fname1 = None
-	lm_bit1 = '00001'
 	fname2 = None
-	lm_bit2 = '00001'
 	filter_type = 1
 	is_correct_type = 2
 	rpart_cp = 0.01
+	loss_matrix = [0,1,1,0]
+	prior_prob = None
+	training_perc = 0.8
 	need_cal_hg_p_value = 0
 	debug = 0
 	commit = 0
@@ -249,18 +336,20 @@ if __name__ == '__main__':
 			schema = arg
 		elif opt in ("-i"):
 			fname1 = arg
-		elif opt in ("-l"):
-			lm_bit1 = arg
 		elif opt in ("-j"):
 			fname2 = arg
-		elif opt in ("-m"):
-			lm_bit2 = arg
 		elif opt in ("-f"):
 			filter_type = int(arg)
 		elif opt in ("-y"):
 			is_correct_type = int(arg)
 		elif opt in ("-p"):
 			rpart_cp = float(arg)
+		elif opt in ("-l"):
+			loss_matrix = map(float, arg.split(','))
+		elif opt in ("-o"):
+			prior_prob = float(arg)
+		elif opt in ("-s"):
+			training_perc = float(arg)
 		elif opt in ("-g"):
 			need_cal_hg_p_value = 1
 		elif opt in ("-b"):
@@ -270,9 +359,9 @@ if __name__ == '__main__':
 		elif opt in ("-r"):
 			report = 1
 	if schema and fname1 and fname2:
-		instance = rpart_prediction(hostname, dbname, schema, fname1, lm_bit1, \
-			fname2, lm_bit2, filter_type, is_correct_type, rpart_cp, need_cal_hg_p_value, \
-			debug, commit, report)
+		instance = rpart_prediction(hostname, dbname, schema, fname1, fname2, \
+			filter_type, is_correct_type, rpart_cp, loss_matrix, prior_prob, training_perc, \
+			need_cal_hg_p_value, debug, commit, report)
 		instance.run()
 	else:
 		print __doc__
