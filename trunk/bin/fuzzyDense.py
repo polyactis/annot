212ec6e062bf85847c33360beebbc75993a5a3a0
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2005-12-19 06:02:33 +0000

    12-18-05         This standalone program gets a pattern from the pattern_table,         get the dense part and do         hypergeometric p-value calculation. Output in darwin format.
    git-svn-id: file:///usr/local/svn/annot@971 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/fuzzyDense.py
+++ trunk/bin/fuzzyDense.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python
 """
+Usage: fuzzyDense.py -i pattern_id [OPTIONS]
+
+Option:
+	-z ..., --hostname=...	the hostname, zhoudb(default)
+	-d ..., --dbname=...	the database name, graphdb(default)
+	-k ..., --schema=...	which schema in the database, 'scfim30' (default)
+	-i ..., 	input pattern_id
+	-j ...,		input pattern_table, pattern_scfim30m3x30bfs(default)
+	-e ...,	degree_cut_off, 0.3(default)
+	-p ...,	p_value_cut_off, 0.001 (default)
+	-o ...,	output_file, fuzzyDense.out(default)
+	-a ...,	min ratio of #associated genes for one bs_no vs cluster size, 0(default)
+	-t ...,	top_number of scores to be kept, 5(default)
+	-x ...,	tax_id, 4932(default, yeast), for get_gene_id2gene_symbol
+	-b,	debug version.
+	-r,	enable report flag
+	-h,	Display the usage infomation.
+
 12-16-05
 	counterpart of haiyan's fuzzyDense
+12-18-05
+	This standalone program gets a pattern from the pattern_table, 
+	get the dense part and do 
+	hypergeometric p-value calculation. Output in darwin format.
+	
 """
 import boost.graph as bgl
 from MpiFromDatasetSignatureToPattern import decodeOccurrence
+from sets import Set
+
 
 class fuzzyDense:
 	"""
@@ -31,10 +56,10 @@ class fuzzyDense:
 		for v in graph.vertices:
 			if graph.in_degree(v) == 0:
 				vertices_to_remove.append(v)
-		"""
+		
 		if self.debug:
 			print "no of singleton vertices to remove", len(vertices_to_remove)
-		"""
+		
 		for v in vertices_to_remove:
 			graph.remove_vertex(v)
 	
@@ -51,11 +76,11 @@ class fuzzyDense:
 		self.remove_singleton_vertices(graph)
 		vertex_min_degree, min_degree = self.get_vertex_min_degree(graph)
 		degree_percentage = float(min_degree)/(graph.num_vertices()-1)
-		while degree_percentage<degree_cut_off:
-			"""
+		while degree_percentage<degree_cut_off and vertex_min_degree:
+			
 			if self.debug:
 				print "vertex %s removed with min_degree %s, degree_percentage %s"%(graph.vertex_properties['vertex_id'][vertex_min_degree], min_degree, degree_percentage)
-			"""
+			
 			graph.clear_vertex(vertex_min_degree)
 			graph.remove_vertex(vertex_min_degree)
 			vertex_min_degree, min_degree = self.get_vertex_min_degree(graph)
@@ -68,7 +93,9 @@ class fuzzyDense:
 		12-16-05
 			global structures used:
 				--self.edge2encodedOccurrence
-		
+		12-18-05
+			expand to all datasets
+			
 			--init_graph_from_vertex_set()
 			--decodeOccurrence()
 			--remove_loose_part_of_graph()
@@ -76,11 +103,15 @@ class fuzzyDense:
 				--get_vertex_min_degree()
 		"""
 		no_of_datasets = len(recurrence_array)
+		#initialize all graphs
 		graph_list = [None]*no_of_datasets
 		anti_vertex_id_list = [None]*no_of_datasets
+		recurrence_set = Set()
 		for i in range(no_of_datasets):
+			graph_list[i], anti_vertex_id_list[i] = self.init_graph_from_vertex_set(vertex_list)
 			if recurrence_array[i] == 1:
-				graph_list[i], anti_vertex_id_list[i] = self.init_graph_from_vertex_set(vertex_list)
+				recurrence_set.add(i)
+		
 		no_of_vertices = len(vertex_list)
 		#vertex_list.sort()	#presorted
 		#construct graphs for each 'on' dataset
@@ -99,67 +130,183 @@ class fuzzyDense:
 					"""
 					for k in edge_recurrence:
 						index = k-1
-						if recurrence_array[index] == 1:	#just those on datasets
-							"""
-							if self.debug:
-								print "checking dataset", index
-							"""
-							v_descriptor1 = anti_vertex_id_list[index][vertex_list[i]]
-							v_descriptor2 = anti_vertex_id_list[index][vertex_list[j]]
-							graph_list[index].add_edge(v_descriptor1, v_descriptor2)
+						v_descriptor1 = anti_vertex_id_list[index][vertex_list[i]]
+						v_descriptor2 = anti_vertex_id_list[index][vertex_list[j]]
+						graph_list[index].add_edge(v_descriptor1, v_descriptor2)
 		#remove loose part for each graph
-		vertex_id2occurrence = {}
-		on_dataset_index_ls = []
+		on_dataset_index_ls = [0]*no_of_datasets
 		for i in range(no_of_datasets):
-			if graph_list[i]:
-				"""
-				if self.debug:
-					print "checking graph", i
-				"""
+			if graph_list[i].num_edges()>1:	#at least the graph has two edges
 				degree_percentage = self.remove_loose_part_of_graph(graph_list[i], degree_cut_off)
 				if graph_list[i].num_vertices()>=4:	#min graph size
 					"""
 					if self.debug:
 						print "graph %s has %s vertices remaining with degree_percentage: %s."%(i, graph_list[i].num_vertices(), degree_percentage)
 					"""
-					on_dataset_index_ls.append(i)	#this dataset should be counted as 'on'
-					for v in graph_list[i].vertices:
-						vertex_id = graph_list[i].vertex_properties['vertex_id'][v]
-						if vertex_id not in vertex_id2occurrence:
-							vertex_id2occurrence[vertex_id]	= 0
-						vertex_id2occurrence[vertex_id] += 1
-		core_vertex_set = []
+					on_dataset_index_ls[i] = 1	#this dataset should be counted as 'on'
+				else:
+					on_dataset_index_ls[i] = 0
+		
+		#find core vertex_list only in those recurrent 'on' datasets
+		vertex_id2occurrence = {}
+		recurrent_and_on_datasets_ls = []
+		on_but_not_recurrent_dataset2vertex_set = {}
+		for i in range(no_of_datasets):
+			if recurrence_array[i] == 1 and on_dataset_index_ls[i] == 1:
+				recurrent_and_on_datasets_ls.append(i)
+				for v in graph_list[i].vertices:
+					vertex_id = graph_list[i].vertex_properties['vertex_id'][v]
+					if vertex_id not in vertex_id2occurrence:
+						vertex_id2occurrence[vertex_id]	= 0
+					vertex_id2occurrence[vertex_id] += 1
+			if recurrence_array[i] == 0 and on_dataset_index_ls[i] == 1:
+				on_but_not_recurrent_dataset2vertex_set[i] = Set()
+				for v in graph_list[i].vertices:
+					vertex_id = graph_list[i].vertex_properties['vertex_id'][v]
+					on_but_not_recurrent_dataset2vertex_set[i].add(vertex_id)
+		#only vertices in recurrent and 'on' datasets go into core_vertex_set
+		core_vertex_set = Set()
 		for vertex_id in vertex_id2occurrence:
-			if vertex_id2occurrence[vertex_id] == len(on_dataset_index_ls):
-				core_vertex_set.append(vertex_id)
-		core_vertex_set.sort()
-		return core_vertex_set, on_dataset_index_ls
+			if vertex_id2occurrence[vertex_id] == len(recurrent_and_on_datasets_ls):
+				core_vertex_set.add(vertex_id)
+		
+		#find other on datasets from on_but_not_recurrent_dataset2vertex_set
+		for dataset_no, vertex_set in on_but_not_recurrent_dataset2vertex_set.iteritems():
+			intersection_set = core_vertex_set & vertex_set
+			if len(intersection_set)==len(core_vertex_set):
+				recurrent_and_on_datasets_ls.append(dataset_no)
+		
+		core_vertex_ls  = list(core_vertex_set)
+		core_vertex_ls.sort()
+		recurrent_and_on_datasets_ls.sort()
+		
+		return core_vertex_ls, recurrent_and_on_datasets_ls
 
 
 if __name__ == '__main__':
 	"""
 	12-16-05 give it a test
 	"""
-	import sys,os
+	import sys,os,getopt
 	sys.path += [os.path.expanduser('~/script/annot/bin')]
-	from codense.common import db_connect
-	from MpiCrackSplat import MpiCrackSplat
-	hostname='zhoudb'
-	dbname='graphdb'
+	if len(sys.argv) == 1:
+		print __doc__
+		sys.exit(2)
+	
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:i:j:e:p:o:a:t:x:br", ["help", "hostname=", \
+			"dbname=", "schema="])
+	except:
+		print __doc__
+		sys.exit(2)
+	
+	hostname = 'zhoudb'
+	dbname = 'graphdb'
 	schema = 'scfim30'
-	conn, curs = db_connect(hostname, dbname, schema)
-	MpiCrackSplat_instance = MpiCrackSplat()
-	edge2encodedOccurrence = {}
-	min_sup = 3
-	max_sup = 30
-	no_of_datasets = MpiCrackSplat_instance.fill_edge2encodedOccurrence(hostname, dbname, \
-	schema, edge2encodedOccurrence, min_sup, max_sup)
-	vertex_list = [854078, 854317, 854381, 854469, 855539, 855547, 855613, 855730, 855862, 856019, 856226]
-	recurrence_array =  [0.0, 0.0, 0.0, 1.0, 0.53333333333333333, 0.0, 0.066666666666666666, \
-	0.20000000000000001, 0.33333333333333331, 0.0, 0.26666666666666666, 0.20000000000000001, \
-	0.0, 0.0, 0.33333333333333331, 0.0, 0.66666666666666663, 1.0, 0.0, 0.66666666666666663, 1.0, \
-	0.0, 0.066666666666666666, 0.0, 0.46666666666666667, 0.0, 0.13333333333333333, \
-	0.20000000000000001, 0.26666666666666666, 1.0]
+	pattern_id = None
+	pattern_table = 'pattern_scfim30m3x30bfs'
 	degree_cut_off = 0.3
-	fuzzyDense_instance = fuzzyDense(edge2encodedOccurrence, debug=1)
-	print fuzzyDense_instance.get_core_vertex_set(vertex_list, recurrence_array, degree_cut_off)
+	p_value_cut_off = 0.001
+	output_file = 'fuzzyDense.out'
+	ratio_cutoff = 0.0
+	top_number = 5
+	tax_id = 4932
+	debug = 0
+	report = 0
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			print __doc__
+			sys.exit(2)
+		elif opt in ("-z", "--hostname"):
+			hostname = arg
+		elif opt in ("-d", "--dbname"):
+			dbname = arg
+		elif opt in ("-k", "--schema"):
+			schema = arg
+		elif opt in ("-i",):
+			pattern_id = int(arg)
+		elif opt in ("-j",):
+			pattern_table = arg
+		elif opt in ("-e",):
+			degree_cut_off = float(arg)
+		elif opt in ("-p",):
+			p_value_cut_off = float(arg)
+		elif opt in ("-o",):
+			output_file = arg
+		elif opt in ("-a",):
+			ratio_cutoff = float(arg)
+		elif opt in ("-t",):
+			top_number = int(arg)
+		elif opt in ("-x",):
+			tax_id = int(arg)
+		elif opt in ("-b",):
+			debug = 1
+		elif opt in ("-r",):
+			report = 1
+	
+	if schema and pattern_id:
+		from codense.common import db_connect, get_gene_id2gene_symbol, dict_map, get_dataset_no2desc
+		from MpiCrackSplat import MpiCrackSplat
+		conn, curs = db_connect(hostname, dbname, schema)
+		MpiCrackSplat_instance = MpiCrackSplat()
+		edge2encodedOccurrence = {}
+		min_sup = 3
+		max_sup = 30
+		no_of_datasets = MpiCrackSplat_instance.fill_edge2encodedOccurrence(hostname, dbname, \
+		schema, edge2encodedOccurrence, min_sup, max_sup)
+		
+		"""
+		vertex_list = [854078, 854317, 854381, 854469, 855539, 855547, 855613, 855730, 855862, 856019, 856226]
+		recurrence_array =  [0.0, 0.0, 0.0, 1.0, 0.53333333333333333, 0.0, 0.066666666666666666, \
+		0.20000000000000001, 0.33333333333333331, 0.0, 0.26666666666666666, 0.20000000000000001, \
+		0.0, 0.0, 0.33333333333333331, 0.0, 0.66666666666666663, 1.0, 0.0, 0.66666666666666663, 1.0, \
+		0.0, 0.066666666666666666, 0.0, 0.46666666666666667, 0.0, 0.13333333333333333, \
+		0.20000000000000001, 0.26666666666666666, 1.0]
+		degree_cut_off = 0.3
+		"""
+		
+		curs.execute("select vertex_set, recurrence_array from %s where id=%s"%(pattern_table, pattern_id))
+		rows = curs.fetchall()
+		vertex_list, recurrence_array = rows[0]
+		vertex_list = vertex_list[1:-1].split(',')
+		vertex_list = map(int, vertex_list)
+		recurrence_array = recurrence_array[1:-1].split(',')
+		recurrence_array = map(float, recurrence_array)
+		
+		fuzzyDense_instance = fuzzyDense(edge2encodedOccurrence, debug)
+		core_vertex_ls, recurrent_and_on_datasets_ls = fuzzyDense_instance.get_core_vertex_set(vertex_list, recurrence_array, degree_cut_off)
+		
+		from MpiClusterBsStat import MpiClusterBsStat
+		MpiClusterBsStat_instance = MpiClusterBsStat()
+		gene_no2bs_no_block = MpiClusterBsStat_instance.get_gene_no2bs_no_block(curs)
+		gene_no2bs_no_set, bs_no2gene_no_set = MpiClusterBsStat_instance.construct_two_dicts(0, gene_no2bs_no_block)
+		from TF_functions import cluster_bs_analysis
+		ls_to_return = cluster_bs_analysis(core_vertex_ls, gene_no2bs_no_set, bs_no2gene_no_set, ratio_cutoff, \
+			top_number, p_value_cut_off)
+		
+		gene_id2symbol = get_gene_id2gene_symbol(curs, tax_id)
+		dataset_no2desc = get_dataset_no2desc(curs)
+		
+		dataset_no_desc_ls = []
+		for dataset_index in recurrent_and_on_datasets_ls:
+			dataset_no = dataset_index +1
+			dataset_no_desc_ls.append([dataset_no, dataset_no2desc[dataset_no]])
+		
+		
+		outf = open(output_file, 'w')
+		outf.write("out:=[\n")
+		for i in range(len(ls_to_return)):
+			row = ls_to_return[i]
+			score, score_type, bs_no_list, target_gene_no_list, global_ratio, local_ratio, expected_ratio, unknown_ratio = row
+			core_vertex_symbol_ls = dict_map(gene_id2symbol, core_vertex_ls)
+			bs_no_symbol_list = dict_map(gene_id2symbol, bs_no_list)
+			if i == len(ls_to_return)-1:
+				outf.write('[{%s},{%s},{%s}]\n'%(repr(core_vertex_symbol_ls)[1:-1], repr(bs_no_symbol_list)[1:-1], repr(dataset_no_desc_ls)[1:-1]))
+			else:
+				outf.write('[{%s},{%s},{%s}],\n'%(repr(core_vertex_symbol_ls)[1:-1], repr(bs_no_symbol_list)[1:-1], repr(dataset_no_desc_ls)[1:-1]))
+		
+		outf.write(']:\n')
+		
+	else:
+		print __doc__
+		sys.exit(2)
