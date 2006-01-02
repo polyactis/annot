ae6c93b0470afac52f701c69de08a5e87f520417
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2006-01-02 09:23:54 +0000

    output_edge_data_fim_edge_oriented_from_sig_vector_file() 	01-01-06 	add delimiter_char and type
    git-svn-id: file:///usr/local/svn/annot@1021 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/PreFimInput.py
+++ trunk/bin/PreFimInput.py
@@ -9,6 +9,8 @@ Option:
 	-s ...,	sig_vector file
 	-m ..., --min_sup=...	minimum support of an edge in the database, 0(default)
 	-x ..., --max_sup=...	maximum support of an edge, 200(default)
+	-t ...,	delimiter for the outputfile, 'tab'(default)
+	-y ...,	type, 1(fim_closed, Jianfei Zhu's default), 2(closet+)
 	-b, --debug	debug version
 	-r,	enable report flag
 	-h, --help              show this help
@@ -66,17 +68,29 @@ def output_edge_data_fim_edge_oriented(outputfile, min_support=3, \
 	sys.stderr.write("Done\n")
 
 def output_edge_data_fim_edge_oriented_from_sig_vector_file(outputfile, min_support=3, \
-	max_support=200, sig_vector_fname='', debug=0, report=0):
+	max_support=200, sig_vector_fname='', delimiter_char='\t', type=1, debug=0, report=0):
 	"""
 	12-30-05
 		read edge data from sig_vector_file
+	01-01-06
+		add delimiter_char and type
+		
+		For type==2 (closet+) 's spec file
+		1. file name of the data set;
+		2. total number of distinct items in the dataset;
+		3. Maximum length of one transaction
+			(i.e., maximum number of distinct items in each transaction);
+		4. total number of transactions
 	"""	
-	fname = outputfile
 	reader = csv.reader(open(sig_vector_fname, 'r'), delimiter='\t')
-	writer = csv.writer(open(fname,'w'),delimiter='\t')
+	writer = csv.writer(open(outputfile, 'w'),delimiter=delimiter_char)
 	
+	if type==2:	#closet+ needs a spec file
+		spec_fname = '%s.spec'%outputfile
+		spec_f = open(spec_fname, 'w')
 	sys.stderr.write("Getting edge matrix for all functions...\n")
 	counter = 0
+	real_counter = 0
 	for row in reader:
 		sig_vector = row[2:]
 		sig_vector = map(int, sig_vector)
@@ -85,12 +99,22 @@ def output_edge_data_fim_edge_oriented_from_sig_vector_file(outputfile, min_supp
 			for i in range(len(sig_vector)):
 				if sig_vector[i]==1:
 					new_row.append(i+1)
+			if type==2:	#closet+ needs no of items for each transaction
+				new_row.insert(0, len(new_row))
 			writer.writerow(new_row)
+			real_counter += 1
 		counter +=1
 		if report and counter%5000==0:
 			sys.stderr.write('%s%s'%('\x08'*20, counter))
 		if debug:
 			break
+	if type==2:
+		spec_f.write('%s\n'%os.path.basename(outputfile))	#1. file name of the data set;
+		spec_f.write('%s\n'%(len(sig_vector)+1))	#2. total number of distinct items in the dataset;
+			#01-02-06 +1 because for closet+, item no starts from 0.
+		spec_f.write('%s\n'%(len(sig_vector)+1))	#3. Maximum length of one transaction
+		spec_f.write('%s\n'%real_counter)	#4. total number of transactions
+		del spec_f
 	del reader, writer
 	sys.stderr.write("Done\n")
 	
@@ -100,7 +124,7 @@ if __name__ == '__main__':
 		sys.exit(2)
 		
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:s:m:x:br", ["help", "hostname=", \
+		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:s:m:x:t:y:br", ["help", "hostname=", \
 			"dbname=", "schema=", "min_sup", "max_sup=", "debug"])
 	except:
 		print __doc__
@@ -112,6 +136,8 @@ if __name__ == '__main__':
 	sig_vector_fname = ''
 	min_sup = 0
 	max_sup = 200
+	delimiter_char = '\t'
+	type = 1
 	debug = 0
 	report = 0
 	for opt, arg in opts:
@@ -130,6 +156,10 @@ if __name__ == '__main__':
 			min_sup = int(arg)
 		elif opt in ("-x", "--max_sup"):
 			max_sup = int(arg)
+		elif opt in ("-t",):
+			delimiter_char = arg
+		elif opt in ("-y",):
+			type = int(arg)
 		elif opt in ("-b", "--debug"):
 			debug = 1
 		elif opt in ("-r",):
@@ -138,7 +168,7 @@ if __name__ == '__main__':
 	#12-30-05 discard the schema form, take input from sig_vector_fname
 	if sig_vector_fname and outputfile:
 		output_edge_data_fim_edge_oriented_from_sig_vector_file(outputfile, min_sup, max_sup, \
-			sig_vector_fname, debug, report)
+			sig_vector_fname, delimiter_char, type, debug, report)
 	else:
 		print __doc__
 		sys.exit(2)
