a06612616116b31df41993b58cf5f9bfb6209438
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2006-01-06 09:04:40 +0000

    Program to draw histogram of tfbs_similarity_ls between two groups of homologenes.
    git-svn-id: file:///usr/local/svn/annot@1034 4ebff559-900f-0410-a220-daa186eb8a63

---
+++ trunk/bin/OrthologTFBSCompare.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+"""
+Usage: OrthologTFBSCompare.py -p output_prefix [OPTIONS]
+
+Option:
+	-z ..., --hostname=...	the hostname, zhoudb(default)
+	-d ..., --dbname=...	the database name, graphdb(default)
+	-k ..., --schema=...	which schema in the database(IGNORE)
+	-p ...,	output prefix
+	-b	enable debugging, no debug by default
+	-r	report the progress(a number)
+	-h, --help              show this help
+
+Examples:
+	OrthologTFBSCompare.py -p /tmp/yuhuang/hs_mm_tfbs_homo -r
+
+Description:
+	Program to draw histogram of tfbs_similarity_ls between two groups
+		of homologenes.
+"""
+import os, sys, getopt, random
+sys.path += [os.path.join(os.path.expanduser('~/script/annot/bin'))]
+from codense.common import db_connect
+from sets import Set
+from rpy import r
+
+class OrthologTFBSCompare:
+	"""
+	01-05-06
+	"""
+	def __init__(self, hostname='zhoudb', dbname='graphdb', schema=None,\
+		output_prefix=None, debug=0, report=0):
+		self.hostname = hostname
+		self.dbname = dbname
+		self.schema = schema
+		self.output_prefix = output_prefix
+		self.debug = int(debug)
+		self.report = int(report)
+	
+	def get_gene_id2mt_no_set(self, curs, binding_site_table='transfac.binding_site', \
+		prom_seq_table='transfac.prom_seq', matrix_table='transfac.matrix'):
+		sys.stderr.write("Getting gene_id2mt_no_set...\n")
+		curs.execute("DECLARE crs CURSOR  FOR select p.prom_acc, m.id from %s b, %s p, %s m \
+			where p.id=b.prom_id  and b.mt_id=m.mt_id and p.prom_type_id=1"%(binding_site_table, \
+			prom_seq_table, matrix_table))
+			#only upstream
+		gene_id2mt_no_set = {}
+		curs.execute("fetch 5000 from crs")
+		rows = curs.fetchall()
+		counter = 0
+		while rows:
+			for row in rows:
+				gene_id, mt_no = row
+				gene_id = int(gene_id)
+				if gene_id not in gene_id2mt_no_set:
+					gene_id2mt_no_set[gene_id] = Set()
+				gene_id2mt_no_set[gene_id].add(mt_no)
+				counter += 1
+			if self.report:
+				sys.stderr.write("%s%s"%('\x08'*20, counter))
+			curs.execute("fetch 5000 from crs")
+			rows = curs.fetchall()
+		curs.execute("close crs")
+		sys.stderr.write("Done.\n")
+		return gene_id2mt_no_set
+	
+	def get_homo_genes_for_2_tax_ids(self, homo_group, tax_id1=9606, tax_id2=10090):
+		"""
+		01-06-06, just hs and mm
+		"""
+		tax_id1_gene_id_list = []
+		tax_id2_gene_id_list = []
+		for tax_id, gene_id in homo_group:
+			if tax_id==tax_id1:
+				tax_id1_gene_id_list.append(gene_id)
+			elif tax_id==tax_id2:
+				tax_id2_gene_id_list.append(gene_id)
+		return tax_id1_gene_id_list, tax_id2_gene_id_list
+	
+	def get_ortholog_pairs(self, curs, homologene_table='homologene.homologene'):
+		"""
+		01-06-06, just hs and mm
+		"""		
+		sys.stderr.write("Getting ortholog pairs...\n")
+		curs.execute("select hid, tax_id, gene_id from %s order by hid"%homologene_table)
+		rows = curs.fetchall()
+		prev_hid = None
+		hs_gene_id_list = []
+		mm_gene_id_list = []
+		homo_group = []
+		tax_id1 = 9606
+		tax_id2 = 10090
+		for row in rows:
+			hid, tax_id, gene_id = row
+			if prev_hid == None:
+				prev_hid = hid
+			if hid!=prev_hid:
+				tax_id1_gene_id_list, tax_id2_gene_id_list = \
+					self.get_homo_genes_for_2_tax_ids(homo_group, tax_id1, tax_id2)
+				if tax_id1_gene_id_list and tax_id2_gene_id_list:
+					hs_gene_id_list.append(tax_id1_gene_id_list[0])	#only the 1st one
+					mm_gene_id_list.append(tax_id2_gene_id_list[0])
+				if len(tax_id1_gene_id_list)!=1 or len(tax_id2_gene_id_list)!=1:
+					sys.stdout.write("hid %s has %s tax_id=%s genes, %s tax_id=%s genes.\n"%(hid, \
+						len(tax_id1_gene_id_list), tax_id1, len(tax_id2_gene_id_list), tax_id2))
+				homo_group = []
+				prev_hid = hid
+			homo_group.append((tax_id, gene_id))
+		sys.stderr.write("Done.\n")
+		return hs_gene_id_list, mm_gene_id_list
+	
+	def calculate_tfbs_similarity(self, mt_no_set1, mt_no_set2):
+		return float(len(mt_no_set1&mt_no_set2))/len(mt_no_set1|mt_no_set2)
+		
+	def get_tfbs_similarity_ls(self, hs_gene_id_list, mm_gene_id_list, gene_id2mt_no_set):
+		sys.stderr.write("Calculating tfbs_similarity.\n")
+		tfbs_similarity_ls = []
+		for i in range(len(hs_gene_id_list)):
+			hs_gene_id = hs_gene_id_list[i]
+			mm_gene_id = mm_gene_id_list[i]
+			if hs_gene_id not in gene_id2mt_no_set or mm_gene_id not in gene_id2mt_no_set:
+				continue
+			tfbs_similarity_ls.append(self.calculate_tfbs_similarity(gene_id2mt_no_set[hs_gene_id], \
+				gene_id2mt_no_set[mm_gene_id]))
+		sys.stderr.write("Done.\n")
+		return tfbs_similarity_ls
+	
+	def draw_tfbs_similarity_ls_histogram(self, tfbs_similarity_ls, output_fname):
+		sys.stderr.write("Drawing histogram for tfbs_similarity_ls...")
+		if len(tfbs_similarity_ls)>10:
+			r.png('%s.png'%output_fname)
+			r.hist(tfbs_similarity_ls, main='histogram',xlab='tfbs_similarity',ylab='freq')
+			r.dev_off()
+			sys.stderr.write("Done.\n")
+		else:
+			sys.stderr.write("too short: %s, aborted\n"%tfbs_similarity_ls)
+	
+	def run(self):
+		"""
+		01-06-06
+		
+			--db_connect()
+			--get_gene_id2mt_no_set()
+			--
+		"""
+		(conn, curs) =  db_connect(self.hostname, self.dbname, self.schema)
+		gene_id2mt_no_set = self.get_gene_id2mt_no_set(curs)
+		hs_gene_id_list, mm_gene_id_list = self.get_ortholog_pairs(curs)
+		tfbs_similarity_ls = self.get_tfbs_similarity_ls(hs_gene_id_list, mm_gene_id_list, gene_id2mt_no_set)
+		ortholog_pair_hist_fname = '%s_ortholog_pair_hist.png'%self.output_prefix
+		self.draw_tfbs_similarity_ls_histogram(tfbs_similarity_ls, ortholog_pair_hist_fname)
+		
+		#random
+		random.shuffle(mm_gene_id_list)
+		random_tfbs_similarity_ls = self.get_tfbs_similarity_ls(hs_gene_id_list, mm_gene_id_list, gene_id2mt_no_set)
+		random_pair_hist_fname = '%s_random_pair_hist.png'%self.output_prefix
+		self.draw_tfbs_similarity_ls_histogram(random_tfbs_similarity_ls, random_pair_hist_fname)
+		
+
+if __name__ == '__main__':
+	if len(sys.argv) == 1:
+		print __doc__
+		sys.exit(2)
+	
+	long_options_list = ["help", "hostname=", "dbname=", "schema="]
+	opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:p:br", long_options_list)
+	
+	hostname = 'zhoudb'
+	dbname = 'graphdb'
+	schema = None
+	output_prefix = None
+	debug = 0
+	report = 0
+
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			print __doc__
+			sys.exit(2)
+		elif opt in ("-z", "--hostname"):
+			hostname = arg
+		elif opt in ("-d", "--dbname"):
+			dbname = arg
+		elif opt in ("-k", "--schema"):
+			schema = arg
+		elif opt in ("-p",):
+			output_prefix = arg
+		elif opt in ("-b", "--debug"):
+			debug = 1
+		elif opt in ("-r",):
+			report = 1
+		
+	if output_prefix:
+		instance = OrthologTFBSCompare(hostname, dbname, schema, output_prefix, \
+			debug, report)
+		instance.run()
+	else:
+		print __doc__
+		sys.exit(2)
