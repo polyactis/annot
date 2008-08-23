a059fb44711c5dcd12b53e650876664d78d3fae5
yh <yh@4ebff559-900f-0410-a220-daa186eb8a63>
2008-08-23 08:28:56 +0000

    def dstruc_loadin_from_db(): 	2008-08-23 	fit into the new db infrastructure 	association info comes from 'go_association' table 	use tax_id to restrict genes
    add node type 6: all nodes whose size is from size to max_size but not their children
    
    use ProcessOptions
    
    git-svn-id: file:///usr/local/svn/annot@1169 4ebff559-900f-0410-a220-daa186eb8a63

--- trunk/bin/GO/go_informative_node.py
+++ trunk/bin/GO/go_informative_node.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python
 """
-Usage:	go_informative_node.py -a GO_ASSOCIATION -i GO_INDEX [OPTIONS}
-	go_informative_node.py -k SCHEMA -b [OPTIONS]
 
-Option:
-	-z ..., --hostname=...	the hostname, zhoudb(default)
-	-d ..., --dbname=...	the database name, graphdb(default)
-	-k ..., --schema=...	which schema in the database
-	-a ..., -go_association=...	the file mapping go_id to gene_id
-	-i ..., --go_index=...	the file mapping go_id to go_index, OR the go_graph file
-	-s ..., --size=...	the size of the informative node, 60(default)
-	-m ...,	max_no_of_nodes, only nodes bigger than size above, 150(default), node_type=4
-	-t ..., --type=...	output format, 0(default, full), 1(stat), 2(between)
-	-n ..., --node_type=...	1(all, default), 2(informative), 3(level 4), 4(biggest-first-break 
-		control no of nodes), 5(biggest-first-break-level-by-level, similar to 4)
-	-l ..., --level=...	for node_type=3 or 4, this determines the level of the initial
-		function candidates 4(default)
-	-c ...,	branch, 1(molecular_function), 2(biological_process, default), 3(cellular_component)
-	-u,	debug
-	-h, --help      show this help
-	
 Examples:
 	go_informative_node.py -i process.pathindex.annotation.txt -a go2ug.txt
 	go_informative_node.py -i term2term.txt -a go2ug.txt -b
 	go_informative_node.py -i term2term.txt -a go2ug.txt -b -t 1
 	go_informative_node.py -k hs_yh60_42 -b
+	
+	#2006 output informative nodes
 	go_informative_node.py -k sc_38_all_no_info -n 2 -b
-
+	
+	#2008-08-22 generate informative nodes and output into file
+	go_informative_node.py -d go -o /tmp/go_informative_node.tsv
+	
+	go_informative_node.py -d go -o /tmp/go_informative_node.tsv   -n 2 -s 150
+	
 Description:
 	First usage:
 	this program constructs informative nodes based on go_association file
@@ -48,11 +36,11 @@ import sys, os, math
 bit_number = math.log(sys.maxint)/math.log(2)
 if bit_number>40:       #64bit
 	sys.path.insert(0, os.path.expanduser('~/lib64/python'))
-	sys.path.insert(0, os.path.join(os.path.expanduser('~/script64/annot/bin')))
+	sys.path.insert(0, os.path.join(os.path.expanduser('~/script64')))
 else:   #32bit
 	sys.path.insert(0, os.path.expanduser('~/lib/python'))
-	sys.path.insert(0, os.path.join(os.path.expanduser('~/script/annot/bin')))
-import sys, os, psycopg, getopt, csv
+	sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
+import sys, os, getopt, csv
 from kjbuckets import *
 from heapq import heappush, heappop, heapreplace, heapify
 from sets import Set
@@ -162,37 +150,41 @@ class go_informative_node:
 					sys.stdout.write('%s\t%s\t%s\n'%(go_id, go_index, gene_id))
 
 
-class go_informative_node_bfs:
-	"""
-	03-08-05
-	"""
-	def __init__(self, hostname='zhoudb', dbname='graphdb', schema=None, go_association=None, \
-		go_graph=None, size=60, max_no_of_nodes=160,\
-		type=0, node_type=1, level=4, branch=2, debug=0):
+class go_informative_node_bfs(object):
+	__doc__ = __doc__
+	option_default_dict = {('drivername', 1,):['mysql', 'v', 1, 'which type of database? mysql or postgres', ],\
+							('hostname', 1, ): ['papaya.usc.edu', 'z', 1, 'hostname of the db server', ],\
+							('dbname', 1, ): ['stock_250k', 'd', 1, 'database name', ],\
+							('schema', 0, ): [None, 'k', 1, 'database schema name', ],\
+							('db_user', 1, ): [None, 'u', 1, 'database username', ],\
+							('db_passwd', 1, ): [None, 'p', 1, 'database password', ],\
+							("go_association", 0, ): ['genome.gene2go', 'a', 1, 'the table or file mapping go_id to gene_id'],\
+							("go_graph", 0, ): [None, 'i', 1, 'the file mapping go_id to go_index, OR the go_graph file. if not given, get graph from database.'],\
+							('size', 1, int): [60, 's', 1, 'the minimum size for each informative node'],\
+							('max_size', 1, int): [100, 'e', 1, 'the maximum size for each node, only for node_type=6'],\
+							('max_no_of_nodes', 0, int): [160, 'm', 1, 'only nodes bigger than size above, only for node_type=4'],\
+							('type', 0, int): [0, 't', 1, 'output format, 0(full), 1(stat), 2(between)'],\
+							('node_type', 0, int): [2, 'n', 1, '1(all), 2(informative), 3(level 4), 4(biggest-first-break \
+		control no of nodes), 5(biggest-first-break-level-by-level, similar to 4), 6(all nodes whose size is from size to max_size but not their children)'],\
+							('level', 1, int):[4, 'l', 1, 'for node_type=3 or 4, this determines the level of the initial\
+		function candidates 4(default)'],\
+							('tax_id', 0, int): [3702, 'x', 1, 'Taxonomy ID to get gene position and coordinates.'],\
+							('branch', 0, int):[2, 'c', 1, 'branch, 1(molecular_function), 2(biological_process), 3(cellular_component)'],\
+							("output_fname", 0, ): [None, 'o', 1, 'if you wanna results into a file'],\
+							('exclude_IEA', 0): [0, 'I', 0, 'Exclude gene2go entries whose evidence is IEA'],\
+							('debug', 0, int):[0, 'b', 0, 'toggle debug mode'],\
+							('report', 0, int):[0, 'r', 0, 'toggle report, more verbose stdout/stderr.']}
+	def __init__(self, **keywords):
 		"""
+		2008-08-22 use ProcessOptions
 		03-08-05
 			add a parameter, level to indicate the level of qualified nodes for node_type==3.
 		11-06-05 add node_type=4
 		12-30-05
 			add branch
 		"""
-		self.schema = schema
-		if self.schema:
-		#input from database
-			self.conn = psycopg.connect('host=%s dbname=%s'%(hostname, dbname))
-			self.curs = self.conn.cursor()
-			self.curs.execute("set search_path to %s"%self.schema)
-		else:
-		#input from files
-			self.goa_f = csv.reader(open(go_association, 'r'), delimiter='\t')
-			self.gog_f = csv.reader(open(go_graph, 'r'), delimiter='\t')
-		self.size = int(size)
-		self.max_no_of_nodes = int(max_no_of_nodes)
-		self.type = int(type)
-		self.node_type = int(node_type)
-		self.level = int(level)
-		self.branch = int(branch)
-		self.debug = int(debug)
+		from pymodule import ProcessOptions
+		self.ad = ProcessOptions.process_function_arguments(keywords, self.option_default_dict, error_doc=self.__doc__, class_to_have_attr=self)
 		
 		output_format_dict = {0:self.output_full,
 			1:self.output_stat,
@@ -224,7 +216,8 @@ class go_informative_node_bfs:
 		self.go_id_descendent2gene_id_dict = {}
 		self.informative_node_dict = {}
 		self.go_id2depth = {}
-		self.log_file = open('/tmp/go_informative_node.log', 'w')
+		if self.debug:
+			self.log_file = open('/tmp/go_informative_node.log', 'w')
 
 	def dstruc_loadin(self):
 		sys.stderr.write("Loading Data STructure...")
@@ -243,15 +236,19 @@ class go_informative_node_bfs:
 		self.go_id_set = kjSet(self.go_graph.keys() + self.go_graph.values())
 		sys.stderr.write("Done\n")
 
-	def dstruc_loadin_from_db(self):
+	def dstruc_loadin_from_db(self, go_association, tax_id, go_branch, go_unknown_id, exclude_IEA=0, go_term_table='go.term', go_term2term_table='go.term2term'):
 		"""
+		2008-08-23
+			fit into the new db infrastructure
+			association info comes from 'go_association' table
+			use tax_id to restrict genes
 		11-07-05 add the complement_go_graph
 		12-30-05
 			flexible, adjut to self.branch
 		03-11-06
 			count #total genes
 		"""
-		sys.stderr.write("Loading Data STructure...")
+		sys.stderr.write("Loading Data Structure ...")
 		#biological_process, not obsolete, not biological_process unknown.
 		"""
 		#12-30-05 deal with non-schema specific association tasks, here is human
@@ -261,10 +258,13 @@ class go_informative_node_bfs:
 			self.branch_unknown_acc_dict[self.branch]))
 		"""
 		#not obsolete, not unknown
-		self.curs.execute("select a.go_id, g.gene_id from graph.association a, go.term t,\
-			gene g where g.gene_id=a.gene_id and t.acc=a.go_id and\
-			t.term_type='%s' and t.acc!='%s' and t.is_obsolete=0"%(self.branch_name_dict[self.branch],
-			self.branch_unknown_acc_dict[self.branch]))
+		query = "select a.go_id, a.gene_id from %s a, %s t\
+				where a.tax_id=%s and t.acc=a.go_id and\
+				t.term_type='%s' and t.acc!='%s' and t.is_obsolete=0"%(go_association, go_term_table, tax_id, go_branch,
+																						go_unknown_id)
+		if exclude_IEA:
+			query += " and evidence!='IEA'"
+		self.curs.execute(query)
 		
 		#03-11-06 used to count #genes in total
 		self.total_gene_id_set = Set()
@@ -283,10 +283,10 @@ class go_informative_node_bfs:
 		#get the non-obsolete biological_process GO DAG
 		#12-30-05
 		self.curs.execute("select t2t.term1_id, t2t.term2_id, t1.acc, t2.acc,t1.depth, t2.depth from \
-			go.term2term t2t, go.term t1, go.term t2 where t2t.term1_id=t1.id and \
+			%s t2t, %s t1, %s t2 where t2t.term1_id=t1.id and \
 			t2t.term2_id=t2.id and t1.is_obsolete=0 and t2.is_obsolete=0 and \
-			t1.term_type='%s' and t2.term_type='%s' "%(self.branch_name_dict[self.branch],
-			self.branch_name_dict[self.branch]))
+			t1.term_type='%s' and t2.term_type='%s' "%(go_term2term_table, go_term_table,\
+													go_term_table, go_branch, go_branch))
 		
 		rows = self.curs.fetchall()
 		for row in rows:
@@ -308,12 +308,12 @@ class go_informative_node_bfs:
 		self.go_id_set = kjSet(self.go_graph.keys() + self.go_graph.values())
 		
 		#setup self.go_id2go_name
-		self.curs.execute("select acc, name from go.term")
+		self.curs.execute("select acc, name from %s"%go_term_table)
 		rows = self.curs.fetchall()
 		for row in rows:
 			self.go_id2go_name[row[0]] = row[1]
 		
-		sys.stderr.write("Done\n")
+		sys.stderr.write("%s total genes. Done\n"%(len(self.total_gene_id_set)))
 	
 	def keep_source_and_singletons_from_candidates(self, go_graph, go_id_descendent2gene_id_dict, candidates_set):
 		"""
@@ -524,23 +524,66 @@ class go_informative_node_bfs:
 		sys.stderr.write("Done getting nodes for node type 5 with %s/%s nodes.\n"%(len(candidates_set), no_of_big_nodes))
 		return informative_node_dict
 	
-	def run(self):
+	def node_type_2(self, root, min_size):
 		"""
-		03-06-05
-			add the node_type=3, level=5
-		11-06-05 add node_type=4
-		11-07-05 add node_type=5
+		2008-08-22
+			split out of run()
 		"""
-		if self.schema:
-		#input from database	
-			self.dstruc_loadin_from_db()
-		else:
-		#input from files
-			self.dstruc_loadin()
+		sys.stderr.write("Generating type 2 nodes ... ")
+		#below is analogous to BFS,
+		#informative_node_dict's role is similar to vertex coloring in real BFS
+		self.informative_node_dict[root] = 1
+		list_queue = [root]
+		while list_queue:
+			go_id = list_queue.pop(0)
+			neighbor_list = self.go_graph.neighbors(go_id)
+			for neighbor in neighbor_list:
+				if len(self.go_id_descendent2gene_id_dict[neighbor]) >= min_size:
+					if self.debug:
+						self.log_file.write('%s ousted by %s\n'%(go_id,neighbor))
+					self.informative_node_dict[go_id] = 0
+					if self.informative_node_dict[neighbor] == 0:
+					#trick is here. informative_node candidates and first touch
+						list_queue.append(neighbor)
+					self.informative_node_dict[neighbor] = 1
+		sys.stderr.write("Done.\n")
+	
+	def node_type_6(self, root, min_size, max_size, go_graph, informative_node_dict, \
+				go_id2gene_id_dict):
+		"""
+		2008-08-22
+			all nodes whose size is from size to max_size but not their children
+		"""
+		sys.stderr.write("Generating type 6 nodes ... ")
+		#below is analogous to BFS,
+		#informative_node_dict's role is similar to vertex coloring in real BFS
+		informative_node_dict[root] = 0
+		list_queue = [root]
+		while list_queue:
+			go_id = list_queue.pop(0)
+			neighbor_list = go_graph.neighbors(go_id)
+			for neighbor in neighbor_list:
+				no_of_associated_genes = len(go_id2gene_id_dict[neighbor])
+				if no_of_associated_genes >= min_size and no_of_associated_genes<=max_size:
+					informative_node_dict[neighbor] = 1
+				else:
+					if no_of_associated_genes >= min_size:
+						list_queue.append(neighbor)
+		sys.stderr.write("Done.\n")
+	
+	def prepareData(self):
+		"""
+		2008-08-22
+			split out of run()
+		"""
+		sys.stderr.write("Preparing data ... ")
 		#11-06-05
 		if self.node_type==4 or self.node_type==5:
 			init_candidates_queue = []	#a min queue, each entry is (size, go_id), sorted by size
 			init_candidates_set = Set()
+		else:
+			init_candidates_queue = None
+			init_candidates_set = None
 			
 		for go_id in self.go_id_set.items():
 			#collect the associated genes from descendent terms
@@ -568,60 +611,99 @@ class go_informative_node_bfs:
 				if self.go_id2depth[go_id] == self.level and node_size>0:
 					heappush(init_candidates_queue, [node_size, go_id])	#WATCH NO minus(-) ahead of node_size
 					init_candidates_set.add(go_id)
+		sys.stderr.write("Done.\n")
+		return init_candidates_queue, init_candidates_set
+	
+	def run(self):
+		"""
+		2008-08-22
+			restructure
+		03-06-05
+			add the node_type=3, level=5
+		11-06-05 add node_type=4
+		11-07-05 add node_type=5
+		"""
+		if os.path.isfile(self.go_association):
+			#input from files
+			self.goa_f = csv.reader(open(go_association, 'r'), delimiter='\t')
+			self.gog_f = csv.reader(open(go_graph, 'r'), delimiter='\t')
+			self.dstruc_loadin()
+		else:
+			if self.drivername=='postgres':
+				#input from database
+				try:
+					import psycopg
+				except:
+					import psycopg2 as psycopg
+				self.conn = psycopg.connect('host=%s dbname=%s user=%s passwd=%s'%(hostname, dbname, self.db_user, self.db_passwd))
+				self.curs = self.conn.cursor()
+				if self.schema:
+					self.curs.execute("set search_path to %s"%self.schema)
+			elif self.drivername=='mysql':
+				import MySQLdb
+				self.conn = MySQLdb.connect(db=self.dbname, host=self.hostname, user=self.db_user, passwd = self.db_passwd)
+				self.curs = self.conn.cursor()
+			self.dstruc_loadin_from_db(self.go_association, self.tax_id, self.branch_name_dict[self.branch], \
+									self.branch_unknown_acc_dict[self.branch], self.exclude_IEA)
 		
+		init_candidates_queue, init_candidates_set = self.prepareData()
+		
+		"""
+		node_size_ls = [len(value) for key, value in self.go_id2gene_id_dict.iteritems()]
+		import pylab
+		pylab.title("Histogram of GO node size")
+		pylab.hist(node_size_ls, 300)
+		pylab.show()
+		"""
 		if self.node_type==2:
-			#below is analogous to BFS,
-			#informative_node_dict's role is similar to vertex coloring in real BFS
-			self.informative_node_dict[self.root] = 1
-			list_queue = [self.root]
-			while list_queue:
-				go_id = list_queue.pop(0)
-				neighbor_list = self.go_graph.neighbors(go_id)
-				for neighbor in neighbor_list:
-					if len(self.go_id_descendent2gene_id_dict[neighbor]) >= self.size:
-						self.log_file.write('%s ousted by %s\n'%(go_id,neighbor))
-						self.informative_node_dict[go_id] = 0
-						if self.informative_node_dict[neighbor] == 0:
-						#trick is here. informative_node candidates and first touch
-							list_queue.append(neighbor)
-						self.informative_node_dict[neighbor] = 1
+			self.node_type_2(self.root, self.size)
 		elif self.node_type==4:
 			self.informative_node_dict = self.node_type_4(init_candidates_queue, init_candidates_set,\
 				self.go_graph, self.complement_go_graph, self.go_id_descendent2gene_id_dict, self.max_no_of_nodes, self.size)
 		elif self.node_type==5:
 			self.informative_node_dict = self.node_type_5(init_candidates_queue, init_candidates_set,\
 				self.go_graph, self.complement_go_graph, self.go_id_descendent2gene_id_dict, self.max_no_of_nodes, self.size)
-		self.output()
+		elif self.node_type==6:
+			self.node_type_6(self.root, self.size, self.max_size, self.go_graph, self.informative_node_dict, \
+				self.go_id_descendent2gene_id_dict)
+		if self.output_fname:
+			self.output(self.output_fname, self.go_id_descendent2gene_id_dict)
 		"""
 		#03-12-06 to see how the informative_node's are distributed, designed for node_type=2
 		self.output_go_graph_above_informative_nodes(self.root, self.go_graph, self.informative_node_dict, \
 			self.go_id_descendent2gene_id_dict, '/tmp/informative_node.dot', self.size)
 		"""
 		
-	def output(self):
+	def output(self, output_fname, go_id2gene_id_dict):
 		"""
+		2008-08-22
+			add output_fname
 		03-11-06 count #genes in informative nodes
 		"""
+		sys.stderr.write("Outputting nodes ...")
+		self.outf = open(output_fname, 'w')
 		gene_id_in_informative_nodes_set = Set()
+		no_of_go_nodes = 0
 		for go_id,value in self.informative_node_dict.iteritems():
 			if value == 1:
-				gene_id_list = self.go_id_descendent2gene_id_dict[go_id].items()
+				no_of_go_nodes += 1
+				gene_id_list = go_id2gene_id_dict[go_id].items()
 				self._output(go_id, gene_id_list)
 				#03-11-06
 				for gene_id in gene_id_list:
 					gene_id_in_informative_nodes_set.add(gene_id)
-		sys.stderr.write("%s-%s=%s, #genes lost.\n"%(len(self.total_gene_id_set), len(gene_id_in_informative_nodes_set),\
+		sys.stderr.write("%s GO nodes retained. %s-%s=%s, #genes lost.\n"%(no_of_go_nodes, len(self.total_gene_id_set), len(gene_id_in_informative_nodes_set),\
 			len(self.total_gene_id_set) - len(gene_id_in_informative_nodes_set)))
 	
 	def output_full(self, go_id, gene_id_list):
 		for gene_id in gene_id_list:
-			sys.stdout.write('%s\t%s\t%s\n'%(go_id, self.go_id2go_name[go_id], gene_id))
+			self.outf.write('%s\t%s\t%s\n'%(go_id, self.go_id2go_name[go_id], gene_id))
 
 	def output_stat(self, go_id, gene_id_list):
-		sys.stdout.write("%s\t%d\n"%(go_id, len(gene_id_list)))
+		self.outf.write("%s\t%d\n"%(go_id, len(gene_id_list)))
 
 	def output_between(self, go_id, gene_id_list):
-		sys.stdout.write("%s\t%s\t%s\n"%(go_id, self.go_id2go_name[go_id], '\t'.join(gene_id_list)))
+		self.outf.write("%s\t%s\t%s\n"%(go_id, self.go_id2go_name[go_id], '\t'.join(gene_id_list)))
 
 	def output_go_graph_above_informative_nodes(self, root, go_graph, informative_node_dict, \
 			go_id_descendent2gene_id_dict, output_fname, size):
@@ -694,64 +776,9 @@ class go_informative_node_bfs:
 
 
 if __name__ == '__main__':
-	if len(sys.argv) == 1:
-		print __doc__
-		sys.exit(2)
-		
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hz:d:k:a:i:s:m:t:n:l:c:bu", ["help", "hostname=", \
-			"dbname=", "schema=", "go_association=", "go_index=", "size=", "type=", "node_type=", "level=", "bfs"])
-	except:
-		print __doc__
-		sys.exit(2)
+	from pymodule import ProcessOptions
+	main_class = go_informative_node_bfs
+	po = ProcessOptions(sys.argv, main_class.option_default_dict, error_doc=main_class.__doc__)
 	
-	hostname = 'zhoudb'
-	dbname = 'graphdb'
-	schema = ''	
-	go_association = ''
-	go_index = ''
-	size = 60
-	max_no_of_nodes = 160
-	type = 0
-	node_type = 1
-	level = 4
-	branch = 2
-	bfs = 1
-	debug = 0
-	for opt, arg in opts:
-		if opt in ("-h", "--help"):
-			print __doc__
-			sys.exit(2)
-		elif opt in ("-z", "--hostname"):
-			hostname = arg
-		elif opt in ("-d", "--dbname"):
-			dbname = arg
-		elif opt in ("-k", "--schema"):
-			schema = arg
-		elif opt in ("-a", "--go_association"):
-			go_association = arg
-		elif opt in ("-i", "--go_index"):
-			go_index = arg
-		elif opt in ("-s", "--size"):
-			size = int(arg)
-		elif opt in ("-m",):
-			max_no_of_nodes = int(arg)
-		elif opt in ("-t", "--type"):
-			type = int(arg)
-		elif opt in ("-n", "--node_type"):
-			node_type = int(arg)
-		elif opt in ("-l", "--level"):
-			level = int(arg)
-		elif opt in ("-c",):
-			branch = int(arg)
-		elif opt in ("-b", "--bfs"):
-			bfs = 1
-		elif opt in ("-u",):
-			debug = 1
-	if schema or (go_association and go_index):
-		instance = go_informative_node_bfs(hostname, dbname, schema, go_association, \
-			go_index, size, max_no_of_nodes, type, node_type, level, branch, debug)
-		instance.run()
-	else:
-		print __doc__
-		sys.exit(2)
+	instance = main_class(**po.long_option2value)
+	instance.run()
